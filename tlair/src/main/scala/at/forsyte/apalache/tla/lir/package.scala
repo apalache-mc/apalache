f3845611ec21d95f096f6cddf5b222075ef5225f
Igor Konnov <konnov@forsyte.at>
2016-11-08 15:44:00 +0100

    scopes and modules

--- tlair/src/main/scala/at/forsyte/apalache/tla/lir/package.scala
+++ tlair/src/main/scala/at/forsyte/apalache/tla/lir/package.scala
@@ -3,7 +3,10 @@ package at.forsyte.apalache.tla
 package lir {
   import at.forsyte.apalache.tla.lir.oper._
 
-  /** the base class for the universe of objects used in TLA+ */
+import scala.collection.immutable.HashMap
+import scala.collection.parallel.mutable
+
+/** the base class for the universe of objects used in TLA+ */
   abstract class TlaValue
 
   /** a constant as defined by CONSTANT */
@@ -12,14 +15,133 @@ package lir {
   /** a variable as defined by VARIABLE */
   class TlaVar(val name: String) extends TlaValue
 
-  /** A scope in which constants and variables are declared.
-    * TODO: use the scope!
-    */
-  trait TlaScope {
-    def declareConst(name: String): TlaConst
-    def declareVar(name: String): TlaVar
-    def lookup(name: String): TlaValue
+
+  abstract class ScopeSymbol {
+    def name: String
   }
+
+  // declare a for LET a = ... IN...
+  case class ScopeLetVariable(param: SimpleParam) extends ScopeSymbol {
+    override def name: String = param.name
+  }
+  // declare x for \forall x \in X. ...
+  case class ScopeBoundVariable(param: SimpleParam) extends ScopeSymbol {
+    override def name: String = param.name
+  }
+  // an operator parameter
+  case class ScopeFormalParameter(param: Param) extends ScopeSymbol {
+    override def name: String = param.name
+  }
+  // a variable
+  case class ScopeVariable(tlaVar: TlaVar) extends ScopeSymbol {
+    override def name: String = tlaVar.name
+  }
+  // a constant
+  case class ScopeConstant(const: TlaConst) extends ScopeSymbol {
+    override def name: String = const.name
+  }
+  // an operator definition
+  case class ScopeOper(operDef: TlaOperDef) extends ScopeSymbol {
+    override def name: String = operDef.name
+  }
+
+  // a lookup exception
+  class SymbolNotFoundException(val msg: String) extends RuntimeException
+
+  /**
+   *  A scope in which constants and variables are declared.
+   */
+  trait TlaScope {
+    def add(sym: ScopeSymbol): Unit
+    def remove(name: String): Unit
+    def exists(name: String): Boolean
+    def lookup(name: String): ScopeSymbol // throws SymbolNotFoundException
+  }
+
+  class FlatScope extends TlaScope {
+    var map = HashMap.empty[String, ScopeSymbol]
+
+    override def add(sym: ScopeSymbol) = {
+      map = map + ((sym.name, sym))
+    }
+
+    override def remove(name: String) = {
+      map = map - name
+    }
+
+    override def exists(name: String): Boolean = {
+      map.contains(name)
+    }
+
+    override def lookup(name: String): ScopeSymbol = {
+      map.get(name) match {
+        case Some(s) => s
+        case None => throw new SymbolNotFoundException("Symbol %s not found".format(name))
+      }
+    }
+  }
+
+  class ScopeStack extends TlaScope {
+    var scopes: List[TlaScope] = List()
+
+    override def add(sym: ScopeSymbol) = {
+      scopes.head.add(sym)
+    }
+
+    override def remove(name: String) = {
+      scopes.find { s => s.exists(name) } match {
+        case Some(s) => s.remove(name)
+        case None => ()
+      }
+    }
+
+    override def lookup(name: String) = {
+      def search(s: Seq[TlaScope]): ScopeSymbol = {
+        s match {
+          case Seq() =>
+            throw new SymbolNotFoundException("Symbol %s not found".format(name))
+
+          case hd :: tl =>
+            try {
+              hd.lookup(name)
+            } catch {
+              case _: SymbolNotFoundException => search(tl)
+            }
+        }
+      }
+      search(scopes)
+    }
+
+    override def exists(name: String) = {
+      try {
+        lookup(name)
+        true
+      } catch {
+        case _: SymbolNotFoundException =>
+          false
+      }
+    }
+
+    def push(s: TlaScope) = {
+      scopes = s :: scopes
+    }
+
+    def pop(): Unit = {
+      if (scopes.isEmpty) throw new IllegalStateException("Empty scope stack")
+      else scopes = scopes.tail
+    }
+  }
+
+  /**
+   * A module is just an hierarchical scope.
+   */
+  class Module(val name: String, val imported: Seq[Module]) extends ScopeStack {
+    for (m <- imported) {
+      push(m)
+    }
+    push(new FlatScope)
+  }
+
 
   /**
   A formal parameter of an operator. Note that one can use a formal parameter in a TLA+ expression,
