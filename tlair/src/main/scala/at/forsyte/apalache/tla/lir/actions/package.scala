c17fbf1d39193fc265aa0b5d850c80cf190b5cdb
Igor Konnov <konnov@forsyte.at>
2017-03-12 19:02:37 +0100

    fixed arity of \cdot (as Jure correctly pointed out months earlier)

--- tlair/src/main/scala/at/forsyte/apalache/tla/lir/actions/package.scala
+++ tlair/src/main/scala/at/forsyte/apalache/tla/lir/actions/package.scala
@@ -3,60 +3,71 @@ package at.forsyte.apalache.tla.lir
 import at.forsyte.apalache.tla.lir.oper._
 
 /**
- * Action operators.
- */
+  * Action operators.
+  */
 package object actions {
+
   abstract class TlaActionOper extends TlaOper {
     override def interpretation: Interpretation.Value = Interpretation.Predefined
   }
-  
+
   object TlaActionOper {
     /**
-     * The prime operator. By the TLA+ restrictions, we cannot apply it twice, e.g., (x')' is illegal.
-     */
+      * The prime operator. By the TLA+ restrictions, we cannot apply it twice, e.g., (x')' is illegal.
+      */
     val prime = new TlaActionOper {
       override def name: String = "'"
+
       override def arity: OperArity = FixedArity(1)
     }
 
     /**
-     * The operator that executes an action or keeps the variable values.
-     */
+      * The operator that executes an action or keeps the variable values.
+      */
     val stutter = new TlaActionOper {
       override def name: String = "[A]_e"
+
       override def arity: OperArity = FixedArity(2)
     }
 
     /**
-     * The operator that executes an action and enforces the values to change.
-     */
+      * The operator that executes an action and enforces the values to change.
+      */
     val nostutter = new TlaActionOper {
       override def name: String = "<A>_e"
+
       override def arity: OperArity = FixedArity(2)
     }
 
     /**
-     * The ENABLED operator.
-     */
+      * The ENABLED operator.
+      */
     val enabled = new TlaActionOper {
       override def name: String = "ENABLED"
+
       override def arity: OperArity = FixedArity(1)
     }
 
     /**
-     * The operator that executes an action or keeps the variable values.
-     */
+      * The operator that executes an action or keeps the variable values.
+      */
     val unchanged = new TlaActionOper {
       override def name: String = "UNCHANGED"
+
       override def arity: OperArity = FixedArity(1)
     }
 
     /**
-     * The sequential composition. //Note (Jure, 17.11): Arity 2?
-     */
+      * The sequential composition of operators.
+      *
+      * Jure@17.11.16: Arity 2?
+      * Igor@12.03.17: Arity 2. Fixed.
+      */
     val composition = new TlaActionOper {
       override def name: String = "\\cdot"
-      override def arity: OperArity = FixedArity(1)
+
+      override def arity: OperArity = FixedArity(2)
     }
   }
+
 }
