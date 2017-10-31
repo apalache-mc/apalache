ed8ae148faa0dceb6e2e2a240ff94ae28ab0768c
Igor Konnov <konnov@forsyte.at>
2017-11-01 00:25:52 +0100

    more rewriting operations

--- tla-bmcmt/src/main/scala/at/forsyte/apalache/tla/bmcmt/ArenaCell.scala
+++ tla-bmcmt/src/main/scala/at/forsyte/apalache/tla/bmcmt/ArenaCell.scala
@@ -2,11 +2,17 @@ package at.forsyte.apalache.tla.bmcmt
 
 import at.forsyte.apalache.tla.bmcmt.types.CellType
 
+object ArenaCell {
+  val namePrefix = "$C$"
+}
+
 /**
   * A symbolic memory cell. Each cell has an identifier (similar to a memory address in a physical computer).
   *
   * @author Igor Konnov
   */
 class ArenaCell(val id: Int, val cellType: CellType) {
-
+  override def toString: String = {
+    "%s%d".format(ArenaCell.namePrefix, id)
+  }
 }
