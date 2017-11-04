fc3487b31087a63fd2bc07833bedd32f40551c6d
Igor Konnov <konnov@forsyte.at>
2017-11-04 21:01:07 +0100

    value cache for integer literals

--- tla-bmcmt/src/main/scala/at/forsyte/apalache/tla/bmcmt/rules/IntConstRule.scala
+++ tla-bmcmt/src/main/scala/at/forsyte/apalache/tla/bmcmt/rules/IntConstRule.scala
@@ -5,12 +5,17 @@ import at.forsyte.apalache.tla.lir.oper.TlaOper
 import at.forsyte.apalache.tla.lir.values.TlaInt
 import at.forsyte.apalache.tla.lir.{NameEx, OperEx, ValEx}
 
+import scala.collection.mutable
+
 /**
   * Implements the rule SE-INT-CONST.
   *
   * @author Igor Konnov
   */
 class IntConstRule(rewriter: SymbStateRewriter) extends RewritingRule {
+  // cache the integer constants that are introduced for integer literals
+  private val cache: mutable.Map[BigInt, String] = new mutable.HashMap()
+
   override def isApplicable(symbState: SymbState): Boolean = {
     symbState.ex match {
       case ValEx(TlaInt(_)) => true
@@ -21,8 +26,16 @@ class IntConstRule(rewriter: SymbStateRewriter) extends RewritingRule {
   override def apply(state: SymbState): SymbState = {
     state.ex match {
       case ValEx(TlaInt(n)) =>
-        val intConst = state.solverCtx.introIntConst()
-        state.solverCtx.assertGroundExpr(OperEx(TlaOper.eq, NameEx(intConst), ValEx(TlaInt(n))))
+        val intConst =
+          if (cache.contains(n)) {
+            cache(n)
+          } else {
+            // introduce a new constant
+            val intConst = state.solverCtx.introIntConst()
+            state.solverCtx.assertGroundExpr(OperEx(TlaOper.eq, NameEx(intConst), ValEx(TlaInt(n))))
+            cache.put(n, intConst)
+            intConst
+          }
         val finalState =
           state.setTheory(IntTheory()).setRex(NameEx(intConst))
         rewriter.coerce(finalState, state.theory)
