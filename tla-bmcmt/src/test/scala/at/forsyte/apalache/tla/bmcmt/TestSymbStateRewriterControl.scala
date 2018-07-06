7b2e0d95fdb5538e6715f0162f54943f92804e2c
Igor Konnov <konnov@forsyte.at>
2018-07-06 23:29:12 +0200

    IF-THEN-ELSE returning sets

--- tla-bmcmt/src/test/scala/at/forsyte/apalache/tla/bmcmt/TestSymbStateRewriterControl.scala
+++ tla-bmcmt/src/test/scala/at/forsyte/apalache/tla/bmcmt/TestSymbStateRewriterControl.scala
@@ -103,6 +103,60 @@ class TestSymbStateRewriterControl extends RewriterBase {
     }
   }
 
+  test("""SE-ITE[5]: IF 3 < 2 THEN {1, 2} ELSE {2, 3} ~~> {2, 3}""") {
+    val pred = tla.lt(tla.int(3), tla.int(2))
+    val e1 = tla.enumSet(tla.int(1), tla.int(2))
+    val e2 = tla.enumSet(tla.int(2), tla.int(3))
+    val ite = tla.ite(pred, e1, e2)
+
+    val state = new SymbState(ite, CellTheory(), arena, new Binding)
+    val rewriter = new SymbStateRewriter(solverContext)
+    val nextState = rewriter.rewriteUntilDone(state)
+    nextState.ex match {
+      case res @ NameEx(name) =>
+        assert(CellTheory().hasConst(name))
+        assert(solverContext.sat())
+        rewriter.push()
+        val eqState = rewriter.rewriteUntilDone(nextState.setTheory(BoolTheory()).setRex(tla.eql(res, e2)))
+        solverContext.assertGroundExpr(eqState.ex)
+        assert(solverContext.sat())
+        rewriter.pop()
+        val neqState = rewriter.rewriteUntilDone(nextState.setTheory(BoolTheory()).setRex(tla.eql(res, e1)))
+        solverContext.assertGroundExpr(neqState.ex)
+        assert(!solverContext.sat())
+
+      case _ =>
+        fail("Unexpected rewriting result")
+    }
+  }
+
+  test("""SE-ITE[5]: IF 2 < 3 THEN {1, 2} ELSE {2, 3} ~~> {1, 2}""") {
+    val pred = tla.lt(tla.int(2), tla.int(3))
+    val e1 = tla.enumSet(tla.int(1), tla.int(2))
+    val e2 = tla.enumSet(tla.int(2), tla.int(3))
+    val ite = tla.ite(pred, e1, e2)
+
+    val state = new SymbState(ite, CellTheory(), arena, new Binding)
+    val rewriter = new SymbStateRewriter(solverContext)
+    val nextState = rewriter.rewriteUntilDone(state)
+    nextState.ex match {
+      case res @ NameEx(name) =>
+        assert(CellTheory().hasConst(name))
+        assert(solverContext.sat())
+        rewriter.push()
+        val eqState = rewriter.rewriteUntilDone(nextState.setTheory(BoolTheory()).setRex(tla.eql(res, e1)))
+        solverContext.assertGroundExpr(eqState.ex)
+        assert(solverContext.sat())
+        rewriter.pop()
+        val neqState = rewriter.rewriteUntilDone(nextState.setTheory(BoolTheory()).setRex(tla.eql(res, e2)))
+        solverContext.assertGroundExpr(neqState.ex)
+        assert(!solverContext.sat())
+
+      case _ =>
+        fail("Unexpected rewriting result")
+    }
+  }
+
   test("""LET A == TRUE IN... ~~> [A -> TRUE]""") {
     val decl = TlaOperDecl("A", List(), tla.bool(true))
     val letIn = tla.letIn(OperEx(decl.operator), decl)
