bfae6b6eff921fd1de3601b8bdcd23a6769f1e22
Igor Konnov <konnov@forsyte.at>
2018-07-06 23:29:12 +0200

    IF-THEN-ELSE returning sets

--- tla-bmcmt/src/main/scala/at/forsyte/apalache/tla/bmcmt/rules/IfThenElseRule.scala
+++ tla-bmcmt/src/main/scala/at/forsyte/apalache/tla/bmcmt/rules/IfThenElseRule.scala
@@ -2,11 +2,10 @@ package at.forsyte.apalache.tla.bmcmt.rules
 
 import at.forsyte.apalache.tla.bmcmt._
 import at.forsyte.apalache.tla.bmcmt.implicitConversions._
-import at.forsyte.apalache.tla.bmcmt.types.{BoolT, ConstT, IntT}
-import at.forsyte.apalache.tla.lir.OperEx
+import at.forsyte.apalache.tla.bmcmt.types._
 import at.forsyte.apalache.tla.lir.control.TlaControlOper
 import at.forsyte.apalache.tla.lir.convenience._
-
+import at.forsyte.apalache.tla.lir.{OperEx, TlaEx}
 
 /**
   * Implements the rules: SE-ITE[1-5].
@@ -29,28 +28,62 @@ class IfThenElseRule(rewriter: SymbStateRewriter) extends RewritingRule {
         val elseState = rewriter.rewriteUntilDone(thenState.setTheory(CellTheory()).setRex(elseEx))
         val thenCell = elseState.arena.findCellByNameEx(thenState.ex)
         val elseCell = elseState.arena.findCellByNameEx(elseState.ex)
+        val commonType = thenCell.cellType.unify(elseCell.cellType)
+        if (commonType.isEmpty) {
+          throw new RewriterException("Failed to unify %s and %s in IF-THEN-ELSE"
+            .format(thenCell.cellType, elseCell.cellType))
+        }
         val pred = predState.ex
         (thenCell.cellType, elseCell.cellType) match {
           case (BoolT(), BoolT()) | (IntT(), IntT()) | (ConstT(), ConstT()) =>
-            val newArena = elseState.arena.appendCell(thenCell.cellType)
-            val newCell = newArena.topCell
-            // it's OK to use the SMT equality, as we are dealing with the basic types here
-            val ifCond = tla.and(pred, tla.eql(newCell, thenCell))
-            val elseCond = tla.and(tla.not(pred), tla.eql(newCell, elseCell))
-            rewriter.solverContext.assertGroundExpr(tla.or(ifCond, elseCond))
-            val finalState = elseState
-                .setArena(newArena)
-                .setRex(newCell.toNameEx)
-            rewriter.coerce(finalState, state.theory)
+            val finalState = iteBasic(elseState, commonType.get, pred, thenCell, elseCell)
+            rewriter.coerce(finalState, state.theory) // coerce to the source theory
+
+          case (FinSetT(_), FinSetT(_)) =>
+            val finalState = iteSet(elseState, commonType.get, pred, thenCell, elseCell)
+            rewriter.coerce(finalState, state.theory) // coerce to the source theory
 
           case _ =>
             throw new RewriterException("Attempting to apply IF-THEN-ELSE to %s and %s, not supported"
               .format(thenCell.cellType, elseCell.cellType))
         }
 
-
       case _ =>
         throw new RewriterException("%s is not applicable".format(getClass.getSimpleName))
     }
+  }
+
+  private def iteBasic(state: SymbState, commonType: CellT, pred: TlaEx, thenCell: ArenaCell, elseCell: ArenaCell) = {
+    val newArena = state.arena.appendCell(commonType)
+    val newCell = newArena.topCell
+    // it's OK to use the SMT equality, as we are dealing with the basic types here
+    val ifCond = tla.and(pred, tla.eql(newCell, thenCell))
+    val elseCond = tla.and(tla.not(pred), tla.eql(newCell, elseCell))
+    rewriter.solverContext.assertGroundExpr(tla.or(ifCond, elseCond))
+    state.setArena(newArena).setRex(newCell.toNameEx)
+  }
+
+  // TODO: introduce this rule in the semantics report
+  private def iteSet(state: SymbState, commonType: CellT, pred: TlaEx, thenCell: ArenaCell, elseCell: ArenaCell) = {
+    val newArena = state.arena.appendCell(commonType)
+    val newSetCell = newArena.topCell
+    // make a union and introduce conditional membership
+    val ifElems = state.arena.getHas(thenCell)
+    val thenElems = state.arena.getHas(elseCell)
+    val newArena2 = (ifElems ++ thenElems).foldLeft(newArena)((a, e) => a.appendHas(newSetCell, e))
+    // x \in NewSet <=> p /\ x \in S1 \/ ~p /\ x \in S2
+    def addCellCons(elemCell: ArenaCell): Unit = {
+      val inUnion = tla.in(elemCell, newSetCell)
+      val inLeftSet = tla.in(elemCell, thenCell)
+      val inRightSet = tla.in(elemCell, elseCell)
+      val inLeftOrRight = tla.or(tla.and(pred, inLeftSet), tla.and(tla.not(pred), inRightSet))
+      rewriter.solverContext.assertGroundExpr(tla.eql(inUnion, inLeftOrRight))
+    }
+
+    // add SMT constraints
+    for (cell <- ifElems ++ thenElems)
+      addCellCons(cell)
+
+    state.setTheory(CellTheory()).setArena(newArena2).setRex(newSetCell.toNameEx)
   }
 }
