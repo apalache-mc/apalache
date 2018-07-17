089f2a128a9a83765b8ee0bb2527bbdbebf49526
Kukovec <jkukovec@forsyte.at>
2018-07-17 12:18:25 +0200

    AlphaTLAP classes

---
+++ tla-assignments/src/main/scala/at/forsyte/apalache/tla/assignments/AlphaTLAP.scala
@@ -0,0 +1,60 @@
+package at.forsyte.apalache.tla.assignments
+
+import at.forsyte.apalache.tla.lir._
+import at.forsyte.apalache.tla.lir.actions.TlaActionOper
+import at.forsyte.apalache.tla.lir.control.TlaControlOper
+import at.forsyte.apalache.tla.lir.oper.{TlaBoolOper, TlaOper, TlaSetOper}
+import at.forsyte.apalache.tla.lir.values.TlaFalse
+
+abstract class AlphaEx( ex : TlaEx ) {
+  val id : UID = ex.ID
+}
+
+sealed case class Star( ex : TlaEx ) extends AlphaEx( ex ) {
+  val deps : Set[Var] = {
+    SpecHandler.bottomUpVal[Set[Var]](
+      ex,
+      _ match {
+        case OperEx( TlaActionOper.prime, NameEx( _ ) ) => true
+        case _ => false
+      },
+      _ match {
+        case OperEx( TlaActionOper.prime, NameEx( n ) ) => Set( n )
+        case _ => Set.empty[Var]
+      },
+      ( _, s ) => s.fold( Set.empty[Var] ) {_ ++ _},
+      Set.empty[Var]
+    )
+    Set.empty[Var] // TODO
+  }
+}
+
+sealed case class FalseEx( ex : TlaEx ) extends AlphaEx( ex )
+
+sealed case class AsgnEx( ex : OperEx, v : Var, rhs : Star ) extends AlphaEx( ex )
+
+sealed case class AndOr( ex : OperEx, args : AlphaEx* ) extends AlphaEx( ex ) {
+  val oper : TlaOper = ex.oper
+}
+
+sealed case class Exists( ex : OperEx, boundVar: Star, set : Star, body : AlphaEx ) extends AlphaEx( ex )
+
+sealed case class ITE( ex : OperEx, IF : Star, THEN : AlphaEx, ELSE : AlphaEx ) extends AlphaEx( ex )
+
+object AlphaTransform {
+  def apply( ex : TlaEx ) : AlphaEx = ex match {
+    case e@OperEx( TlaBoolOper.and | TlaBoolOper.or, args@_* ) =>
+      AndOr( e, args map {
+        AlphaTransform( _ )
+      } : _* )
+    case e@OperEx( TlaControlOper.ifThenElse, ifArgs, thenArgs, elseArgs ) =>
+      ITE( e, Star( ifArgs ), AlphaTransform( thenArgs ), AlphaTransform( elseArgs ) )
+    case e@OperEx( TlaBoolOper.exists, x, set, body ) =>
+      Exists( e, Star(x), Star( set ), AlphaTransform( body ) )
+    case e@OperEx( TlaSetOper.in, OperEx( TlaActionOper.prime, NameEx( v ) ), rhs ) =>
+      AsgnEx( e, v, Star( rhs ) )
+    case ValEx( TlaFalse ) => FalseEx( ex )
+    case _ => Star( ex )
+  }
+}
+
---
+++ tla-assignments/src/test/scala/at/forsyte/apalache/tla/assignments/TestAlphaTransform.scala
@@ -0,0 +1,151 @@
+package at.forsyte.apalache.tla.assignments
+
+import at.forsyte.apalache.tla.lir._
+import at.forsyte.apalache.tla.lir.convenience._
+import at.forsyte.apalache.tla.imp._
+import org.junit.runner.RunWith
+import org.scalatest.FunSuite
+import org.scalatest.junit.JUnitRunner
+
+@RunWith( classOf[JUnitRunner] )
+class TestAlphaTransform extends FunSuite with TestingPredefs {
+  val testFolderPath = "src/test/resources/assignmentSolver/"
+
+  def specFromFile(p_file : String, p_next : String = "Next") : TlaEx = {
+    val declsRaw = declarationsFromFile( testFolderPath + p_file )
+
+    val declsRenamed = OperatorHandler.uniqueVarRename( declsRaw )
+
+    val transformer = new Transformer()
+
+    /** Make all LET-IN calls explicit, to move towards alpha-TLA+ */
+    val decls = declsRenamed.map(
+      {
+        case TlaOperDecl( name, params, body ) =>
+          TlaOperDecl( name, params, transformer.explicitLetIn( body )( DummySrcDB ) )
+        case e@_ => e
+      }
+    )
+
+    /** Extract transition relation */
+    val nextBody = findBodyOf( p_next, decls : _* )
+
+    /** If extraction failed, throw */
+    //    assert( !nextBody.isNull )
+    if ( nextBody.isNull )
+      throw new AssignmentException(
+        "%s not found or not an operator".format( p_next )
+      )
+
+    /** Sanity check */
+    assert( nextBody.ID.valid )
+    //      throw new AssignmentException(
+    //        "%s has an invalid ID".format( p_nextName )
+    //      )
+
+    /** Preprocess body (inline operators, replace UNCHANGED, turn equality to set membership, etc.) */
+    val cleaned = transformer( nextBody, decls : _* )( DummyBodyDB, DummySrcDB )
+
+    /** Sanity check */
+    assert( cleaned.isDefined && cleaned.get.ID.valid )
+    //      throw new AssignmentException(
+    //        "%s could not be sanitized".format( p_nextName )
+    //      )
+
+    cleaned.get
+  }
+
+  test( "Star abstraction" ) {
+
+    val ex1 = trueEx
+    val ex2 : TlaEx = 5
+    val ex3 : TlaEx = x_in_S
+    val ex4 : TlaEx = tla.choose( n_x, n_S, n_p )
+    val ex5 : TlaEx = tla.caseOther( n_c, n_p, n_a, n_q, n_b )
+    val ex6 : TlaEx = tla.card( n_S )
+
+    val exs = List( ex1, ex2, ex3, ex4, ex5, ex6 )
+
+    assert( exs forall { ex =>
+      AlphaTransform( ex ) == Star( ex )
+    } )
+
+  }
+
+  test( "No abstraction" ) {
+
+    val ex1 = falseEx
+    val ex2 : TlaEx = tla.primeInSingleton( n_x, 1 )
+    val ex3 : TlaEx = tla.ite( n_p, n_a, n_b )
+    val ex4 : TlaEx = tla.or( ex1, ex2, ex3 )
+    val ex5 : TlaEx = tla.and( ex1, ex2, ex3 )
+
+    val exs = List( ex1, ex2, ex3, ex4, ex5 )
+
+    assert( exs forall { ex =>
+      !AlphaTransform( ex ).isInstanceOf[Star]
+    } )
+
+  }
+
+
+  def correctRecursiveApplication( exs: Traversable[TlaEx] ): Boolean = {
+    val trs = ( exs map { ex => ex -> AlphaTransform( ex ) } ).toMap
+    def argMatch( exargs: Traversable[TlaEx], args: Traversable[AlphaEx] ): Boolean =
+      ( exargs map { arg => trs.getOrElse(arg, AlphaTransform(arg)) } ) == args
+
+    trs forall { case (ex@OperEx( _, _* ), tr) => tr match {
+      case AndOr( OperEx( _, exargs@_* ), args@_* ) => argMatch( exargs, args )
+      case ITE( OperEx( _, exargs@_* ), i, t, e ) => argMatch( exargs, Seq(i,t,e) )
+      case Exists( OperEx( _, exargs@_* ), x, s, b ) => argMatch( exargs, Seq(x,s,b) )
+      case AsgnEx( OperEx( _, exargs@_* ), v, r ) => argMatch( exargs.tail, Seq(r)) // skip variable
+      case Star( e ) => e == ex
+      case _ => false
+    }
+    case (ex, tr) => tr == trs(ex)
+    }
+  }
+
+  test( "And/Or nesting" ) {
+
+    val ex1 = trueEx
+    val ex2 = falseEx
+    val ex3 = tla.and( ex1, ex2 )
+    val ex4 = tla.or( ex1, ex2 )
+
+    val ex5 = tla.and( ex4, ex3 )
+    val ex6 = tla.or( ex5, ex4, ex3, ex2, ex1 )
+
+    val exs : List[TlaEx] = List( ex1, ex2, ex3, ex4, ex5, ex6 )
+
+    assert( correctRecursiveApplication( exs ) )
+  }
+
+  test( "QuantAlt" ) {
+    val ex1 = tla.forall( n_x, n_S, tla.exists( n_y, n_T, tla.eql( 1, 2 ) ) )
+    val ex2 = tla.exists( n_x, n_S, tla.forall( n_y, n_T, tla.eql( 1, 2 ) ) )
+
+    assert( correctRecursiveApplication( Seq(ex1, ex2) ) )
+  }
+
+  test( "ITE nesting" ) {
+    val ex1 = tla.and( trueEx, falseEx, trueEx, trueEx, falseEx)
+    val ex2 = tla.exists( n_x, n_S, tla.primeInSingleton( n_t, n_x ) )
+    val ex3 = tla.ite( n_p, ex1, ex2 )
+
+    assert( correctRecursiveApplication( Seq(ex1, ex2, ex3) ) )
+  }
+
+  test( "Assignments" ) {
+    val ex1 = tla.primeInSingleton(n_x, 1)
+    val ex2 = tla.primeInSingleton( n_x, tla.primeInSingleton(n_x, 2) )
+
+    assert( correctRecursiveApplication( Seq(ex1, ex2) ) )
+  }
+
+  test( "Real spec" ) {
+    val spec = specFromFile( "Paxos.tla" )
+
+    assert( correctRecursiveApplication( Seq(spec) ) )
+  }
+}
