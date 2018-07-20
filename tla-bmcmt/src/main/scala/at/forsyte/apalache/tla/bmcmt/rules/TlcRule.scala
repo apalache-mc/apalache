40caa60548a87fbde040a0d84c7a112f8b5f262f
Igor Konnov <konnov@forsyte.at>
2018-07-20 20:25:48 +0200

    TLC!Assert should return TRUE

--- tla-bmcmt/src/main/scala/at/forsyte/apalache/tla/bmcmt/rules/TlcRule.scala
+++ tla-bmcmt/src/main/scala/at/forsyte/apalache/tla/bmcmt/rules/TlcRule.scala
@@ -1,7 +1,7 @@
 package at.forsyte.apalache.tla.bmcmt.rules
 
 import at.forsyte.apalache.tla.bmcmt._
-import at.forsyte.apalache.tla.bmcmt.types.{FailPredT, UnknownT}
+import at.forsyte.apalache.tla.bmcmt.types.FailPredT
 import at.forsyte.apalache.tla.lir.convenience.tla
 import at.forsyte.apalache.tla.lir.oper.TlcOper
 import at.forsyte.apalache.tla.lir.values.TlaStr
@@ -33,11 +33,8 @@ class TlcRule(rewriter: SymbStateRewriter) extends RewritingRule {
 
       case OperEx(TlcOper.assert, value, ValEx(TlaStr(message))) =>
         val valueState = rewriter.rewriteUntilDone(state.setRex(value).setTheory(BoolTheory()))
-        // introduce an unknown value for the outcome of assert, since this value may be unified in IF-THEN-ELSE
-        var arena = valueState.arena.appendCell(UnknownT())
-        val result = arena.topCell
         // introduce a new failure predicate
-        arena = arena.appendCell(FailPredT())
+        var arena = state.arena.appendCell(FailPredT())
         val failPred = arena.topCell
         rewriter.addMessage(failPred.id, "Assertion error: " + message)
         val assertion = valueState.ex
@@ -46,7 +43,7 @@ class TlcRule(rewriter: SymbStateRewriter) extends RewritingRule {
         // return isReachable. If there is a model M s.t. M |= isReachable, then M |= failPred allows us
         // to check, whether the assertion is violated or not
         val finalState = valueState.setArena(arena)
-          .setRex(result.toNameEx)
+          .setRex(tla.bool(true)) // if you need a value of a type different from bool, use TypedAssert
           .setTheory(BoolTheory())
         rewriter.coerce(finalState, state.theory)
 
