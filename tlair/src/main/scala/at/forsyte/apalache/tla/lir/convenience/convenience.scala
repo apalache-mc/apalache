4d3f280e7f04ad104ff9a7580d6b56189dfed541
Kukovec <jkukovec@forsyte.at>
2018-07-25 11:57:39 +0200

    .

--- tlair/src/main/scala/at/forsyte/apalache/tla/lir/convenience/convenience.scala
+++ tlair/src/main/scala/at/forsyte/apalache/tla/lir/convenience/convenience.scala
@@ -1,5 +1,7 @@
 package at.forsyte.apalache.tla.lir
 
+import at.forsyte.apalache.tla.lir.oper.{FixedArity, OperArity}
+
 /**
   * Convenient shortcuts and definitions. Import them, when needed.
   */
@@ -7,9 +9,11 @@ package object convenience {
   /**
     * This is just a short-hand to Builder, so one can type more naturally, e.g., tla.plus(tla.int(2), tla.int(3))
     */
-  val tla: Builder.type = Builder
+  val tla : Builder.type = Builder
 
-  implicit def makeUID (id : Int) : UID = UID(id)
-  implicit def makeEID (id : Int) : EID = EID(id)
+  implicit def makeUID( id : Int ) : UID = UID( id )
 
+  implicit def makeEID( id : Int ) : EID = EID( id )
+
+  implicit def opArity( n : Int ) : OperArity = FixedArity( n )
 }
--- tlair/src/test/scala/at/forsyte/apalache/tla/lir/TestEnvHandler.scala
+++ tlair/src/test/scala/at/forsyte/apalache/tla/lir/TestEnvHandler.scala
@@ -1,6 +1,9 @@
 package at.forsyte.apalache.tla.lir
 
+
+import at.forsyte.apalache.tla.lir._
 import at.forsyte.apalache.tla.lir.convenience._
+import at.forsyte.apalache.tla.lir.oper.TlaOper
 import org.junit.runner.RunWith
 import org.scalatest.FunSuite
 import org.scalatest.junit.JUnitRunner
@@ -82,7 +85,97 @@ class TestEnvHandler extends FunSuite with TestingPredefs {
 
     val replEx = eh.replaceAll( ex, toReplace, replaceWith )
 
-    println(replEx)
+    assert( eh.fullyIdentified( ex ) && !eh.fullyIdentified( replEx ) )
+
+  }
+
+  test( "Unfold 1x" ) {
+    val eh = newEh
+
+    val opName1 = "X"
+    val opName2 = "Y"
+    val opName3 = "Z"
+
+    val paramName1 = "a"
+    val paramName2 = "b"
+
+    val decl1 = tla.declOp( opName1, 1 )
+    val decl2 = tla.declOp( opName2, tla.plus( paramName1, 2 ), paramName1 )
+    val decl3 = tla.declOp( opName3, tla.eql( paramName1, paramName2 ), paramName1, paramName2 )
+
+
+    eh.extract( decl1 )
+    eh.extract( decl2 )
+    eh.extract( decl3 )
+
+    eh.m_bodyDB.print()
+
+    val appEx =  tla.appOp( opName2, 3 )
+    val uo = new TlaUserOper( opName3, decl3.formalParams.size, decl3 )
+    val uopEx = OperEx( uo, 42, 103 )
+
+    val ex1 = tla.plus( opName1, 2 )
+    val ex2 = tla.and( trueEx, appEx )
+    val ex3 = tla.exists( n_x, n_S,  uopEx )
+    val ex4 = tla.appOp( opName2, tla.appOp(opName2, 1 ) )
+
+//    println( ex4 )
+
+    //    eh.m_bodyDB.print( )
+
+
+    val unfolded1 = eh.unfoldOnce( ex1 )
+    val unfolded2 = eh.unfoldOnce( ex2 )
+    val unfolded3 = eh.unfoldOnce( ex3 )
+    val unfolded4 = eh.unfoldOnce( ex4 )
+
+//    println( unfolded1 )
+//    println( unfolded2 )
+//    println( unfolded3 )
+//    println( unfolded4 )
+
+    assert( unfolded1 === ex1 ) // Since the operator application appears incorrectly as a NameEx
+
+//    def isNotOper( ex : TlaEx ) : Boolean = ex != NameEx( opName1 )
+
+//    assert( RecursiveProcessor.globalTlaExProperty( isNotOper )( unfolded1 ) )
+
+
+  }
+
+  test("Unfold max") {
+    val eh = newEh
+
+    val opName1 = "X"
+    val opName2 = "Y"
+
+    val paramName = "a"
+
+    val decl1 = tla.declOp( opName1, 1 )
+    val decl2 = tla.declOp( opName2, tla.plus( paramName, tla.appOp(opName1) ), paramName )
+
+    eh.extract( decl1, decl2 )
+    val ex = tla.appOp( opName2, 0 )
+
+    eh identify ex
+
+
+    val unfolded1 = eh.unfoldOnce( ex )
+    val unfolded2 = eh.unfoldOnce( unfolded1 )
+    val unfoldedMax = eh.unfoldMax( ex )
+
+    assert(
+      ( ex !== unfolded1 ) &&
+      ( unfolded1 !== unfolded2 ) &&
+      ( unfolded2 === unfoldedMax )
+    )
+
+    def notOpers(ops: String*)( ex : TlaEx ) : Boolean = !ops.map( NameEx ).contains( ex )
+
+    assert( RecursiveProcessor.globalTlaExProperty( notOpers(opName2) )(unfolded1) )
+    assert( RecursiveProcessor.globalTlaExProperty( notOpers(opName1, opName2) )(unfolded2) )
+
+
 
 
   }
