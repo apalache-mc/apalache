c14f38597109f07b8ebe8940775d9da7cc2ad7b2
Igor Konnov <igor@interchain.io>
2019-11-02 19:51:39 +0100

    simplified SimplifyRecordAccess: one recursive function, no excessive lambdas

--- tlair/src/main/scala/at/forsyte/apalache/tla/lir/transformations/standard/SimplifyRecordAccess.scala
+++ tlair/src/main/scala/at/forsyte/apalache/tla/lir/transformations/standard/SimplifyRecordAccess.scala
@@ -5,45 +5,56 @@ import at.forsyte.apalache.tla.lir.oper.TlaFunOper
 import at.forsyte.apalache.tla.lir.transformations.{TlaExTransformation, TransformationTracker}
 import at.forsyte.apalache.tla.lir.values.TlaStr
 
-object SimplifyRecordAccess {
-  private def simplifyLeaf( tracker : TransformationTracker ) : TlaExTransformation = tracker.track {
-    case OperEx(
-    TlaFunOper.app,
-    OperEx( TlaFunOper.enum, args@_* ),
-    ValEx( TlaStr( s ) )
-    ) =>
-      val recFields = ( args.grouped( 2 ).toSeq map {
-        case Seq( ValEx( TlaStr( k ) ), v ) => k -> v
-      } ).toMap
+/**
+  * A simplifier that replaces record access to an explicitly constructed record with the known value,
+  * e.g., {a |-> 1, b |-> 2}.b is rewritten into 2.
+  *
+  * TODO: this transformation should be part of a more general constant simplifier, see how it is done in Keramelizer.
+  *
+  * @param tracker a transformation tracker
+  *
+  * @author Jure Kukovec, Igor Konnov
+  */
+class SimplifyRecordAccess(tracker: TransformationTracker) extends TlaExTransformation {
+  override def apply(ex: TlaEx) = transform(ex)
 
-      recFields.getOrElse(
-        s,
-        throw new IllegalArgumentException( "Attempting to access nonexistent record field" )
-      )
+  def transform: TlaExTransformation = tracker.track {
+    // main case
+    case OperEx(TlaFunOper.app,
+                OperEx(TlaFunOper.enum, ctorArgs @ _*),
+                ValEx(TlaStr(accessedKey))) =>
+      val rewrittenArgs = ctorArgs map transform
+      val found = rewrittenArgs.grouped(2).find {
+        case Seq(ValEx(TlaStr(key)), _) => key == accessedKey
+      }
+      found match {
+        case Some(pair) => pair(1) // get the value
+        case _ => throw new IllegalArgumentException(s"Access to non-existent record field $accessedKey")
+      }
+
+    // standard recursive processing for operators and let-in definitions
+    case ex @ LetInEx(body, defs @ _*) =>
+      // Transform bodies of all op.defs
+      val newDefs = defs.map { x =>
+        x.copy(body = transform(x.body))
+      }
+      val newBody = transform(body)
+      if (defs == newDefs && body == newBody) {
+        ex
+      } else {
+        LetInEx(newBody, newDefs: _*)
+      }
+
+    case ex@OperEx( op, args@_* ) =>
+      val newArgs = args map transform
+      if ( args == newArgs ) ex else OperEx( op, newArgs : _* )
+
     case ex => ex
   }
+}
 
-  def apply( tracker : TransformationTracker ) : TlaExTransformation = tracker.track { ex =>
-    val tr = simplifyLeaf( tracker )
-    lazy val self = apply( tracker )
-    ex match {
-      case LetInEx( body, defs@_* ) =>
-        // Transform bodies of all op.defs
-        val newDefs = defs.map { x =>
-          x.copy(
-            body = self( x.body )
-          )
-        }
-        val newBody = self( body )
-        val retEx = if ( defs == newDefs && body == newBody ) ex else LetInEx( newBody, newDefs : _* )
-        tr( retEx )
-
-      case ex@OperEx( op, args@_* ) =>
-        val newArgs = args map self
-        val retEx = if ( args == newArgs ) ex else OperEx( op, newArgs : _* )
-        tr( retEx )
-
-      case _ => tr( ex )
-    }
+object SimplifyRecordAccess {
+  def apply(tracker: TransformationTracker): SimplifyRecordAccess = {
+    new SimplifyRecordAccess(tracker)
   }
 }
