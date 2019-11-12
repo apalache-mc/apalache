c12c500decb70299b95d76af25bd61193135bee3
Kukovec <jkukovec@forsyte.at>
2019-11-12 11:17:10 +0100

    Typedefs and string constants

--- tla-types/src/main/scala/at/forsyte/apalache/tla/types/package.scala
+++ tla-types/src/main/scala/at/forsyte/apalache/tla/types/package.scala
@@ -1,6 +1,44 @@
 package at.forsyte.apalache.tla
 
+import at.forsyte.apalache.tla.lir.UID
+import at.forsyte.apalache.tla.lir.smt.SmtTools.BoolFormula
+
+/**
+  * Typedefs used in this package
+  */
 package object types {
-  type typeContext = Map[TypeVar, SmtTypeVariable]
-  type nameContext = Map[String, SmtTypeVariable]
+  type TypeContext = Map[TypeVar, SmtTypeVariable]
+  type NameContext = Map[String, SmtDatatype]
+  type Template = Seq[SmtDatatype] => BoolFormula
+  type TypeMap[kType] = Map[kType, TlaType]
+  type TypeVarAssignment = Map[UID, SmtDatatype]
+  type OperatorApplicationStack = List[UID]
+  // Because out type-analysis assigns types on a per-call-site basis, we cannot simply
+  // assign a type to every UID, because different calls to a polymorphic operator might
+  // generate mutually exclusive types to the same UIDs appearing in the body of said operator
+  type OperatorContext = Map[OperatorApplicationStack, TypeVarAssignment]
+
+  object Names {
+    val intVarSymb : String = "i"
+    val tVarSymb   : String = "f"
+
+    val atIndexName: String = "atIndex"
+    val atFieldName: String = "atField"
+    val hasIndexName : String = "hasIndex"
+    val hasFieldName : String = "hasField"
+    val sizeOfName   : String = "sizeOf"
+
+    val dtName    : String = "tlaT"
+    val intTName  : String = "intT"
+    val strTName  : String = "strT"
+    val boolTName : String = "boolT"
+    val setTName  : String = "setT"
+    val seqTName  : String = "seqT"
+    val funTName  : String = "funT"
+    val operTName : String = "operT"
+    val tupTName  : String = "tupT"
+    val recTName  : String = "recT"
+    val varTName  : String = "varT"
+  }
+
 }
