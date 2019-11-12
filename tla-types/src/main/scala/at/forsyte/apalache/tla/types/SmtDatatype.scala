e8332d0708de1bd7bff4051f8b4a850ad7402572
Kukovec <jkukovec@forsyte.at>
2019-11-12 11:17:40 +0100

    Smt datatype

--- tla-types/src/main/scala/at/forsyte/apalache/tla/types/SmtDatatype.scala
+++ tla-types/src/main/scala/at/forsyte/apalache/tla/types/SmtDatatype.scala
@@ -2,11 +2,27 @@ package at.forsyte.apalache.tla.types
 
 import at.forsyte.apalache.tla.lir.smt.SmtTools
 
+/**
+  * SmtDatatype represents an intermediate layer of abstraction between the TlaType
+  * and the eventual solver-dependent SMT datatype implementation
+  */
 abstract class SmtDatatype
 
-sealed case class SmtIntVariable( id: Long )
-sealed case class SmtTypeVariable( id: Long ) extends SmtDatatype
+/**
+  * SmtVariable is either an integer of represents a TlaType
+  */
+trait SmtVariable
 
+sealed case class SmtIntVariable( id: Int ) extends SmtVariable {
+  override def toString : String = s"${Names.intVarSymb}($id)"
+}
+sealed case class SmtTypeVariable( id: Int ) extends SmtDatatype with SmtVariable
+
+/**
+  * See paper for a discussion on the datatype.
+  *
+  * We don't need a representation of varT, since no signature-based constraints define it
+  */
 object int extends SmtDatatype
 object str extends SmtDatatype
 object bool extends SmtDatatype
@@ -17,8 +33,16 @@ sealed case class rec( r: SmtIntVariable ) extends SmtDatatype
 sealed case class fun( arg: SmtDatatype, res: SmtDatatype ) extends SmtDatatype
 sealed case class oper( oarg: SmtDatatype, ores: SmtDatatype ) extends SmtDatatype
 
-sealed case class Eql( lhs: SmtDatatype, rhs: SmtDatatype  ) extends SmtTools.BoolFormula
+/**
+  * We include abstractions of the functions used in the constraints
+  *
+  * Note that e.g. hasField(a,b,c) gets translated to [hasField(a,b) && atField(a,b) = c] at
+  * the SMT level, to allow for easier solution recovery from the SMT models
+  */
 sealed case class hasField( v: SmtIntVariable, s: String, t: SmtDatatype ) extends SmtTools.BoolFormula
 sealed case class hasIndex( v: SmtIntVariable, i: Int, t: SmtDatatype  ) extends SmtTools.BoolFormula
 // sizeOf(i) = j
 sealed case class sizeOfEql( i: SmtIntVariable, j: Int ) extends SmtTools.BoolFormula
+
+// Eql was not defined in SmtTools.BoolFormula
+sealed case class Eql( lhs: SmtDatatype, rhs: SmtDatatype  ) extends SmtTools.BoolFormula
