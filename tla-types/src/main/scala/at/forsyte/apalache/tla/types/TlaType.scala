42c7ae0115b386b9eb40945ac6dcb4d0c2fdd18e
Kukovec <jkukovec@forsyte.at>
2019-11-12 11:16:20 +0100

    Type class + moved SMTVarGen

--- tla-types/src/main/scala/at/forsyte/apalache/tla/types/TlaType.scala
+++ tla-types/src/main/scala/at/forsyte/apalache/tla/types/TlaType.scala
@@ -1,5 +1,11 @@
 package at.forsyte.apalache.tla.types
 
+/**
+  * This class represents the types introduced in our paper.
+  *
+  * We do not enforce the exact grammar through class hierarchies, but defer the responsibility
+  * of defining permissible types to the user.
+  */
 abstract class TlaType
 
 sealed case class TypeVar( i : Int ) extends TlaType
@@ -11,8 +17,7 @@ sealed case class FunT( domT : TlaType, cdmT : TlaType ) extends TlaType
 sealed case class SetT( t : TlaType ) extends TlaType
 sealed case class SeqT( t : TlaType ) extends TlaType
 sealed case class TupT( ts: TlaType* ) extends TlaType
-sealed case class KvPair[kType]( k: kType, v: TlaType )
-sealed case class RecT( ts: KvPair[String]* ) extends TlaType
-sealed case class SparseTupT( ts: KvPair[Int]* ) extends TlaType
+sealed case class RecT( tmap: TypeMap[String] ) extends TlaType
+sealed case class SparseTupT( tmap: TypeMap[Int] ) extends TlaType
 sealed case class OperT( domT : TupT, cdmT : TlaType ) extends TlaType
-sealed case class PolyOperT( tVars: List[TypeVar], op: OperT )
+sealed case class PolyOperT( tVars: List[TypeVar], op: OperT ) extends TlaType
---
+++ tla-types/src/main/scala/at/forsyte/apalache/tla/types/smt/SmtVarGenerator.scala
@@ -0,0 +1,33 @@
+package at.forsyte.apalache.tla.types
+
+/**
+  * Generator for fresh SMT variables, of the TlaType datatype or integer variety.
+  */
+class SmtVarGenerator {
+  private var idx_t : Int = 0
+  private var idx_i : Int = 0
+
+  def getFresh : SmtTypeVariable = {
+    val ret = SmtTypeVariable( idx_t )
+    idx_t += 1
+    ret
+  }
+
+  def getNFresh( n : Int ) : List[SmtTypeVariable] = List.fill( n ) {
+    getFresh
+  }
+
+  def getFreshInt : SmtIntVariable = {
+    val ret = SmtIntVariable( idx_i )
+    idx_i += 1
+    ret
+  }
+
+  def getNFreshInt( n : Int ) : List[SmtIntVariable] = List.fill( n ) {
+    getFreshInt
+  }
+
+  def allVars : Seq[SmtVariable] =
+    ( 0 until idx_i map SmtIntVariable ) ++ ( 0 until idx_t map SmtTypeVariable )
+
+}
