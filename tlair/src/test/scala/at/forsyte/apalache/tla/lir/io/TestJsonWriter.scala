ef2873126213dc5f531336356830ead78bbdaa02
Andrey Kuprianov <andrey@informal.systems>
2020-02-04 11:30:21 +0100

    JsonWriter and TestJsonWriter: enum sets and tuple
    - progressing with (Test)JsonWriter: add json formatting for enum-sets and tuple
    - simplified and restructured TestJsonWriter: allow single- and multi-line tests

--- tlair/src/test/scala/at/forsyte/apalache/tla/lir/io/TestJsonWriter.scala
+++ tlair/src/test/scala/at/forsyte/apalache/tla/lir/io/TestJsonWriter.scala
@@ -4,33 +4,33 @@ import java.io.{PrintWriter, StringWriter}
 
 import at.forsyte.apalache.tla.lir.convenience.tla._
 import at.forsyte.apalache.tla.lir.oper.TlaOper
-import at.forsyte.apalache.tla.lir.{OperEx, SimpleFormalParam, TlaEx, TlaOperDecl}
+import at.forsyte.apalache.tla.lir.predef.TlaRealSet
+import at.forsyte.apalache.tla.lir.{OperEx, SimpleFormalParam, TlaEx, TlaOperDecl, ValEx}
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.{BeforeAndAfterEach, FunSuite}
 
 @RunWith(classOf[JUnitRunner])
 class TestJsonWriter extends FunSuite with BeforeAndAfterEach {
-  private var stringWriter = new StringWriter()
-  private var printWriter = new PrintWriter(stringWriter)
-  private var writer = new JsonWriter(printWriter)
 
-  override protected def beforeEach(): Unit = {
-    stringWriter = new StringWriter()
-    printWriter = new PrintWriter(stringWriter)
-    writer = new JsonWriter(printWriter)
-  }
-
-  protected def compare(ex: TlaEx, expected: String): Unit = {
+  // compare expression and expected result (single-line formatting)
+  def compare(ex: TlaEx, expected: String, indent: Int = -1): Unit = {
+    val stringWriter = new StringWriter()
+    val printWriter = new PrintWriter(stringWriter)
+    val writer = new JsonWriter(printWriter, indent)
     writer.write(ex)
     printWriter.flush()
     assert(expected == stringWriter.toString)
   }
 
+  // compare expression and expected result (multi-line formatting)
+  def compareMultiLine(ex: TlaEx, expected: String): Unit =
+    compare(ex, expected, 2)
+
   test("name") {
     compare(
       name("awesome"),
-      """{"kind":"name","value":"awesome"}"""
+      """{"tla":"name","val":"awesome"}"""
     )
   }
 
@@ -44,7 +44,82 @@ class TestJsonWriter extends FunSuite with BeforeAndAfterEach {
   test("int") {
     compare(
       int(42),
-      """{"kind":"int","value":"42"}"""
+      """{"tla":"int","val":"42"}"""
+    )
+  }
+
+  test("RealSet") {
+    compare(
+      ValEx(TlaRealSet), // TODO: builders for sets? (Andrey)
+      """{"tla":"set","val":"Real"}"""
+    )
+  }
+
+  test("prime name") {
+    compare(
+      prime("awesome"),
+      """{"tla":"prime","val":{"tla":"name","val":"awesome"}}"""
+    )
+  }
+
+  test("empty set") {
+    compare(
+      enumSet(),
+      """{"tla":"set","val":null}"""
+    )
+  }
+
+  test("empty set multi-line") {
+    compareMultiLine(
+      enumSet(),
+      """{
+        |  "tla": "set",
+        |  "val": null
+        |}""".stripMargin
+    )
+  }
+
+  test("singleton set") {
+    compare(
+      enumSet(42),
+      """{"tla":"set","val":{"tla":"int","val":"42"}}"""
+    )
+  }
+
+  test("enum set") {
+    compare(
+      enumSet(int(1), int(2), int(3)),
+      """{"tla":"set","val":[{"tla":"int","val":"1"},{"tla":"int","val":"2"},{"tla":"int","val":"3"}]}"""
+    )
+  }
+
+  test("enum set multi-line") {
+    compareMultiLine(
+      enumSet(int(1), int(2), int(3)),
+      """{
+        |  "tla": "set",
+        |  "val": [
+        |    {
+        |      "tla": "int",
+        |      "val": "1"
+        |    },
+        |    {
+        |      "tla": "int",
+        |      "val": "2"
+        |    },
+        |    {
+        |      "tla": "int",
+        |      "val": "3"
+        |    }
+        |  ]
+        |}""".stripMargin
+    )
+  }
+
+  test("tuple") {
+    compare(
+      tuple(int(1), int(2), int(3)),
+      """{"tla":"tuple","val":[{"tla":"int","val":"1"},{"tla":"int","val":"2"},{"tla":"int","val":"3"}]}"""
     )
   }
 }
