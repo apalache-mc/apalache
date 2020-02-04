2786ebce6fc7833ce699b2d9dcf2686440e40856
Andrey Kuprianov <andrey@informal.systems>
2020-02-04 15:35:22 +0100

    JsonWriter: refactored to generalize handling of different cases
    In order to reduce the code size, and the number of cases handled,
    refactored JsonWriter to use the same approach as in the PrettyWriter,
    with pattern-matching against operators in the map.
    
    It is unclear what names to employ for TLA operators in JSON encoding.
    Currently, the most reasonable approach seems to use ASCII names
    of TLA operators with several exceptions to avoid ambiguity.

--- tlair/src/main/scala/at/forsyte/apalache/tla/lir/io/JsonWriter.scala
+++ tlair/src/main/scala/at/forsyte/apalache/tla/lir/io/JsonWriter.scala
@@ -1,6 +1,6 @@
 package at.forsyte.apalache.tla.lir.io
 
-import java.io.PrintWriter
+import java.io.{File, FileWriter, PrintWriter}
 
 import at.forsyte.apalache.tla.lir._
 import at.forsyte.apalache.tla.lir.convenience._
@@ -8,6 +8,8 @@ import at.forsyte.apalache.tla.lir.oper.{TlaBoolOper, _}
 import at.forsyte.apalache.tla.lir.predef._
 import at.forsyte.apalache.tla.lir.values.{TlaBool, TlaInt, TlaStr}
 import ujson._
+
+import scala.collection.immutable.HashMap
 /**
  * <p>A formatter of TlaEx and TlaModule to JSON, for interoperability with external tools.</p>
  * @author Andrey Kuprianov
@@ -19,45 +21,120 @@ class JsonWriter(writer: PrintWriter, indent: Int = 2) {
     writer.write(ujson.write(toJson((0, 0), expr), indent))
   }
 
-  def obj(tla: String, value: ujson.Value = ujson.Null): ujson.Value = {
-    Obj("tla" -> tla, "val" -> value)
+  def unary(tla: String, arg: ujson.Value): ujson.Value = {
+    Obj("tla" -> tla, "arg" -> arg)
+  }
+
+  def binary(tla: String, arg1: ujson.Value, arg2: ujson.Value): ujson.Value = {
+    Obj("tla" -> tla, "arg1" -> arg1, "arg2" -> arg2)
+  }
+
+  def nary(tla: String, args: ujson.Value*): ujson.Value = {
+    Obj("tla" -> tla, "args" -> args)
   }
 
   def toJson(parentPrecedence: (Int, Int), expr: TlaEx): ujson.Value = {
     expr match {
-      case NameEx(x) => obj("name", Str(x))
+      case NameEx(x) => unary("name", Str(x))
 
       case ValEx(TlaStr(str)) => Str(str)
-      case ValEx(TlaInt(value)) => obj("int", Str(value.toString))
+      case ValEx(TlaInt(value)) => unary("int", Str(value.toString))
       case ValEx(TlaBool(b)) => if (b) True else False
-      case ValEx(TlaBoolSet) => obj("set", "BOOLEAN")
-      case ValEx(TlaIntSet) => obj("set", "Int")
-      case ValEx(TlaNatSet) => obj("set", "Nat")
-      case ValEx(TlaRealSet) => obj("set", "Real")
-      case ValEx(TlaStrSet) => obj("set", "STRING")
+      case ValEx(TlaBoolSet) => unary("set", "BOOLEAN")
+      case ValEx(TlaIntSet) => unary("set", "Int")
+      case ValEx(TlaNatSet) => unary("set", "Nat")
+      case ValEx(TlaRealSet) => unary("set", "Real")
+      case ValEx(TlaStrSet) => unary("set", "STRING")
 
-      case OperEx(op@TlaActionOper.prime, e) =>
-        obj("prime", toJson(op.precedence, e))
+//      case OperEx(op@TlaSetOper.enumSet, args@_*) =>
+//        // a set enumeration, e.g., { 1, 2, 3 }
+//        val argJsons = args.map(toJson(op.precedence, _))
+//        obj("set", argJsons)
+//
+//      case OperEx(op@TlaFunOper.tuple, args@_*) =>
+//        // a tuple, e.g., <<1, 2, 3>>
+//        val argJsons = args.map(toJson(op.precedence, _))
+//        obj("tuple", argJsons)
+//
+//      case OperEx(op@TlaFunOper.tuple, args@_*) =>
+//        // a tuple, e.g., <<1, 2, 3>>
+//        val argJsons = args.map(toJson(op.precedence, _))
+//        obj("tuple", argJsons)
 
-      case OperEx(TlaSetOper.enumSet) =>
-        // an empty set
-        obj("set", Arr())
+      case OperEx(op@_, arg) if JsonWriter.unaryOps.contains(op) =>
+        unary(JsonWriter.unaryOps(op), toJson(op.precedence, arg))
 
-      case OperEx(op@TlaSetOper.enumSet, arg) =>
-        // a singleton set
-        obj("set", Arr(toJson(op.precedence, arg)))
+      case OperEx(op@_, arg1, arg2) if JsonWriter.binaryOps.contains(op) =>
+        Obj(
+          "tla" -> JsonWriter.unaryOps(op),
+          "arg1" -> toJson(op.precedence, arg1),
+          "arg2" -> toJson(op.precedence, arg2)
+        )
 
-      case OperEx(op@TlaSetOper.enumSet, args@_*) =>
-        // a set enumeration, e.g., { 1, 2, 3 }
+      case OperEx(op@_, args@_*) if JsonWriter.naryOps.contains(op)  =>
         val argJsons = args.map(toJson(op.precedence, _))
-        obj("set", argJsons)
-
-      case OperEx(op@TlaFunOper.tuple, args@_*) =>
-        // a tuple, e.g., <<1, 2, 3>>
-        val argJsons = args.map(toJson(op.precedence, _))
-        obj("tuple", argJsons)
+        nary(JsonWriter.naryOps(op), argJsons)
 
       case _ => True
     }
   }
+}
+
+object JsonWriter {
+  protected val unaryOps = HashMap(
+    TlaActionOper.prime -> "prime", // TODO: should we use `'` or `prime` or something else?
+    TlaBoolOper.not -> "~",
+    TlaArithOper.uminus -> "-",
+    TlaSetOper.union -> "UNION",
+    TlaSetOper.powerset -> "SUBSET",
+    TlaActionOper.enabled -> "ENABLED",
+    TlaActionOper.unchanged -> "UNCHANGED",
+    TlaFunOper.domain -> "DOMAIN",
+    TlaTempOper.box -> "[]",
+    TlaTempOper.diamond -> "<>"
+  )
+
+  protected val binaryOps = HashMap(
+    TlaOper.eq -> "=",
+    TlaOper.ne -> "/=",
+    TlaBoolOper.implies -> "=>",
+    TlaBoolOper.equiv -> "<=>",
+    TlaArithOper.plus -> "+",
+    TlaArithOper.minus -> "-",
+    TlaArithOper.mult -> "*",
+    TlaArithOper.div -> "/",
+    TlaArithOper.mod -> "%",
+    TlaArithOper.realDiv -> "/.",
+    TlaArithOper.exp -> "^",
+    TlaArithOper.dotdot -> "..",
+    TlaArithOper.lt -> "<",
+    TlaArithOper.gt -> ">",
+    TlaArithOper.le -> "<=",
+    TlaArithOper.ge -> ">=",
+    TlaSetOper.in -> "\\in",
+    TlaSetOper.notin -> "\\notin",
+    TlaSetOper.cap -> "\\cap",
+    TlaSetOper.cup -> "\\cup",
+    TlaSetOper.setminus -> "\\",
+    TlaSetOper.subseteq -> "\\subseteq",
+    TlaSetOper.subsetProper -> "\\subset",
+    TlaSetOper.supseteq -> "\\supseteq",
+    TlaSetOper.supsetProper -> "\\supset",
+    TlaActionOper.composition -> "\\cdot",
+    TlaTempOper.leadsTo -> "~>",
+    TlaTempOper.guarantees -> "-+->",
+    TlaSeqOper.concat -> "\\o",
+    TlcOper.atat -> "@@",
+    TlcOper.colonGreater -> ":>",
+    BmcOper.assign -> "<-",
+    BmcOper.withType -> "<:"
+  )
+
+  protected val naryOps: Map[TlaOper, String] = HashMap(
+    TlaSetOper.enumSet -> "enum",
+    TlaFunOper.tuple -> "tuple",
+    TlaSetOper.times -> "\\X",
+    TlaArithOper.sum -> "+",
+    TlaArithOper.prod -> "*"
+  )
 }
