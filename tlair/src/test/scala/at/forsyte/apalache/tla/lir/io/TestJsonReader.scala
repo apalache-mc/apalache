ec014e39846d74848e3b3325b940364745b5883a
Andrey Kuprianov <andrey@informal.systems>
2020-05-15 15:34:31 +0200

    #121 TestJson: refactor JSON tests to use common testset for both reader and writer

--- tlair/src/test/scala/at/forsyte/apalache/tla/lir/io/TestJsonReader.scala
+++ tlair/src/test/scala/at/forsyte/apalache/tla/lir/io/TestJsonReader.scala
@@ -1,24 +1,11 @@
 package at.forsyte.apalache.tla.lir.io
 
-import at.forsyte.apalache.tla.lir.convenience.tla._
 import at.forsyte.apalache.tla.lir._
-import org.junit.runner.RunWith
-import org.scalatest.junit.JUnitRunner
-import org.scalatest.FunSuite
 
-@RunWith(classOf[JUnitRunner])
-class TestJsonReader extends FunSuite {
+class TestJsonReader extends TestJson {
 
-  // compare expression and expected result (single-line formatting)
-  def compare(expected: TlaEx, json: String): Unit = {
+  // check that after reading from json the result matches expr
+  def compare(expected: TlaEx, json: String, indent: Int = -1): Unit = {
     assert(JsonReader.read(json) == expected)
-  }
-
-  test("str") {
-    // "Hello, World!"
-    compare(
-      str("Hello, World!"),
-      """{"str":"Hello, World!"}"""
-    )
   }
 }
