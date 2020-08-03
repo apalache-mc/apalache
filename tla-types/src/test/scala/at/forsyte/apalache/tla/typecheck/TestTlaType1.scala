d35aba96c49edb9e687d299d991aa62a2150e2c1
Igor Konnov <igor@informal.systems>
2020-08-03 20:20:14 +0200

    case classes for typecheck.TlaType1, similar to types.TlaType

---
+++ tla-types/src/test/scala/at/forsyte/apalache/tla/typecheck/TestTlaType1.scala
@@ -0,0 +1,28 @@
+package at.forsyte.apalache.tla.typecheck
+
+import org.junit.runner.RunWith
+import org.scalatest.FunSuite
+import org.scalatest.junit.JUnitRunner
+
+import scala.collection.immutable.SortedMap
+
+@RunWith(classOf[JUnitRunner])
+class TestTlaType1  extends FunSuite {
+  test("TlaType1.toString") {
+    assert("Int" == IntT1().toString)
+    assert("Bool" == BoolT1().toString)
+    assert("Str" == StrT1().toString)
+    assert("PID" == ConstT1("PID").toString)
+    assert("a" == VarT1("a").toString)
+    assert("Set(a)" == SetT1(VarT1("a")).toString)
+    assert("Set(Int)" == SetT1(IntT1()).toString)
+    assert("Seq(a)" == SeqT1(VarT1("a")).toString)
+    assert("Seq(Int)" == SeqT1(IntT1()).toString)
+    assert("Int -> Bool" == FunT1(IntT1(), BoolT1()).toString)
+    assert("<<Int, Bool, Str>>" == TupT1(IntT1(), BoolT1(), StrT1()).toString)
+    val recType = RecT1(SortedMap("f1" -> IntT1(), "f2" -> BoolT1(), "f3" -> StrT1()))
+    assert("[f1: Int, f2: Bool, f3: Str]" == recType.toString)
+    val operType = OperT1(List(BoolT1(), StrT1()), IntT1())
+    assert("(Bool, Str) => Int" == operType.toString)
+  }
+}
