7eed4cce1f9f6d8d7d39a6e6c55a4bfeaf1ee1d6
Igor Konnov <konnov@forsyte.at>
2020-09-18 23:50:31 +0200

    added typecheck command

---
+++ tla-types/src/main/scala/at/forsyte/apalache/tla/typecheck/passes/EtcTypeCheckerPassImpl.scala
@@ -0,0 +1,45 @@
+package at.forsyte.apalache.tla.typecheck.passes
+
+import at.forsyte.apalache.infra.passes.{Pass, PassOptions, TlaModuleMixin}
+import at.forsyte.apalache.tla.imp.src.SourceStore
+import at.forsyte.apalache.tla.typecheck.TypeCheckerTool
+import com.google.inject.Inject
+import com.google.inject.name.Named
+import com.typesafe.scalalogging.LazyLogging
+
+class EtcTypeCheckerPassImpl @Inject()(val options: PassOptions,
+                                       val sourceStore: SourceStore,
+                                       @Named("AfterTypeChecker") val nextPass: Pass with TlaModuleMixin)
+    extends EtcTypeCheckerPass with LazyLogging {
+
+  /**
+    * The name of the pass
+    *
+    * @return the name associated with the pass
+    */
+  override def name: String = "TypeChecker"
+
+  /**
+    * Run the pass.
+    *
+    * @return true, if the pass was successful
+    */
+  override def execute(): Boolean = {
+    if (tlaModule.isDefined) {
+      logger.info(" > running Embarrassingly simple Type Checker (ETC)")
+      val isWellDefined = new TypeCheckerTool().check(new LoggingTypeCheckerListener(sourceStore), tlaModule.get)
+      isWellDefined
+    } else {
+      logger.info(" > no input for type checker")
+      false
+    }
+  }
+  /**
+    * Get the next pass in the chain. What is the next pass is up
+    * to the module configuration and the pass outcome.
+    *
+    * @return the next pass, if exists, or None otherwise
+    */
+  override def next(): Option[Pass] =
+    tlaModule map {_ => nextPass}
+}
