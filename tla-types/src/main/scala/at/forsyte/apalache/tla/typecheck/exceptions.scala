7175f5ae6e985f66c4838e0d57dd12ca9e91f4ef
Shon Feder <shon@informal.systems>
2020-10-20 15:41:13 -0400

    Add Etc Exceptions module
    Originally authored by Igor Konnov <konnov@forsyte.at>

---
+++ tla-types/src/main/scala/at/forsyte/apalache/tla/typecheck/exceptions.scala
@@ -0,0 +1,21 @@
+/*
+ We group all exceptions in a single file.
+
+ See the Scala style guide: http://docs.scala-lang.org/style/files.html
+ */
+
+package at.forsyte.apalache.tla.typecheck
+
+/**
+  * This exception is thrown, whenever the type checker finds an irrecoverable error.
+  *
+  * @author konnov
+  */
+class TypingException(message: String) extends Exception(message)
+
+/**
+  * This exception is thrown, whenever the type checker finds an irrecoverable error in the user input.
+  *
+  * @author konnov
+  */
+class TypingInputException(message: String) extends TypingException(message)
