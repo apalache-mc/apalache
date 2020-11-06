30fc08f9703a15cde3de3f2116888f6f7f1bf8f7
Shon Feder <shon@informal.systems>
2020-11-06 13:21:20 -0500

    Add TypeChecker module

---
+++ tla-types/src/main/scala/at/forsyte/apalache/tla/typecheck/TypeChecker.scala
@@ -0,0 +1,21 @@
+package at.forsyte.apalache.tla.typecheck
+
+import at.forsyte.apalache.tla.typecheck.etc.EtcExpr
+
+/**
+  * The interface to a type checker.
+  *
+  * @author Igor Konnov
+  */
+trait TypeChecker {
+  /**
+    * Compute the expression type in a type context. If the expression is not well-typed, return None.
+    * As a side effect, call the listener, when discovering new types or errors.
+    *
+    * @param listener a listener that will receive the type error or type info
+    * @param ctx a typing context
+    * @param ex an expression
+    * @return Some(type), if the expression is well-typed; and None otherwise.
+    */
+  def compute(listener: TypeCheckerListener, ctx: TypeContext, ex: EtcExpr): Option[TlaType1]
+}
