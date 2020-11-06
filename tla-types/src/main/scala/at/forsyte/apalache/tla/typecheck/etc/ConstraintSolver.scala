0a8b061245f0ef63120e894899f29cb8bfeca402
Shon Feder <shon@informal.systems>
2020-11-06 14:05:33 -0500

    Add ConstraintSolver module

---
+++ tla-types/src/main/scala/at/forsyte/apalache/tla/typecheck/etc/ConstraintSolver.scala
@@ -0,0 +1,99 @@
+package at.forsyte.apalache.tla.typecheck.etc
+
+import at.forsyte.apalache.tla.typecheck.TlaType1
+
+/**
+  * A constraint solver that collects a series of equations and solves them with the type unification algorithm.
+  *
+  * @author Igor Konnov
+  */
+class ConstraintSolver(approximateSolution: Substitution = Substitution.empty) {
+  private var solution: Substitution = approximateSolution
+  private var constraints: List[Clause] = List.empty
+  private var typesToReport: List[(Clause, TlaType1)] = List.empty
+
+  def addConstraint(cons: Clause): Unit = {
+    constraints = constraints :+ cons
+  }
+
+  def solvePartially(): Option[Substitution] = {
+    var progress = true
+    while (constraints.nonEmpty && progress) {
+      var postponed: List[Clause] = List.empty
+      progress = false
+
+      for (cons <- constraints) {
+        val result = solveOne(solution, cons)
+        if (result.isDefined) {
+          // there is a unique solution
+          progress = true
+          solution = result.get._1
+          typesToReport :+= (cons, solution(result.get._2))
+        } else {
+          cons match {
+            case OrClause(_@_*) =>
+              // no solution for a disjunctive constraint:
+              // try to resolve the unit constraints and postpone the disjunctive one for later
+              postponed = postponed :+ cons
+
+            case EqClause(_, term) =>
+              // no solution for a unit constraint:
+              // flag an error immediately
+              cons.onTypeError(Seq(solution(term)))
+              // reset the constraints, so they are not reported later
+              constraints = List.empty
+              return None
+          }
+        }
+      }
+
+      // solve the postponed constraints at the next iteration
+      constraints = postponed
+    }
+
+    // return the partial solution
+    Some(solution)
+  }
+
+  def solve(): Option[Substitution] = {
+    val isDefined = solvePartially().isDefined
+
+    if (isDefined && constraints.isEmpty) {
+      // all constraints have been solved, report the types
+      for ((c, t) <- typesToReport) {
+        c.onTypeFound(solution(t))
+      }
+
+      Some(solution)
+    } else {
+      constraints.foreach {
+        case c @ OrClause(clauses @ _*) =>
+          val partialSignatures = clauses.map { c => solution(c.term) }
+          c.onTypeError(partialSignatures)
+
+        case c @ EqClause(_, term) =>
+          c.onTypeError(Seq(solution(term)))
+      }
+      None
+    }
+  }
+
+  private def solveOne(solution: Substitution, cons: Clause): Option[(Substitution, TlaType1)] = {
+    cons match {
+      case EqClause(unknown, term) =>
+        // If there is a solution, we return it. We ignore the type, as it should be bound to `unknown`.
+        new TypeUnifier().unify(solution, unknown, term)
+
+      case OrClause(eqs @ _*) =>
+        // try to solve a disjunctive clause
+        val solutions = eqs.map(e => solveOne(solution, e)).collect { case Some(s) => s }
+        if (solutions.size != 1) {
+          // either no solution, or multiple solutions
+          None
+        } else {
+          // there is a unique solution
+          Some(solutions.head)
+        }
+    }
+  }
+}
