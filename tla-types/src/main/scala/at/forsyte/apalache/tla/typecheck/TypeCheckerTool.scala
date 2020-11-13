7057645d0da0e01e20a9e6c0a110959c34782ddf
Shon Feder <shon@informal.systems>
2020-11-13 10:17:34 -0500

    Put comment over relevant code

--- tla-types/src/main/scala/at/forsyte/apalache/tla/typecheck/TypeCheckerTool.scala
+++ tla-types/src/main/scala/at/forsyte/apalache/tla/typecheck/TypeCheckerTool.scala
@@ -13,14 +13,17 @@ class TypeCheckerTool {
     // TODO: remember to type check the assumptions!
 
     val varPool = new TypeVarPool()
-    // translate the whole TLA+ module into a long EtcExpr. Is not that cool?
     val toEtc = new ToEtcExpr(varPool)
+
     // Bool is the final expression in the chain of let-definitions
     val terminalExpr: EtcExpr = EtcConst(BoolT1()) (BlameRef(UID.unique))
+
+    // translate the whole TLA+ module into a long EtcExpr. Is not that cool?
     val rootExpr =
       module.operDeclarations.foldRight(terminalExpr) {
         case (decl, inScopeEx) => toEtc(decl, inScopeEx)
       }
+
     // run the type checker
     val result = new EtcTypeChecker(varPool).compute(listener, TypeContext.empty, rootExpr)
     result.isDefined
