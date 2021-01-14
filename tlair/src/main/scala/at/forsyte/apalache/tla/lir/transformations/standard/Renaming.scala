14cb1e3609dd5f7c699e5a311bedb6d8e731b614
Igor Konnov <konnov@forsyte.at>
2021-01-14 11:00:28 +0100

    removed an outdated TODO

--- tlair/src/main/scala/at/forsyte/apalache/tla/lir/transformations/standard/Renaming.scala
+++ tlair/src/main/scala/at/forsyte/apalache/tla/lir/transformations/standard/Renaming.scala
@@ -10,8 +10,6 @@ import scala.collection.immutable.HashMap
   * This class contains methods that are related to renaming.
   *
   * @author Igor Konnov
-  *
-  * TODO: shall we move this class to *.lir.transformations.standard?
   */
 class Renaming (tracker: TransformationTracker) extends TlaExTransformation {
   /**
