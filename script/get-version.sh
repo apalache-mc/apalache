e39fb04e4d33e525b1551ff66fceb6ebac21dc3d
Shon Feder <shon.feder@gmail.com>
2021-01-18 12:09:46 -0500

    Move version getting line into own script
    Because we'll want to use this in the release prep script.

---
+++ script/get-version.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Fetch the version of the project from mvn
+
+# The directory of this file
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+# shellcheck source=./shared.sh
+source "$DIR"/shared.sh
+
+# We need to be in the root dir to get the project version from mvn
+cd "$PROJ_ROOT"
+
+# See https://stackoverflow.com/a/26514030/1187277
+mvn -q \
+    -Dexec.executable=echo \
+    -Dexec.args='${project.version}' \
+    --non-recursive \
+    exec:exec
