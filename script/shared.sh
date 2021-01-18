85fe0f02e289810ecdc66610c50c87e06549f518
Shon Feder <shon.feder@gmail.com>
2021-01-18 13:34:14 -0500

    Remove outdated comment

--- script/shared.sh
+++ script/shared.sh
@@ -4,7 +4,6 @@ set -euo pipefail
 # Shared variables used in other scripts
 
 exports () {
-    # tell shellcheck we can use `local`
     # The directory of this file
     local DIR
     DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
