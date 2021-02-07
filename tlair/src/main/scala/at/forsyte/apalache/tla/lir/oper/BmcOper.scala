fa2bff7343fd3b3c8d97499d1617e90dde3bc171
Kukovec <jkukovec@forsyte.at>
2021-02-07 18:59:48 +0100

    Formatting

--- tlair/src/main/scala/at/forsyte/apalache/tla/lir/oper/BmcOper.scala
+++ tlair/src/main/scala/at/forsyte/apalache/tla/lir/oper/BmcOper.scala
@@ -1,13 +1,13 @@
 package at.forsyte.apalache.tla.lir.oper
 
 /**
-  * The operators defined in the module Apalache.tla. This module gives the users a facility to provide hints.
-  * The "Apalache" module is automatically looked up when Apalache is running.
-  *
-  * TODO: rename this class to ApalacheOper, once ik/multicore is merged into unstable.
-  *
-  * @author konnov
-  */
+ * The operators defined in the module Apalache.tla. This module gives the users a facility to provide hints.
+ * The "Apalache" module is automatically looked up when Apalache is running.
+ *
+ * TODO: rename this class to ApalacheOper, once ik/multicore is merged into unstable.
+ *
+ * @author konnov
+ */
 abstract class BmcOper extends TlaOper {
   override def interpretation: Interpretation.Value = Interpretation.StandardLib
 }
@@ -15,8 +15,8 @@ abstract class BmcOper extends TlaOper {
 object BmcOper {
 
   /**
-    * A type annotation of an expression with another expression that encodes a type.
-    */
+   * A type annotation of an expression with another expression that encodes a type.
+   */
   object withType extends BmcOper {
     override def name: String = "BMC!<:"
     override def arity: OperArity = FixedArity(2)
@@ -24,8 +24,8 @@ object BmcOper {
   }
 
   /**
-    * An operator x <- e that is interpreted as an assignment of e to x (the variable can have a prime too).
-    */
+   * An operator x <- e that is interpreted as an assignment of e to x (the variable can have a prime too).
+   */
   object assign extends BmcOper {
     override def name: String = "BMC!:="
     override def arity: OperArity = FixedArity(2)
@@ -33,9 +33,9 @@ object BmcOper {
   }
 
   /**
-    * Skolemization hint. In an expression Skolem(\E x \in S: e), the existential may be skolemized, that is, translated
-    * into a constant.
-    */
+   * Skolemization hint. In an expression Skolem(\E x \in S: e), the existential may be skolemized, that is, translated
+   * into a constant.
+   */
   object skolem extends BmcOper {
     override def name: String = "BMC!Skolem"
     override def arity: OperArity = FixedArity(1)
@@ -43,10 +43,10 @@ object BmcOper {
   }
 
   /**
-    * An expansion hint that can be applied to SUBSET S or [S -> T]. This hint orders the rewriter
-    * to expand the underlying expression into a finite set. Since, such an expansion results in an exponential
-    * blow up, this should be done carefully (and avoided as much as possible).
-    */
+   * An expansion hint that can be applied to SUBSET S or [S -> T]. This hint orders the rewriter
+   * to expand the underlying expression into a finite set. Since, such an expansion results in an exponential
+   * blow up, this should be done carefully (and avoided as much as possible).
+   */
   object expand extends BmcOper {
     override def name: String = "BMC!Expand"
     override def arity: OperArity = FixedArity(1)
@@ -54,10 +54,10 @@ object BmcOper {
   }
 
   /**
-    * An optimization hint for a cardinality constraint like Cardinality(S) >= k, where k is a constant.
-    * Similar to BMC!Skolem, this optimization has to be applied carefully, as it is not sound, when the cardinality
-    * test is located under negation.
-    */
+   * An optimization hint for a cardinality constraint like Cardinality(S) >= k, where k is a constant.
+   * Similar to BMC!Skolem, this optimization has to be applied carefully, as it is not sound, when the cardinality
+   * test is located under negation.
+   */
   object constCard extends BmcOper {
     override def name: String = "BMC!ConstCardinality"
     override def arity: OperArity = FixedArity(1)
@@ -65,11 +65,11 @@ object BmcOper {
   }
 
   /**
-    * The distinct operator that is equivalent to (distinct ...) in SMT-LIB.
-    * Formally, BMC!Distinct(x_1, ..., x_n) is equivalent to \A i, j \in 1..n: i /= j => x_i /= x_j.
-    *
-    * XXX: there seems to be no way of defining a user-defined variadic operator in Apalache.tla.
-    */
+   * The distinct operator that is equivalent to (distinct ...) in SMT-LIB.
+   * Formally, BMC!Distinct(x_1, ..., x_n) is equivalent to \A i, j \in 1..n: i /= j => x_i /= x_j.
+   *
+   * XXX: there seems to be no way of defining a user-defined variadic operator in Apalache.tla.
+   */
   object distinct extends BmcOper {
     override def name: String = "BMC!Distinct"
     override def arity: OperArity = AnyArity()
@@ -77,10 +77,10 @@ object BmcOper {
   }
 
   /**
-    * Attempt to dynamically cast an Int -> T function to a Seq(T).
-    * The first argument should be the function expression and the second argument
-    * should be an integer, denoting the maximal length of the sequence.
-    */
+   * Attempt to dynamically cast an Int -> T function to a Seq(T).
+   * The first argument should be the function expression and the second argument
+   * should be an integer, denoting the maximal length of the sequence.
+   */
   object funAsSeq extends BmcOper {
     override def name: String = "BMC!FunAsSeq"
     override def arity: OperArity = FixedArity(2)
