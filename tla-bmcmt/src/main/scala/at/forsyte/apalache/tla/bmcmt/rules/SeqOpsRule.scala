2c5fcee9512270e47138fde09beed6a50d10f2ff
Igor Konnov <igor@informal.systems>
2021-03-21 15:09:31 +0100

    fix TestSymbStateRewriterSequence

--- tla-bmcmt/src/main/scala/at/forsyte/apalache/tla/bmcmt/rules/SeqOpsRule.scala
+++ tla-bmcmt/src/main/scala/at/forsyte/apalache/tla/bmcmt/rules/SeqOpsRule.scala
@@ -1,9 +1,9 @@
 package at.forsyte.apalache.tla.bmcmt.rules
 
 import at.forsyte.apalache.tla.bmcmt._
-import at.forsyte.apalache.tla.lir.{OperEx, TlaEx}
+import at.forsyte.apalache.tla.lir.{BoolT1, IntT1, OperEx, TlaEx, TlaType1}
 import at.forsyte.apalache.tla.lir.convenience.tla
-import at.forsyte.apalache.tla.lir.UntypedPredefs._
+import at.forsyte.apalache.tla.lir.TypedPredefs._
 import at.forsyte.apalache.tla.bmcmt.rules.aux.CherryPick
 import at.forsyte.apalache.tla.bmcmt.types.CellT
 import at.forsyte.apalache.tla.lir.oper.TlaSeqOper
@@ -15,6 +15,7 @@ import at.forsyte.apalache.tla.lir.oper.TlaSeqOper
  */
 class SeqOpsRule(rewriter: SymbStateRewriter) extends RewritingRule {
   private val picker = new CherryPick(rewriter)
+  private val types = Map("i" -> IntT1(), "b" -> BoolT1())
 
   override def isApplicable(symbState: SymbState): Boolean = {
     symbState.ex match {
@@ -31,8 +32,9 @@ class SeqOpsRule(rewriter: SymbStateRewriter) extends RewritingRule {
 
   override def apply(state: SymbState): SymbState = {
     state.ex match {
-      case OperEx(TlaSeqOper.head, seq) =>
-        rewriter.rewriteUntilDone(state.setRex(tla.appFun(seq, tla.int(1))))
+      case ex @ OperEx(TlaSeqOper.head, seq) =>
+        val elemType = TlaType1.fromTypeTag(ex.typeTag)
+        rewriter.rewriteUntilDone(state.setRex(tla.appFun(seq, tla.int(1)).typed(elemType)))
 
       case OperEx(TlaSeqOper.len, seq) =>
         translateLen(state, seq)
@@ -62,7 +64,10 @@ class SeqOpsRule(rewriter: SymbStateRewriter) extends RewritingRule {
     val end = cells.tail.head
     // increment start, unless it goes over the bound
     val updatedStart =
-      tla.ite(tla.lt(start.toNameEx, end.toNameEx), tla.plus(tla.int(1), start.toNameEx), start.toNameEx)
+      tla
+        .ite(tla.lt(start.toNameEx ? "i", end.toNameEx ? "i") ? "b", tla.plus(tla.int(1), start.toNameEx ? "i") ? "i",
+            start.toNameEx ? "i")
+        .typed(Map("i" -> IntT1(), "b" -> BoolT1()), "i")
     // increment start
     nextState = rewriter.rewriteUntilDone(nextState.setRex(updatedStart))
     val newStart = nextState.asCell
@@ -75,6 +80,7 @@ class SeqOpsRule(rewriter: SymbStateRewriter) extends RewritingRule {
 
   private def translateSubSeq(state: SymbState, seq: TlaEx, newStartEx: TlaEx, newEndEx: TlaEx) = {
     var nextState = state
+
     def rewriteToCell(ex: TlaEx): ArenaCell = {
       nextState = rewriter.rewriteUntilDone(nextState.setRex(ex))
       nextState.asCell
@@ -85,20 +91,30 @@ class SeqOpsRule(rewriter: SymbStateRewriter) extends RewritingRule {
     val start = cells.head
     val end = cells.tail.head
 
+    val expectedStartEx = tla
+      .plus(start.toNameEx ? "i", tla.minus(newStartEx ? "i", tla.int(1)) ? "i")
+      .typed(types, "i")
     // compute the new interval [expectedStart, expectedEnd)
-    val expectedStart = rewriteToCell(tla.plus(start.toNameEx, tla.minus(newStartEx, tla.int(1))))
-    val expectedEnd = rewriteToCell(tla.plus(start.toNameEx, newEndEx))
+    val expectedStart = rewriteToCell(expectedStartEx)
+    val expectedEndEx = tla
+      .plus(start.toNameEx ? "i", newEndEx)
+      .typed(types, "i")
+    val expectedEnd = rewriteToCell(expectedEndEx)
     // use the computed values, as soon as they do not violate the invariant:
     //   start <= end, start >= oldStart, end <= oldEnd
     val seqInvariant = rewriteToCell(
-        tla.and(
-            tla.le(expectedStart.toNameEx, expectedEnd.toNameEx),
-            tla.le(start.toNameEx, expectedStart.toNameEx),
-            tla.le(expectedEnd.toNameEx, end.toNameEx)
-        ))
+        tla
+          .and(
+              tla.le(expectedStart.toNameEx ? "i", expectedEnd.toNameEx ? "i") ? "b",
+              tla.le(start.toNameEx ? "i", expectedStart.toNameEx ? "i") ? "b",
+              tla.le(expectedEnd.toNameEx ? "i", end.toNameEx ? "i") ? "b"
+          )
+          .typed(types, "b"))
 
-    val newStart = rewriteToCell(tla.ite(seqInvariant.toNameEx, expectedStart.toNameEx, tla.int(0)))
-    val newEnd = rewriteToCell(tla.ite(seqInvariant.toNameEx, expectedEnd.toNameEx, tla.int(0)))
+    val newStart =
+      rewriteToCell(tla.ite(seqInvariant.toNameEx ? "b", expectedStart.toNameEx ? "i", tla.int(0)).typed(types, "i"))
+    val newEnd =
+      rewriteToCell(tla.ite(seqInvariant.toNameEx ? "b", expectedEnd.toNameEx ? "i", tla.int(0)).typed(types, "i"))
 
     // introduce a new sequence that whose start and end are updated as required
     nextState = nextState.updateArena(_.appendCell(seqCell.cellType))
@@ -115,7 +131,7 @@ class SeqOpsRule(rewriter: SymbStateRewriter) extends RewritingRule {
     val start = cells.head
     val end = cells.tail.head
     val oldElems = cells.tail.tail
-    nextState = rewriter.rewriteUntilDone(nextState.setRex(tla.plus(tla.int(1), end.toNameEx)))
+    nextState = rewriter.rewriteUntilDone(nextState.setRex(tla.plus(tla.int(1), end.toNameEx ? "i").typed(types, "i")))
     val newEnd = nextState.asCell
     nextState = rewriter.rewriteUntilDone(nextState.setRex(newElem))
     val newElemCell = nextState.asCell
@@ -128,10 +144,16 @@ class SeqOpsRule(rewriter: SymbStateRewriter) extends RewritingRule {
       nextState = picker.pickByOracle(nextState, oracle, Seq(oldElemCell, newElemCell),
           nextState.arena.cellTrue().toNameEx)
       // pick the element from the old sequence: start <= no /\ no < end => oracle = 0
-      solverAssert(tla.impl(tla.and(tla.le(start.toNameEx, tla.int(no)), tla.lt(tla.int(no), end.toNameEx)),
-              oracle.whenEqualTo(nextState, 0)))
+      solverAssert(
+          tla
+            .impl(tla.and(tla.le(start.toNameEx ? "i", tla.int(no)) ? "b",
+                    tla.lt(tla.int(no), end.toNameEx ? "i") ? "b") ? "b", oracle.whenEqualTo(nextState, 0) ? "b")
+            .typed(types, "b"))
       // pick the element from the new sequence: no = end => oracle = 1
-      solverAssert(tla.impl(tla.eql(tla.int(no), end.toNameEx), oracle.whenEqualTo(nextState, 1)))
+      solverAssert(
+          tla
+            .impl(tla.eql(tla.int(no), end.toNameEx ? "i") ? "b", oracle.whenEqualTo(nextState, 1) ? "b")
+            .typed(types, "b"))
       // the other elements are unrestricted, give some freedom to the solver
       nextState.asCell
     }
@@ -152,7 +174,7 @@ class SeqOpsRule(rewriter: SymbStateRewriter) extends RewritingRule {
     val start = cells.head
     val end = cells.tail.head
     // just return end - start
-    rewriter.rewriteUntilDone(nextState.setRex(tla.minus(end.toNameEx, start.toNameEx)))
+    rewriter.rewriteUntilDone(nextState.setRex(tla.minus(end.toNameEx ? "i", start.toNameEx ? "i").typed(types, "i")))
   }
 
   // Implement concatenation on sequences. This is the most expensive operation on sequences.
@@ -168,7 +190,8 @@ class SeqOpsRule(rewriter: SymbStateRewriter) extends RewritingRule {
       val start = connectedCells.head
       val end = connectedCells.tail.head
       val elems = connectedCells.tail.tail
-      nextState = rewriter.rewriteUntilDone(nextState.setRex(tla.minus(end.toNameEx, start.toNameEx)))
+      nextState = rewriter.rewriteUntilDone(
+          nextState.setRex(tla.minus(end.toNameEx ? "i", start.toNameEx ? "i").typed(types, "i")))
       val len = nextState.asCell
       (start, end, len, elems, cell.cellType)
     }
@@ -180,9 +203,10 @@ class SeqOpsRule(rewriter: SymbStateRewriter) extends RewritingRule {
     // introduce a new sequence
     nextState = nextState.updateArena(_.appendCell(cellType))
     val seq3 = nextState.arena.topCell
-    nextState = rewriter.rewriteUntilDone(nextState.setRex(tla.int(0)))
+    nextState = rewriter.rewriteUntilDone(nextState.setRex(tla.int(0).typed()))
     val start3 = nextState.asCell
-    nextState = rewriter.rewriteUntilDone(nextState.setRex(tla.plus(len1.toNameEx, len2.toNameEx)))
+    nextState = rewriter.rewriteUntilDone(
+        nextState.setRex(tla.plus(len1.toNameEx ? "i", len2.toNameEx ? "i").typed(types, "i")))
     val end3 = nextState.asCell
 
     val elems1then2 = elems1 ++ elems2
@@ -191,9 +215,14 @@ class SeqOpsRule(rewriter: SymbStateRewriter) extends RewritingRule {
     // pre-compute integer constants that are used when computing every element
     // offset2 = N1 + start2 - len1
     nextState = rewriter
-      .rewriteUntilDone(nextState.setRex(tla.minus(tla.plus(tla.int(elems1.size), start2.toNameEx), len1.toNameEx)))
+      .rewriteUntilDone(
+          nextState.setRex(
+              tla
+                .minus(tla.plus(tla.int(elems1.size) ? "i", start2.toNameEx ? "i") ? "i", len1.toNameEx ? "i")
+                .typed(types, "i")))
     val offset2 = nextState.asCell
-    nextState = rewriter.rewriteUntilDone(nextState.setRex(tla.plus(len1.toNameEx, len2.toNameEx)))
+    nextState = rewriter.rewriteUntilDone(
+        nextState.setRex(tla.plus(len1.toNameEx ? "i", len2.toNameEx ? "i").typed(types, "i")))
     val len1plus2 = nextState.asCell
 
     // introduce constraints for the i-th element of the resulting sequence
@@ -207,20 +236,23 @@ class SeqOpsRule(rewriter: SymbStateRewriter) extends RewritingRule {
       // If 0 <= i < len1, then require oracle = i + start1,
       // If len1 <= i < len1 + len2, then require oracle = i + offset2 = i - len1 + N1 + start2,
       // Otherwise, set oracle to N
-      val inRange1 = tla.lt(tla.int(i), len1.toNameEx)
+      val inRange1 = tla.lt(tla.int(i), len1.toNameEx ? "i") ? "b"
       val inRange2 =
-        tla.and(tla.le(len1.toNameEx, tla.int(i)), tla.lt(tla.int(i), len1plus2.toNameEx))
+        tla.and(tla.le(len1.toNameEx ? "i", tla.int(i)) ? "b", tla.lt(tla.int(i), len1plus2.toNameEx ? "i") ? "b") ? "b"
 
       val whenInRange1 =
-        tla.or(tla.not(inRange1), tla.eql(oracle.intCell.toNameEx, tla.plus(tla.int(i), start1.toNameEx)))
+        tla.or(tla.not(inRange1) ? "b",
+            tla.eql(oracle.intCell.toNameEx ? "i", tla.plus(tla.int(i), start1.toNameEx ? "i") ? "i") ? "b") ? "b"
       val whenInRange2 =
-        tla.or(tla.not(inRange2), tla.eql(oracle.intCell.toNameEx, tla.plus(tla.int(i), offset2.toNameEx)))
+        tla.or(tla.not(inRange2) ? "b",
+            tla.eql(oracle.intCell.toNameEx ? "i", tla.plus(tla.int(i), offset2.toNameEx ? "i") ? "i") ? "b") ? "b"
       val whenOutOfRange =
-        tla.or(tla.lt(tla.int(i), len1plus2.toNameEx), tla.eql(oracle.intCell.toNameEx, tla.int(ntotal)))
+        tla.or(tla.lt(tla.int(i), len1plus2.toNameEx ? "i") ? "b",
+            tla.eql(oracle.intCell.toNameEx ? "i", tla.int(ntotal)) ? "b") ? "b"
 
-      solverAssert(whenInRange1)
-      solverAssert(whenInRange2)
-      solverAssert(whenOutOfRange)
+      solverAssert(whenInRange1.typed(types, "b"))
+      solverAssert(whenInRange2.typed(types, "b"))
+      solverAssert(whenOutOfRange.typed(types, "b"))
 
       pickedResult
     }
