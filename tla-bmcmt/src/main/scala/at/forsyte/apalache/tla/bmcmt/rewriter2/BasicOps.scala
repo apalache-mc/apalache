988779c64e9b093513e451b2b74018a9408d097e
Kukovec <jure.kukovec@gmail.com>
2021-07-08 15:05:14 +0200

    PR comments

--- tla-bmcmt/src/main/scala/at/forsyte/apalache/tla/bmcmt/rewriter2/BasicOps.scala
+++ tla-bmcmt/src/main/scala/at/forsyte/apalache/tla/bmcmt/rewriter2/BasicOps.scala
@@ -11,34 +11,37 @@ import scalaz.Scalaz._
  * A collection of common methods for manipulating the internal state
  */
 object BasicOps {
-  private def safeBind(key: String, value: ArenaCell): RewritingComputation[Unit] = modify { s =>
-    s.copy(binding = Binding(s.binding.toMap + (key -> value)))
-  }
-
   // Extends the internal binding with key->value
-  def bind(key: String, value: ArenaCell): StateCompWithExceptions[Unit] =
-    EitherT.rightT(safeBind(key, value))
-
-  private def safeAssertGroundExpr(ex: TlaEx): RewritingComputation[Unit] = modify { s =>
-    // xform ex into BoolFormula
-    // proof of concept: every formula is False
-    s.copy(constraints = False() +: s.constraints)
+  def bind(key: String, value: ArenaCell): StateCompWithExceptions[Unit] = cmpIsRight {
+    modify { s =>
+      s.copy(binding = Binding(s.binding.toMap + (key -> value)))
+    }
   }
 
   // Extends the collection of constraints with the SMT formula represented by ex
   // Note: A real implementation should not use the BoolT1 fragment of TlaEx as a proxy for
   // representing SMT constraints.
-  def assertGroundExpr(ex: TlaEx): StateCompWithExceptions[Unit] = EitherT.rightT(safeAssertGroundExpr(ex))
-
-  def safeNewCell(cellType: CellT): RewritingComputation[ArenaCell] = symbState { state =>
-    val newArena = state.arena.appendCell(cellType)
-    (state.copy(arena = newArena), newArena.topCell)
+  def assertGroundExpr(ex: TlaEx): StateCompWithExceptions[Unit] = cmpIsRight {
+    modify { s =>
+      // xform ex into BoolFormula
+      // proof of concept: every formula is False
+      s.copy(constraints = False() +: s.constraints)
+    }
   }
 
   // Adds a new cell of the given type to the arena and returns it
-  def newCell(cellType: CellT): StateCompWithExceptions[ArenaCell] = EitherT.rightT(safeNewCell(cellType))
+  def newCell(cellType: CellT): StateCompWithExceptions[ArenaCell] = cmpIsRight {
+    cmp { state =>
+      val newArena = state.arena.appendCell(cellType)
+      (state.copy(arena = newArena), newArena.topCell)
+    }
+  }
 
   // Shorthand for State[SymbStateInternal, T] { fn }
-  def symbState[T](fn: RewritingState => (RewritingState, T)): RewritingComputation[T] =
+  def cmp[T](fn: RewritingState => (RewritingState, T)): RewritingComputation[T] =
     State[RewritingState, T] { fn }
+
+  // Shorthand for EitherT.rightT[Exception, RewritingComputation, T] { cmp }
+  def cmpIsRight[T](cmp: RewritingComputation[T]): StateCompWithExceptions[T] =
+    EitherT.rightT[Exception, RewritingComputation, T]{ cmp }
 }
