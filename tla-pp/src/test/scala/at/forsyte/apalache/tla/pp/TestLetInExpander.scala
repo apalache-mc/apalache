4970cbfe667adae7a63ec7750567b224f31d98f3
Igor Konnov <igor@informal.systems>
2021-08-26 14:53:41 +0200

    fix types and refactor the test suite

--- tla-pp/src/test/scala/at/forsyte/apalache/tla/pp/TestLetInExpander.scala
+++ tla-pp/src/test/scala/at/forsyte/apalache/tla/pp/TestLetInExpander.scala
@@ -1,132 +1,188 @@
 package at.forsyte.apalache.tla.pp
 
-import at.forsyte.apalache.tla.lir.UntypedPredefs._
-import at.forsyte.apalache.tla.lir.convenience.tla
-import at.forsyte.apalache.tla.lir.oper.{TlaOper, TlaSeqOper}
-import at.forsyte.apalache.tla.lir.transformations.impl.IdleTracker
-import at.forsyte.apalache.tla.lir.values.TlaInt
+import at.forsyte.apalache.io.typecheck.parser.DefaultType1Parser
+import at.forsyte.apalache.tla.lir.TypedPredefs._
 import at.forsyte.apalache.tla.lir._
+import at.forsyte.apalache.tla.lir.convenience.tla
+import at.forsyte.apalache.tla.lir.transformations.impl.IdleTracker
 import org.junit.runner.RunWith
 import org.scalatest.FunSuite
 import org.scalatest.junit.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
-class TestLetInExpander extends FunSuite with TestingPredefs {
+class TestLetInExpander extends FunSuite {
 
   import tla._
 
-  test("Test LetInExpander, skip0Arity = false") {
-    val transformation = LetInExpander(new IdleTracker(), keepNullary = false)
+  private val parser = DefaultType1Parser
 
-    val ex1: TlaEx = n_x
-    val ex2: TlaEx = letIn(appOp(n_A), declOp("A", n_x).untypedOperDecl())
-    val ex3: TlaEx = letIn(appOp(n_A, n_x), declOp("A", n_y, OperParam("y")).untypedOperDecl())
-    val ex4: TlaEx =
-      letIn(
-          ite(
-              ge(n_c, int(0)),
-              letIn(
-                  appOp(NameEx("Y")),
-                  declOp("Y", appOp(NameEx("X"), n_c, n_c)).untypedOperDecl()
-              ),
-              appOp(NameEx("X"), n_p, int(3))
-          ),
-          declOp("X", ge(plus(n_a, n_b), int(0)), "a", "b").untypedOperDecl()
-      )
+  val types =
+    Map("i" -> parser("Int"), "b" -> parser("Bool"), "U" -> parser("() => Int"), "C" -> parser("Int => Int"),
+        "iTOi" -> parser("Int => Int"), "iTOb" -> parser("Int => Bool"), "iiTOi" -> parser("(Int, Int) => Int"),
+        "iiTOb" -> parser("(Int, Int) => Bool"), "si" -> parser("Seq(Int)"))
 
-    val expected1: TlaEx = n_x
-    val expected2: TlaEx = n_x
-    val expected3: TlaEx = n_x
-    val expected4: TlaEx =
-      ite(
-          ge(n_c, int(0)),
-          ge(plus(n_c, n_c), int(0)),
-          ge(plus(n_p, int(3)), int(0))
-      )
-
-    val exs = Seq(ex1, ex2, ex3, ex4)
-    val expected = Seq(expected1, expected2, expected3, expected4)
-    val actual = exs map transformation
-
-    assert(expected == actual)
-
+  private def test1(keepNullary: Boolean): Unit = {
+    val n_x = tla.name("x").typed(IntT1())
+    val input: TlaEx = n_x
+    val output = new LetInExpander(new IdleTracker(), keepNullary = keepNullary)(input)
+    val expected = n_x
+    assert(expected == output)
+    assert(output.eqTyped(expected))
   }
 
-  test("Test LetInExpander, skip0Arity = true") {
-    val transformation = LetInExpander(new IdleTracker(), keepNullary = true)
+  test("Test 1a: LetInExpander, keepNullary = false") {
+    test1(false)
+  }
 
-    val ex1: TlaEx = n_x
-    val ex2: TlaEx = letIn(appOp(n_A), declOp("A", n_x).untypedOperDecl())
-    val ex3: TlaEx = letIn(appOp(n_A, n_x), declOp("A", n_y, OperParam("y")).untypedOperDecl())
-    val ex4: TlaEx =
+  test("Test 1b: LetInExpander, keepNullary = true") {
+    test1(true)
+  }
+
+  test("Test 2a: LetInExpander, keepNullary = false") {
+    val n_x = tla.name("x").typed(IntT1())
+    val n_A = tla.name("A").typed(types, "U")
+    val input = letIn(appOp(n_A) ? "i", declOp("A", n_x).typedOperDecl(types, "U")).typed(types, "i")
+    val output = new LetInExpander(new IdleTracker(), keepNullary = false)(input)
+    val expected = n_x
+    assert(expected == output)
+    assert(output.eqTyped(expected))
+  }
+
+  test("Test 2b: LetInExpander, keepNullary = true") {
+    val n_x = tla.name("x").typed(IntT1())
+    val n_A = tla.name("A").typed(types, "U")
+    val input = letIn(appOp(n_A) ? "i", declOp("A", n_x).typedOperDecl(types, "U")).typed(types, "i")
+    val output = new LetInExpander(new IdleTracker(), keepNullary = true)(input)
+    val expected = input
+    assert(expected == output)
+    assert(output.eqTyped(expected))
+  }
+
+  private def test3(keepNullary: Boolean): Unit = {
+    val n_x = tla.name("x").typed(IntT1())
+    val n_y = tla.name("y").typed(IntT1())
+    val n_B = tla.name("B").typed(types, "iTOi")
+    val input =
+      letIn(appOp(n_B, n_x) ? "i", declOp("B", n_y, OperParam("y")).typedOperDecl(types, "iTOi")).typed(types, "i")
+    val output = new LetInExpander(new IdleTracker(), keepNullary = keepNullary)(input)
+    val expected = n_x
+    assert(expected == output)
+    assert(output.eqTyped(expected))
+  }
+
+  test("Test 3a: LetInExpander, keepNullary = false") {
+    test3(false)
+  }
+
+  test("Test 3b: LetInExpander, keepNullary = true") {
+    test3(true)
+  }
+
+  test("Test 4a: LetInExpander, keepNullary = false") {
+    val n_p = tla.name("p").typed(IntT1())
+    val n_a = tla.name("a").typed(IntT1())
+    val n_b = tla.name("b").typed(IntT1())
+    val n_c = tla.name("c").typed(IntT1())
+    val n_Y = tla.name("Y").typed(types, "U")
+    val n_X = tla.name("X").typed(types, "iiTOb")
+    val input =
       letIn(
           ite(
-              ge(n_c, int(0)),
+              ge(n_c, int(0)) ? "b",
               letIn(
-                  appOp(NameEx("Y")),
-                  declOp("Y", appOp(NameEx("X"), n_c, n_c)).untypedOperDecl()
-              ),
-              appOp(NameEx("X"), n_p, int(3))
-          ),
-          declOp("X", ge(plus(n_a, n_b), int(0)), "a", "b").untypedOperDecl()
-      )
-
-    val expected1: TlaEx = n_x
-    val expected2: TlaEx = letIn(appOp(n_A), declOp("A", n_x).untypedOperDecl())
-    val expected3: TlaEx = n_x
-    val expected4: TlaEx =
+                  appOp(n_Y) ? "i",
+                  declOp("Y", appOp(n_X, n_c, n_c) ? "i").typedOperDecl(types, "U")
+              ) ? "b",
+              appOp(n_X, n_p, int(3)) ? "b"
+          ) ? "b",
+          declOp("X", ge(plus(n_a, n_b) ? "i", int(0)) ? "b", OperParam("a"), OperParam("b")).typedOperDecl(types,
+              "iiTOb")
+      ).typed(types, "i")
+    val output = new LetInExpander(new IdleTracker(), keepNullary = false)(input)
+    val expected =
       ite(
-          ge(n_c, int(0)),
+          ge(n_c, int(0)) ? "b",
+          ge(plus(n_c, n_c) ? "i", int(0)) ? "b",
+          ge(plus(n_p, int(3)) ? "i", int(0)) ? "b"
+      ).typed(types, "b")
+
+    assert(expected == output)
+    assert(output.eqTyped(expected))
+  }
+
+  test("Test 4b: LetInExpander, keepNullary = true") {
+    val n_p = tla.name("p").typed(IntT1())
+    val n_a = tla.name("a").typed(IntT1())
+    val n_b = tla.name("b").typed(IntT1())
+    val n_c = tla.name("c").typed(IntT1())
+    val n_Y = tla.name("Y").typed(types, "U")
+    val n_X = tla.name("X").typed(types, "iiTOb")
+    val input =
+      letIn(
+          ite(
+              ge(n_c, int(0)) ? "b",
+              letIn(
+                  appOp(n_Y) ? "i",
+                  declOp("Y", appOp(n_X, n_c, n_c) ? "b").typedOperDecl(types, "U")
+              ) ? "b",
+              appOp(n_X, n_p, int(3)) ? "b"
+          ) ? "b",
+          declOp("X", ge(plus(n_a, n_b) ? "i", int(0)) ? "b", OperParam("a"), OperParam("b")).typedOperDecl(types,
+              "iiTOb")
+      ).typed(types, "i")
+    val output = new LetInExpander(new IdleTracker(), keepNullary = true)(input)
+    val expected =
+      ite(
+          ge(n_c, int(0)) ? "b",
           letIn(
-              appOp(NameEx("Y")),
-              declOp("Y", ge(plus(n_c, n_c), int(0))).untypedOperDecl()
-          ),
-          ge(plus(n_p, int(3)), int(0))
-      )
+              appOp(n_Y) ? "i",
+              declOp("Y", ge(plus(n_c, n_c) ? "i", int(0)) ? "b").typedOperDecl(types, "U")
+          ) ? "b",
+          ge(plus(n_p, int(3)) ? "i", int(0)) ? "b"
+      ).typed(types, "b")
 
-    val exs = Seq(ex1, ex2, ex3, ex4)
-    val expected = Seq(expected1, expected2, expected3, expected4)
-    val actual = exs map transformation
-
-    assert(expected == actual)
-
+    assert(expected == output)
+    assert(output.eqTyped(expected))
   }
 
-  test("Test LetInExpander and LAMBDA") {
-    val transformation = LetInExpander(new IdleTracker(), keepNullary = false)
+  test("Test 5: LetInExpander and LAMBDA") {
+    val transformation = new LetInExpander(new IdleTracker(), keepNullary = false)
     // this is how we represent LAMBDA in IR
     val lambdaAsLetIn =
-      tla.letIn(tla.name("LAMBDA"),
-          tla.declOp("LAMBDA", tla.eql(tla.name("x"), tla.name("e")), OperParam("x")).untypedOperDecl())
-    val input = OperEx(TlaSeqOper.selectseq, tla.name("s"), lambdaAsLetIn)
+      letIn(name("LAMBDA") ? "iTOb",
+          declOp("LAMBDA", eql(name("x") ? "i", name("e") ? "i") ? "b", OperParam("x"))
+            .typedOperDecl(types, "iTOi"))
+    val input = selectseq(name("s") ? "si", lambdaAsLetIn ? "iTOb").typed(types, "si")
     val output = transformation(input)
     // there is nothing to expand here, as SelectSeq is the standard operator
     assert(output == input)
   }
 
-  test("Do not expand LAMBDA") {
-    val transformation = LetInExpander(new IdleTracker(), keepNullary = false)
+  test("Test 6: Do not expand LAMBDA") {
+    val transformation = new LetInExpander(new IdleTracker(), keepNullary = false)
     // this is how we represent LAMBDA in IR
     val lambdaAsLetIn =
-      tla.letIn(tla.name("LAMBDA"),
-          tla.declOp("LAMBDA", tla.eql(tla.name("x"), tla.name("e")), OperParam("x")).untypedOperDecl())
+      letIn(name("LAMBDA") ? "iTOb",
+          declOp("LAMBDA", eql(name("x") ? "i", name("e") ? "i") ? "b", OperParam("x"))
+            .typedOperDecl(types, "iTOi"))
     // in this case, we cannot do anything, as the lambda operator is passed into the built-in operator
-    val input = OperEx(TlaSeqOper.selectseq, tla.name("s"), lambdaAsLetIn)
+    val input = selectseq(name("s") ? "si", lambdaAsLetIn ? "iTOb").typed(types, "si")
     val output = transformation(input)
     // there is nothing to expand here, as SelectSeq is the standard operator
     assert(output == input)
   }
 
-  test("Expand LAMBDA when it is applied to an argument") {
-    val transformation = LetInExpander(new IdleTracker(), keepNullary = false)
+  test("Test 7: Expand LAMBDA when it is applied to an argument") {
+    val transformation = new LetInExpander(new IdleTracker(), keepNullary = false)
     // this is how we represent LAMBDA in IR
     val lambdaAsLetIn =
-      tla.letIn(tla.name("LAMBDA"),
-          tla.declOp("LAMBDA", tla.eql(tla.name("x"), tla.name("e")), OperParam("x")).untypedOperDecl())
-    val input = OperEx(TlaOper.apply, lambdaAsLetIn, ValEx(TlaInt(3)))
+      letIn(name("LAMBDA") ? "iTOb",
+          declOp("LAMBDA", eql(name("x") ? "i", name("e") ? "i") ? "b", OperParam("x"))
+            .typedOperDecl(types, "iTOi"))
+    val input = appOp(lambdaAsLetIn ? "iTOb", int(3)).typed(types, "b")
     val output = transformation(input)
     // application of the lambda expression should be replaced with the body
-    val expected = tla.eql(ValEx(TlaInt(3)), tla.name("e")).untyped()
+    val expected = eql(int(3), name("e") ? "i").typed(types, "b")
     assert(expected == output)
   }
 }
