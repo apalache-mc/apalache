629a14ecb404fc3ed01efbeaa548ef9fc555b1e7
Igor Konnov <igor@informal.systems>
2021-08-27 14:16:35 +0200

    fix types and refactor TestSourceLocator

--- tla-pp/src/test/scala/at/forsyte/apalache/tla/pp/TestSourceLocator.scala
+++ tla-pp/src/test/scala/at/forsyte/apalache/tla/pp/TestSourceLocator.scala
@@ -1,29 +1,43 @@
 package at.forsyte.apalache.tla.pp
 
-import at.forsyte.apalache.tla.lir.UntypedPredefs._
+import at.forsyte.apalache.io.typecheck.parser.DefaultType1Parser
+import at.forsyte.apalache.tla.lir.TypedPredefs._
+import at.forsyte.apalache.tla.lir._
 import at.forsyte.apalache.tla.lir.aux._
 import at.forsyte.apalache.tla.lir.convenience._
-import at.forsyte.apalache.tla.lir.oper.ApalacheOper
 import at.forsyte.apalache.tla.lir.src.{SourceLocation, SourcePosition, SourceRegion}
 import at.forsyte.apalache.tla.lir.storage.{BodyMapFactory, ChangeListener, SourceLocator, SourceMap}
 import at.forsyte.apalache.tla.lir.transformations.impl.TrackerWithListeners
 import at.forsyte.apalache.tla.lir.transformations.standard._
 import at.forsyte.apalache.tla.lir.transformations.{TlaExTransformation, TransformationListener}
-import at.forsyte.apalache.tla.lir.values.{TlaIntSet, TlaStrSet}
-import at.forsyte.apalache.tla.lir._
 import org.junit.runner.RunWith
 import org.scalatest.FunSuite
 import org.scalatest.junit.JUnitRunner
 
 import scala.collection.mutable
 
+// TODO: Igor, 27.08.2021: this test needs refactoring. It is barely readable.
 @RunWith(classOf[JUnitRunner])
-class TestSourceLocator extends FunSuite with TestingPredefs {
+class TestSourceLocator extends FunSuite {
 
   import tla._
 
-  val decl1: TlaOperDecl = declOp("plus", plus(n_a, n_b), "a", "b").untypedOperDecl()
-  val decl2: TlaOperDecl = declOp("App", appOp(name("X"), n_p), ("X", 1), "p").untypedOperDecl()
+  private val parser = DefaultType1Parser
+
+  val types =
+    Map("i" -> parser("Int"), "b" -> parser("Bool"), "iiTOi" -> parser("(Int, Int) => Int"),
+        "iTOi" -> parser("Int => Int"), "X" -> parser("Int => Int"), "D2" -> parser("(Int => Int, Int) => Int"),
+        "TOi" -> parser("() => Int"), "rx" -> parser("[x: Int]"), "ii" -> parser("<<Int, Int>>"))
+
+  // plus(a, b) == a + b
+  val decl1: TlaOperDecl =
+    declOp("plus", plus(name("a") ? "i", name("b") ? "i") ? "i", OperParam("a"), OperParam("b"))
+      .typedOperDecl(types, "iiTOi")
+
+  // App(X(_), p) == X(p)
+  val decl2: TlaOperDecl =
+    declOp("App", appOp(name("X") ? "X", name("p") ? "i") ? "i", OperParam("X", 1), OperParam("p"))
+      .typedOperDecl(types, "D2")
 
   val decls = List(
       decl1,
@@ -32,39 +46,52 @@ class TestSourceLocator extends FunSuite with TestingPredefs {
 
   val bodyMap = BodyMapFactory.makeFromDecls(decls)
 
-  val ex1 = and(prime(n_x), n_y).untyped()
+  // x' /\ y
+  val ex1 = and(prime(name("x") ? "b") ? "b", name("y") ? "b").typed(types, "b")
+  // LET A(p) == p + 1 IN
+  // A(1) >= 0
   val ex2 = letIn(
-      ge(appOp(n_A, int(1)), int(0)),
-      declOp("A", plus(n_p, int(1)), "p").untypedOperDecl()
-  ).untyped()
-  val ex3 = appDecl(decl1, n_x, int(1)).untyped()
+      ge(appOp(name("A") ? "iTOi", int(1)) ? "i", int(0)) ? "b",
+      declOp("A", plus(name("p") ? "i", int(1)) ? "i", OperParam("p"))
+        .typedOperDecl(types, "iTOi")
+  ).typed(types, "b")
+  // plus(x, 1)
+  val ex3 = appOp(name("plus") ? "iiTOi", name("x") ? "i", int(1)).typed(types, "i")
+  // LET I(p) == p IN
+  // IF y THEN App(I, 0) ELSE FALSE
   val ex4 = letIn(
-      ite(n_y, appDecl(decl2, name("I"), int(0)), falseEx),
-      declOp("I", n_p, "p").untypedOperDecl()
-  ).untyped()
+      ite(name("y") ? "b", appOp(name("App") ? "D2", name("I") ? "X", int(0)) ? "i", bool(false)) ? "b",
+      declOp("I", name("p") ? "i", OperParam("p")).typedOperDecl(types, "b")
+  ).typed(types, "b")
+  // LET A(p, q) == IntentionallyUndefinedOper(p, q) IN
+  //   LET B == b
+  //       C(p) == A(p, B())
+  //   IN
+  //   LET D == x IN
+  //   C(D())
   val ex5 =
     letIn(
         letIn(
             letIn(
-                appOp(name("C"), appOp(name("D"))),
-                declOp("D", n_x).untypedOperDecl()
-            ),
-            declOp("B", n_b).untypedOperDecl(),
-            declOp("C", appOp(n_A, n_p, appOp(name("B"))), "p").untypedOperDecl()
-        ),
-        declOp("A", appOp(name("IntentionallyUndefinedOper"), n_p, n_q), "p", "q").untypedOperDecl()
-    ).untyped()
-  val ex6 = unchanged(n_x).untyped()
-  val ex7 = unchangedTup(n_x, n_y).untyped()
-  val ex8 =
-    OperEx(ApalacheOper.withType, enumFun(str("x"), int(1)),
-        enumFun(
-            str("x"),
-            ValEx(TlaIntSet),
-            str("y"),
-            enumSet(ValEx(TlaStrSet))
-        ))(Untyped())
-  val ex9 = appFun(enumFun(str("x"), int(1)), str("x")).untyped()
+                appOp(name("C") ? "iTOi", appOp(name("D") ? "TOi") ? "i") ? "i",
+                declOp("D", name("x") ? "i").typedOperDecl(types, "TOi")
+            ) ? "i",
+            declOp("B", name("b") ? "i").typedOperDecl(types, "TOi"),
+            declOp("C", appOp(name("A") ? "iiTOi", name("p") ? "i", appOp(name("B") ? "TOi") ? "i") ? "i",
+                OperParam("p"))
+              .typedOperDecl(types, "iTOi")
+        ) ? "i",
+        declOp("A", appOp(name("IntentionallyUndefinedOper") ? "iiTOi", name("p") ? "i", name("q") ? "i") ? "i",
+            OperParam("p"), OperParam("q"))
+          .typedOperDecl(types, "iiTOi")
+    ).typed(types, "i")
+  // UNCHANGED x
+  val ex6 = unchanged(name("x") ? "i").typed(types, "b")
+  // UNCHANGED <<x, y>>
+  val ex7 =
+    unchanged(tuple(name("x") ? "i", name("y") ? "i") ? "ii").typed(types, "b")
+  // [x |-> 1].x
+  val ex9 = appFun(enumFun(str("x"), int(1)) ? "rx", str("x")).typed(types, "i")
 
   val exs = List(
       ex1,
@@ -74,7 +101,6 @@ class TestSourceLocator extends FunSuite with TestingPredefs {
       ex5,
       ex6,
       ex7,
-      ex8,
       ex9
   )
 
@@ -130,13 +156,13 @@ class TestSourceLocator extends FunSuite with TestingPredefs {
   }
 
   test("Test ExplicitLetIn") {
-    val transformation = LetInExpander(tracker, keepNullary = false)
+    val transformation = new LetInExpander(tracker, keepNullary = false)
 
     testTransformation(transformation)
   }
 
   test("Test Flatten") {
-    val transformation = Flatten(tracker)
+    val transformation = Flatten(tracker)(Untyped())
 
     testTransformation(transformation)
   }
