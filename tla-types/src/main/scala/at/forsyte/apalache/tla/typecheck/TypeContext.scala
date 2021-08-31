39420d6522d5745d54518fb1607bb5452bbc9a81
Igor Konnov <igor@informal.systems>
2021-08-31 12:01:36 +0200

    a bit of refactoring after the peer review

--- tla-types/src/main/scala/at/forsyte/apalache/tla/typecheck/TypeContext.scala
+++ tla-types/src/main/scala/at/forsyte/apalache/tla/typecheck/TypeContext.scala
@@ -7,20 +7,29 @@ import scala.collection.immutable.SortedMap
 /**
  * A type context assigns poly- or monotypes to names (such as operator names and the names of quantified variables).
  *
- * @param poolSize the largest variable index that should be treated as non-free, that is, not universally quantified
- * @param types    a map from a name to the assigned typed and universal type variables
+ * @param largestVarIndex the largest variable index that should be treated as non-free, that is, not universally quantified; greater or equal to 0.
+ * @param types           a map from a name to the assigned typed and universal type variables
  * @author Igor Konnov
  */
-class TypeContext(val poolSize: Int, val types: Map[String, (TlaType1, Set[Int])]) {
+class TypeContext(val largestVarIndex: Int, val types: Map[String, (TlaType1, Set[Int])]) {
   def apply(name: String): (TlaType1, Set[Int]) = {
-    types(name)
+    types.getOrElse(name, new IllegalArgumentException(s"No type binding for $name in the type context"))
   }
 }
 
 object TypeContext {
-  val empty: TypeContext = new TypeContext(-1, Map.empty)
+
+  /**
+   * The value of the variable index that means that all variables can be quantified.
+   */
+  val NIL_VAR_INDEX: Int = -1
+
+  /**
+   * An empty type context.
+   */
+  val empty: TypeContext = new TypeContext(NIL_VAR_INDEX, Map.empty)
 
   def apply(namedTypes: (String, (TlaType1, Set[Int]))*): TypeContext = {
-    new TypeContext(-1, SortedMap(namedTypes: _*))
+    new TypeContext(NIL_VAR_INDEX, SortedMap(namedTypes: _*))
   }
 }
