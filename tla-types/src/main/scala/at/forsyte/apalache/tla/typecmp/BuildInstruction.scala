0d5fa19b9c8930a50cb1db4fd969e2e6a6e447e0
Kukovec <jure.kukovec@gmail.com>
2021-10-06 14:12:45 +0200

    err throwing

--- tla-types/src/main/scala/at/forsyte/apalache/tla/typecmp/BuildInstruction.scala
+++ tla-types/src/main/scala/at/forsyte/apalache/tla/typecmp/BuildInstruction.scala
@@ -5,7 +5,10 @@ import at.forsyte.apalache.tla.lir.oper.TlaOper
 
 sealed case class BuildInstruction(oper: TlaOper, typeCmp: typeComputation) {
   def build(args: TlaEx*): TlaEx = {
-    val typeTag = Typed(typeCmp(args))
-    OperEx(oper, args: _*)(typeTag)
+    typeCmp(args) match {
+      case Left(err) => throw err
+      case Right(typeRes) =>
+        OperEx(oper, args: _*)(Typed(typeRes))
+    }
   }
 }
