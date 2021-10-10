31ef8a3854ae75c312d3ca3e7a776574157dd129
Jonathan Lorimer <jonathan_lorimer@mac.com>
2021-10-10 10:45:42 -0400

    initial flake

---
+++ flake.nix
@@ -0,0 +1,57 @@
+{
+  description = "A symbolic model checker for TLA+";
+
+  inputs = {
+    # Nix Inputs
+    nixpkgs.url = github:nixos/nixpkgs/nixpkgs-unstable;
+    pre-commit-hooks.url = github:cachix/pre-commit-hooks.nix;
+    flake-utils.url = github:numtide/flake-utils;
+
+  };
+
+  outputs = { self, nixpkgs, pre-commit-hooks, flake-utils }:
+    with flake-utils.lib;
+    eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        # nix build
+        packages = {
+          dev-shell =
+            pkgs.mkShell {
+              shellHook = self.checks.${system}.pre-commit-check.shellHook;
+              buildInputs = with pkgs; [
+                jdk8
+                scala_2_12
+                metals
+                maven
+                scalafmt
+              ];
+            };
+        };
+
+        # nix flake check
+        checks = {
+          pre-commit-check = pre-commit-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+              nixpkgs-fmt.enable = true;
+              nix-linter.enable = true;
+              scalafmt = {
+                enable = true;
+                name = "scalafmt";
+                entry = "${pkgs.scalafmt}/bin/scalafmt";
+                files = "\\.scala$";
+                language = "system";
+                pass_filenames = false;
+              };
+            };
+          };
+        } // self.packages; # adding packages here ensures that every attr gets built on check
+
+        # nix develop
+        devShell = builtins.trace (builtins.attrNames self.packages) self.packages.${system}.dev-shell;
+      });
+}
+
