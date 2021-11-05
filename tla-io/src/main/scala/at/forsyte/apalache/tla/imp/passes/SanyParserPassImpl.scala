0b03308e7a23148c8aaea8a94ab12643c63a3d2e
Shon Feder <shon@informal.systems>
2021-11-05 12:15:47 -0400

    Fix output format detection
    Followup to #1073
    
    That fix introduced *another* regression test, which caused the of the
    parser to always go to TLA. :(

--- tla-io/src/main/scala/at/forsyte/apalache/tla/imp/passes/SanyParserPassImpl.scala
+++ tla-io/src/main/scala/at/forsyte/apalache/tla/imp/passes/SanyParserPassImpl.scala
@@ -83,16 +83,16 @@ class SanyParserPassImpl @Inject() (
 
         // write parser output to specified destination, if requested
         options.get[String]("parser", "output").foreach { output =>
-          val file = new File(output)
+          val outfile = new File(output)
 
-          if (filename.toLowerCase.endsWith(".tla")) {
+          if (outfile.toString.toLowerCase.endsWith(".tla")) {
             val moduleName = filename.substring(0, filename.length - ".tla".length)
             writerFactory.writeModuleToTla(rootModule.get.copy(name = moduleName), TlaWriter.STANDARD_MODULES,
-                Some(file))
-          } else if (filename.toLowerCase.endsWith(".json")) {
+                Some(outfile))
+          } else if (outfile.toString.toLowerCase.endsWith(".json")) {
             val moduleName = filename.substring(0, filename.length - ".json".length)
             writerFactory.writeModuleToJson(rootModule.get.copy(name = moduleName), TlaWriter.STANDARD_MODULES,
-                Some(file))
+                Some(outfile))
           } else {
             logger.error(s"  > Unrecognized file format: $filename. Supported formats: .tla and .json")
           }
