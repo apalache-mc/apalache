2271fff2ea5189cf11a613991f8ea09046c2c9cf
Shon Feder <shon@informal.systems>
2021-11-05 14:12:03 -0400

    Add output flag to check cmd

--- mod-tool/src/main/scala/at/forsyte/apalache/tla/tooling/opt/ParseCmd.scala
+++ mod-tool/src/main/scala/at/forsyte/apalache/tla/tooling/opt/ParseCmd.scala
@@ -14,6 +14,5 @@ class ParseCmd extends Command(name = "parse", description = "Parse a TLA+ speci
   // TODO Move these options to base trait (IoCmd) that can be exteded by both check and parse
   var file: File = arg[File](description = "a file containing a TLA+ specification (.tla or .json)")
   var output: Option[String] = opt[Option[String]](name = "output",
-      description = "filename where to output the parsed source (.tla or .json),\n" +
-        "default: None")
+      description = "file to which the parsed source is written (.tla or .json), default: None")
 }
