957e1ee86e3a2d9f0f52e6300b2991bcd0385fcb
Shon Feder <shon@informal.systems>
2021-11-05 18:44:11 -0400

    Fix z3 logger

--- tla-bmcmt/src/main/scala/at/forsyte/apalache/tla/bmcmt/smt/Z3SolverContext.scala
+++ tla-bmcmt/src/main/scala/at/forsyte/apalache/tla/bmcmt/smt/Z3SolverContext.scala
@@ -237,15 +237,20 @@ class Z3SolverContext(val config: SolverConfig) extends SolverContext {
     }
   }
 
-  private def initLog(): PrintWriter = {
-    // TODO Fix SMT logging to be able support logging to latest directory
-    val writer = OutputManager.printWriter(OutputManager.runDir, s"log$id.smt")
-    if (!config.debug) {
-      writer.println("Logging is disabled (Z3SolverContext.debug = false). Activate with --debug.")
-      writer.flush()
+  // TODO Fix SMT logging to be able support logging to latest directory
+  private def initLog(): PrintWriter =
+    OutputManager.runDirPathOpt match {
+      case None => new PrintWriter(NullOutputStream.NULL_OUTPUT_STREAM)
+      case Some(runDir) => {
+        val writer = OutputManager.printWriter(OutputManager.runDir, s"log$id.smt")
+        if (!config.debug) {
+          writer.println("Logging is disabled (Z3SolverContext.debug = false). Activate with --debug.")
+          writer.flush()
+        }
+        writer
+      }
     }
-    writer
-  }
+
 
   /**
    * Log message to the logging file. This is helpful to debug the SMT encoding.
