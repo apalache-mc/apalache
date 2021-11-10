8cf4c661809efd899d8717789f2f45842489ab72
rodrigo7491 <rodrigo7491@gmail.com>
2021-11-10 15:38:30 +0100

    add unit tests for arrays encoding

--- tla-bmcmt/src/test/scala/at/forsyte/apalache/tla/bmcmt/TestArenaWithArrays.scala
+++ tla-bmcmt/src/test/scala/at/forsyte/apalache/tla/bmcmt/TestArenaWithArrays.scala
@@ -1,5 +1,16 @@
 package at.forsyte.apalache.tla.bmcmt
 
-// TODO: Extend TestArena as development in the "arrays" encoding progresses
+import at.forsyte.apalache.tla.bmcmt.smt.{SolverConfig, Z3SolverContext}
+import org.junit.runner.RunWith
+import org.scalatest.Outcome
+import org.scalatest.junit.JUnitRunner
 
-class TestArenaWithArrays {}
+@RunWith(classOf[JUnitRunner])
+class TestArenaWithArrays extends TestArena {
+  override protected def withFixture(test: OneArgTest): Outcome = {
+    solver = new Z3SolverContext(SolverConfig.default.copy(debug = true, smtEncoding = arraysEncodingType))
+    val result = test()
+    solver.dispose()
+    result
+  }
+}
--- tla-bmcmt/src/test/scala/at/forsyte/apalache/tla/bmcmt/TestRewriterWithArrays.scala
+++ tla-bmcmt/src/test/scala/at/forsyte/apalache/tla/bmcmt/TestRewriterWithArrays.scala
@@ -1,5 +1,22 @@
 package at.forsyte.apalache.tla.bmcmt
 
+import at.forsyte.apalache.tla.bmcmt.smt.{PreproSolverContext, SolverConfig, Z3SolverContext}
+import org.junit.runner.RunWith
+import org.scalatest.Outcome
+import org.scalatest.junit.JUnitRunner
+
 // TODO: Extend rewriter tests gradually as development in the "arrays" encoding progresses
 
-class TestRewriterWithArrays {}
+@RunWith(classOf[JUnitRunner])
+class TestRewriterWithArrays
+    extends TestSymbStateRewriter with TestSymbStateRewriterInt with TestSymbStateRewriterSet
+    with TestSymbStateRewriterStr {
+  override protected def withFixture(test: OneArgTest): Outcome = {
+    solverContext = new PreproSolverContext(new Z3SolverContext(SolverConfig.default.copy(debug = true,
+                smtEncoding = arraysEncodingType)))
+    arena = Arena.create(solverContext)
+    val result = test(arraysEncodingType)
+    solverContext.dispose()
+    result
+  }
+}
