5205b2185ffeb00b10efd15ad4f0c2806eb35fb7
Igor Konnov <igor@informal.systems>
2022-02-02 14:25:54 +0100

    Update tla-io/src/main/scala/at/forsyte/apalache/io/annotations/AnnotationParserError.scala
    Co-authored-by: Shon Feder <shon@informal.systems>

--- tla-io/src/main/scala/at/forsyte/apalache/io/annotations/AnnotationParserError.scala
+++ tla-io/src/main/scala/at/forsyte/apalache/io/annotations/AnnotationParserError.scala
@@ -2,6 +2,6 @@ package at.forsyte.apalache.io.annotations
 
 class AnnotationParserError(message: String) extends Exception(message) {
   override def toString: String = {
-    "Annotation error: " + message
+    "Annotation syntax error: " + message
   }
 }
