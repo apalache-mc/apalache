7a78ffc153abfd268658626fc16a2650b32e2b6b
Thomas Pani <thomas@informal.systems>
2022-02-10 13:56:17 +0100

    Use Builder

--- tla-pp/src/test/scala/at/forsyte/apalache/tla/pp/TestTlaConstInliner.scala
+++ tla-pp/src/test/scala/at/forsyte/apalache/tla/pp/TestTlaConstInliner.scala
@@ -1,65 +1,67 @@
 package at.forsyte.apalache.tla.pp
 
-import at.forsyte.apalache.tla.lir.UntypedPredefs.untyped
 import at.forsyte.apalache.tla.lir.oper.TlaArithOper.{minus, plus}
 import at.forsyte.apalache.tla.lir.transformations.impl.IdleTracker
 import at.forsyte.apalache.tla.lir.values.TlaInt
 import at.forsyte.apalache.tla.lir._
+import at.forsyte.apalache.tla.lir.convenience.tla
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.{BeforeAndAfterEach, FunSuite, Matchers}
-
+import at.forsyte.apalache.tla.lir.UntypedPredefs._
 
 @RunWith(classOf[JUnitRunner])
 class TestTlaConstInliner extends FunSuite with BeforeAndAfterEach with Matchers {
   // some expressions to play with
-  private val primitiveValuedConst = NameEx("PrimitiveValuedConst")
-  private val nonPrimitiveValuedConst = NameEx("NonPrimitiveValuedConst")
-  private val nonConst = NameEx("x")
-  private val primitiveValue = ValEx(TlaInt(42))
+  private val primitiveValuedConst = tla.name("PrimitiveValuedConst")
+  private val nonPrimitiveValuedConst = tla.name("NonPrimitiveValuedConst")
+  private val nonConst = tla.name("x")
+  private val primitiveValue = tla.int(42)
   // module-level constant names
-  private val constants : Set[String] = Set(primitiveValuedConst.name, nonPrimitiveValuedConst.name)
+  private val constants: Set[String] = Set(primitiveValuedConst.name, nonPrimitiveValuedConst.name)
   // constants mapped to primitive values
-  private val constMap = Map(primitiveValuedConst.name -> primitiveValue, nonConst.name -> primitiveValue)
+  private val constMap = Map(primitiveValuedConst.name -> primitiveValue.untyped().asInstanceOf[ValEx],
+      nonConst.name -> primitiveValue.untyped().asInstanceOf[ValEx])
   // the inliner
-  private var constInliner : TlaConstInliner = _
+  private var constInliner: TlaConstInliner = _
 
   override def beforeEach(): Unit = {
     constInliner = new TlaConstInliner(new IdleTracker(), constants)
   }
-  
+
   test("inlines mapped NameEx constant") {
     val input = primitiveValuedConst
     val result = constInliner.replaceConstWithValue(constMap)(input)
-    result shouldBe ValEx(TlaInt(42))(Untyped())
+    result shouldBe tla.int(42).untyped()
   }
 
   test("passes unmapped NameEx unchanged") {
     val input = nonPrimitiveValuedConst
     val result = constInliner.replaceConstWithValue(constMap)(input)
-    result shouldBe input
+    result shouldBe input.untyped()
   }
 
   test("passes non-CONSTANT NameEx unchanged") {
     val input = nonConst
     val result = constInliner.replaceConstWithValue(constMap)(input)
-    result shouldBe input
+    result shouldBe input.untyped()
   }
 
   test("inlines nested OperEx") {
-    val input = OperEx(plus, nonConst, OperEx(minus, primitiveValuedConst, nonPrimitiveValuedConst))
+    val input = tla.plus(nonConst, tla.minus(primitiveValuedConst, nonPrimitiveValuedConst))
     val result = constInliner.replaceConstWithValue(constMap)(input)
     // should replace nested `primitiveValuedConst` with `primitiveValue`
-    result shouldBe OperEx(plus, nonConst, OperEx(minus, primitiveValue, nonPrimitiveValuedConst))
+    result shouldBe tla.plus(nonConst, tla.minus(primitiveValue, nonPrimitiveValuedConst)).untyped()
   }
 
   test("inlines LetInEx body and operator declaration") {
-    val body = OperEx(plus, nonConst, OperEx(minus, primitiveValuedConst, nonPrimitiveValuedConst))
-    val operDecl = TlaOperDecl("A", List.empty, OperEx(plus, primitiveValuedConst, nonPrimitiveValuedConst))
-    val input = LetInEx(body, operDecl)
+    val body = tla.plus(nonConst, tla.minus(primitiveValuedConst, nonPrimitiveValuedConst))
+    val operDecl = tla.declOp("A", tla.plus(primitiveValuedConst, nonPrimitiveValuedConst))
+    val input = tla.letIn(body, operDecl.untypedOperDecl())
     val result = constInliner.replaceConstWithValue(constMap)(input)
     // should replace `primitiveValuedConst` with `primitiveValue` in body and operator declaration
-    result shouldBe LetInEx(OperEx(plus, nonConst, OperEx(minus, primitiveValue, nonPrimitiveValuedConst)),
-      TlaOperDecl("A", List.empty, OperEx(plus, primitiveValue, nonPrimitiveValuedConst)))
+    val expectedBody = tla.plus(nonConst, tla.minus(primitiveValue, nonPrimitiveValuedConst))
+    val expectedOperDecl = tla.declOp("A", tla.plus(primitiveValue, nonPrimitiveValuedConst)).untypedOperDecl()
+    result shouldBe tla.letIn(expectedBody, expectedOperDecl).untyped()
   }
 }
