aeb2c448ddd38d76d565a0a6f6d6562c0b04d6cb
Thomas Pani <thomas@informal.systems>
2022-02-11 10:04:32 +0100

    Add unit tests for TlaConstInliner.buildConstMap

--- tla-pp/src/test/scala/at/forsyte/apalache/tla/pp/TestTlaConstInliner.scala
+++ tla-pp/src/test/scala/at/forsyte/apalache/tla/pp/TestTlaConstInliner.scala
@@ -15,15 +15,20 @@ class TestTlaConstInliner extends FunSuite with BeforeAndAfterEach with Matchers
   private val nonPrimitiveValuedConst = tla.name("NonPrimitiveValuedConst")
   private val nonConst = tla.name("x")
   private val primitiveValue = tla.int(42)
+  private val otherPrimitiveValue = tla.int(5)
   // module-level constant names
   private val constants: Set[String] = Set(primitiveValuedConst.name, nonPrimitiveValuedConst.name)
   // constants mapped to primitive values
-  private val constMap = Map(primitiveValuedConst.name -> primitiveValue.untyped().asInstanceOf[ValEx],
-      nonConst.name -> primitiveValue.untyped().asInstanceOf[ValEx])
+  private val emptyConstMap = Map[String, ValEx]()
+  private val constMap = Map(
+      primitiveValuedConst.name -> primitiveValue.untyped().asInstanceOf[ValEx],
+      nonConst.name -> primitiveValue.untyped().asInstanceOf[ValEx],
+  )
   // declare the reference to the inliner for use in each test case, initialized to the default value
   private var constInliner: TlaConstInliner = _
 
   override def beforeEach(): Unit = {
+    // Initialize the inliner with `constants` as module-level constants and an idle tracker.
     constInliner = TlaConstInliner(new IdleTracker(), constants)
   }
 
@@ -101,5 +106,47 @@ class TestTlaConstInliner extends FunSuite with BeforeAndAfterEach with Matchers
     val expectedBody = body
     val expectedOperDecl = operDecl.untypedOperDecl()
     result shouldBe tla.letIn(expectedBody, expectedOperDecl).untyped()
+  }
+
+  test("buildConstMap does not extract non-module constants") {
+    val input = tla.eql(nonConst, primitiveValue)
+    val result = constInliner.buildConstMap(emptyConstMap)(input)
+    result shouldBe empty
+  }
+
+  test("buildConstMap extracts constant from equality with empty initial map") {
+    val input = tla.eql(primitiveValuedConst, primitiveValue)
+    val result = constInliner.buildConstMap(emptyConstMap)(input)
+    result should contain only (primitiveValuedConst.name -> primitiveValue.untyped())
+  }
+
+  test("buildConstMap extracts constant from equality already present in initial map") {
+    val input = tla.eql(primitiveValuedConst, primitiveValue)
+    val result = constInliner.buildConstMap(constMap)(input)
+    result shouldBe constMap
+  }
+
+  test("buildConstMap throws extracting constant with different value in initial map") {
+    val input = tla.eql(primitiveValuedConst, otherPrimitiveValue)
+    a[TlaInputError] should be thrownBy constInliner.buildConstMap(constMap)(input)
+  }
+
+  test("buildConstMap extracts constants from nested equalities") {
+    val input = tla.and(tla.eql(primitiveValuedConst, primitiveValue), tla.eql(nonConst, otherPrimitiveValue))
+    val result = constInliner.buildConstMap(emptyConstMap)(input)
+    result should contain only (primitiveValuedConst.name -> primitiveValue.untyped())
+  }
+
+  test("buildConstMap extracts constants from LET IN expressions") {
+    val body = tla.eql(primitiveValuedConst, primitiveValue)
+    val operDecl = tla.declOp("A", tla.plus(nonConst, nonPrimitiveValuedConst))
+    val input = tla.letIn(body, operDecl.untypedOperDecl())
+    val result = constInliner.buildConstMap(emptyConstMap)(input)
+    result should contain only (primitiveValuedConst.name -> primitiveValue.untyped())
+  }
+
+  test("buildConstMap doesn't extract constants from NameEx and ValEx") {
+    constInliner.buildConstMap(emptyConstMap)(primitiveValuedConst) shouldBe empty
+    constInliner.buildConstMap(emptyConstMap)(primitiveValue) shouldBe empty
   }
 }
