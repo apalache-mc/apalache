dacb3e480dfb86ebfebcbaa2823e17c7def15403
Thomas Pani <thomas@informal.systems>
2022-02-15 13:36:15 +0100

    Add language predicate for Keramelizer input fragment

---
+++ tlair/src/test/scala/at/forsyte/apalache/tla/lir/transformations/standard/TestKeramelizerInputLanguagePred.scala
@@ -0,0 +1,185 @@
+package at.forsyte.apalache.tla.lir.transformations.standard
+
+import at.forsyte.apalache.tla.lir._
+import at.forsyte.apalache.tla.lir.convenience._
+import at.forsyte.apalache.tla.lir.values.{TlaIntSet, TlaNatSet}
+import at.forsyte.apalache.tla.lir.UntypedPredefs._
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class TestKeramelizerInputLanguagePred extends LanguagePredTestSuite {
+  private val pred = new KeramelizerInputLanguagePred
+
+  import tla._
+
+  /**
+   * ****************** tests from [[KeraLanguagePred]], except where otherwise noted *****************************
+   */
+
+  test("accepts TLA+ constant expressions") {
+    expectOk(pred.isExprOk(name("x")))
+    expectOk(pred.isExprOk(prime(name("x"))))
+    expectOk(pred.isExprOk(int(22)))
+    expectOk(pred.isExprOk(bool(false)))
+    expectOk(pred.isExprOk(str("foo")))
+    expectOk(pred.isExprOk(booleanSet()))
+    expectOk(pred.isExprOk(ValEx(TlaIntSet)))
+    expectOk(pred.isExprOk(ValEx(TlaNatSet)))
+  }
+
+  test("accepts TLA+ set expressions") {
+    expectOk(pred.isExprOk(cup(enumSet(int(1)), enumSet(int(2)))))
+    expectOk(pred.isExprOk(in(int(1), enumSet(int(2)))))
+    expectOk(pred.isExprOk(enumSet(int(1))))
+    expectOk(pred.isExprOk(subseteq(enumSet(int(1)), enumSet(int(2)))))
+    expectOk(pred.isExprOk(filter(name("x"), enumSet(int(1)), bool(false))))
+    expectOk(pred.isExprOk(map(int(2), name("x"), enumSet(int(1)))))
+
+    expectOk(pred.isExprOk(powSet(enumSet(int(1), int(2)))))
+    expectOk(pred.isExprOk(union(enumSet(enumSet(int(1), int(2))))))
+    expectOk(pred.isExprOk(dotdot(int(10), int(20))))
+
+    // in TLA+, but not in KerA+
+    expectOk(pred.isExprOk(cap(enumSet(int(1)), enumSet(int(2)))))
+    expectOk(pred.isExprOk(setminus(enumSet(int(1)), enumSet(int(2)))))
+    expectOk(pred.isExprOk(notin(int(1), enumSet(int(2)))))
+  }
+
+  test("accepts TLA+ finite set expressions") {
+    expectOk(pred.isExprOk(isFin(enumSet(int(1)))))
+    expectOk(pred.isExprOk(card(enumSet(int(1)))))
+  }
+
+  test("accepts TLA+ function expressions") {
+    expectOk(pred.isExprOk(funDef(tla.int(1), name("x"), name("S"))))
+    expectOk(pred.isExprOk(appFun(name("f"), int(3))))
+    expectOk(pred.isExprOk(except(name("f"), int(3), bool(true))))
+    expectOk(pred.isExprOk(funSet(name("X"), name("Y"))))
+    expectOk(pred.isExprOk(dom(name("X"))))
+  }
+
+  test("accepts TLA+ tuples, records, sequences") {
+    expectOk(pred.isExprOk(tuple(int(1), bool(true))))
+    expectOk(pred.isExprOk(enumFun(str("a"), bool(true))))
+    expectOk(pred.isExprOk(head(tuple(int(1), int(2)))))
+    expectOk(pred.isExprOk(tail(tuple(int(1), int(2)))))
+    expectOk(pred.isExprOk(subseq(tuple(int(1), int(2)), int(3), int(4))))
+    expectOk(pred.isExprOk(len(tuple(int(1), int(2)))))
+    expectOk(pred.isExprOk(append(tuple(int(1), int(2)), tuple(int(2), int(3)))))
+
+    // in TLA+, but not in KerA+
+    expectOk(pred.isExprOk(recSet(name("a"), name("A"))))
+    expectOk(pred.isExprOk(times(name("X"), name("Y"))))
+  }
+
+  test("accepts TLA+ miscellania") {
+    expectOk(pred.isExprOk(label(int(2), "a")))
+    expectOk(pred.isExprOk(label(int(2), "a", "b")))
+  }
+
+  test("accepts TLA+ integer expressions") {
+    expectOk(pred.isExprOk(lt(int(1), int(3))))
+    expectOk(pred.isExprOk(le(int(1), int(3))))
+    expectOk(pred.isExprOk(gt(int(1), int(3))))
+    expectOk(pred.isExprOk(ge(int(1), int(3))))
+    expectOk(pred.isExprOk(plus(int(1), int(3))))
+    expectOk(pred.isExprOk(minus(int(1), int(3))))
+    expectOk(pred.isExprOk(mult(int(1), int(3))))
+    expectOk(pred.isExprOk(div(int(1), int(3))))
+    expectOk(pred.isExprOk(mod(int(1), int(3))))
+    expectOk(pred.isExprOk(exp(int(2), int(3))))
+    expectOk(pred.isExprOk(uminus(int(2))))
+  }
+
+  test("accepts TLA+ control expressions") {
+    expectOk(pred.isExprOk(ite(name("p"), int(1), int(2))))
+
+    // in TLA+, but not in KerA+
+    expectOk(pred.isExprOk(caseOther(int(1), bool(false), int(2))))
+    expectOk(pred.isExprOk(caseSplit(bool(false), int(2))))
+  }
+
+  test("accepts TLA+ logic expressions") {
+    expectOk(pred.isExprOk(or(bool(false), name("b"))))
+    expectOk(pred.isExprOk(and(bool(false), name("b"))))
+
+    // in TLA+, but not in KerA+
+    expectOk(pred.isExprOk(equiv(bool(false), name("b"))))
+    expectOk(pred.isExprOk(impl(bool(false), name("b"))))
+    expectOk(pred.isExprOk(neql(bool(false), name("b"))))
+  }
+
+  /**
+   * **************************** additional tests for [[KeramelizerInputLanguagePred]] ********************************************
+   */
+  test("accepts binding expressions where first argument is a name") {
+    expectOk(pred.isExprOk(exists(name("a"), bool(true))))
+    expectOk(pred.isExprOk(forall(name("a"), bool(true))))
+    expectOk(pred.isExprOk(choose(name("a"), name("a"), bool(true))))
+    expectOk(pred.isExprOk(filter(name("a"), name("a"), bool(true))))
+  }
+
+  test("rejects binding expressions where first argument is not a name") {
+    expectFail(pred.isExprOk(exists(int(1), name("A"), bool(true))))
+    expectFail(pred.isExprOk(exists(str("a"), name("A"), bool(true))))
+    expectFail(pred.isExprOk(forall(int(1), name("A"), bool(true))))
+    expectFail(pred.isExprOk(forall(str("a"), name("A"), bool(true))))
+    expectFail(pred.isExprOk(choose(int(1), name("a"), bool(true))))
+    expectFail(pred.isExprOk(choose(str("a"), name("a"), bool(true))))
+    expectFail(pred.isExprOk(filter(int(1), name("a"), bool(true))))
+    expectFail(pred.isExprOk(filter(str("a"), name("a"), bool(true))))
+  }
+
+  test("rejects operator application where first argument is not a name") {
+    expectFail(pred.isExprOk(appOp(int(1))))
+  }
+
+  test("rejects Seq(_)") {
+    expectFail(pred.isExprOk(seqSet(name("A"))))
+  }
+
+  /**
+   * **************************** tests from [[TestFlatLanguagePred]] ********************************************
+   */
+
+  test("a call to a user operator") {
+    val expr = enumSet(int(1), str("abc"), bool(false))
+    val app = appOp(name("UserOp"), expr)
+    expectFail(pred.isExprOk(app))
+  }
+
+  test("a non-nullary let-in ") {
+    val app = appOp(name("UserOp"), int(3))
+    val letInDef = letIn(app, declOp("UserOp", plus(int(1), name("x")), OperParam("x")).untypedOperDecl())
+    expectFail(pred.isExprOk(letInDef))
+  }
+
+  test("a nullary let-in ") {
+    val app = appOp(name("UserOp"))
+    val letInDef = letIn(app, declOp("UserOp", plus(int(1), int(2))).untypedOperDecl())
+    expectOk(pred.isExprOk(letInDef))
+  }
+
+  test("nested nullary let-in ") {
+    val app = plus(appOp(name("A")), appOp(name("B")))
+    val letInDef = letIn(app, declOp("A", plus(int(1), int(2))).untypedOperDecl())
+    val outerLetIn =
+      letIn(letInDef, declOp("B", int(3)).untypedOperDecl())
+    expectOk(pred.isExprOk(outerLetIn))
+  }
+
+  test("a call to a user operator in a module") {
+    val appB = appOp(name("B"), int(1))
+    val defA = declOp("A", appB)
+    val mod = new TlaModule("mod", Seq(defA))
+    expectFail(pred.isModuleOk(mod))
+  }
+
+  test("a module without calls") {
+    val appB = int(1)
+    val defA = declOp("A", appB)
+    val mod = new TlaModule("mod", Seq(defA))
+    expectOk(pred.isModuleOk(mod))
+  }
+}
