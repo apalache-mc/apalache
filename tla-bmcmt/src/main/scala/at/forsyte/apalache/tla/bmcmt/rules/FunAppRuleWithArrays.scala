36059fbc0a3a146d51520782023dd42062b6e554
rodrigo7491 <rodrigo7491@gmail.com>
2022-03-07 09:58:10 +0100

    fix quantifier use on funApp with arrays

--- tla-bmcmt/src/main/scala/at/forsyte/apalache/tla/bmcmt/rules/FunAppRuleWithArrays.scala
+++ tla-bmcmt/src/main/scala/at/forsyte/apalache/tla/bmcmt/rules/FunAppRuleWithArrays.scala
@@ -32,49 +32,79 @@ class FunAppRuleWithArrays(rewriter: SymbStateRewriter) extends FunAppRule(rewri
     val res = nextState.arena.topCell
 
     if (nElems > 0) {
-      // We use an oracle to pick and arg for which the function is applied
-      val (oracleState, oracle) = picker.oracleFactory.newDefaultOracle(nextState, nElems + 1)
-      nextState = picker.pickByOracle(oracleState, oracle, relationElems, oracleState.arena.cellTrue().toNameEx)
-      val pickedElem = nextState.asCell
+      var comparableArgCell = false
 
-      assert(nextState.arena.getHas(pickedElem).size == 2)
-      val pickedArg = nextState.arena.getHas(pickedElem)(0)
-      val pickedRes = nextState.arena.getHas(pickedElem)(1)
+      // We compare argCell with relationElems and if an equality is found we simply propagate elemRes
+      for (elem <- relationElems) {
+        val elemTuple = nextState.arena.getHas(elem)
+        assert(elemTuple.size == 2) // elem should always have only edges to <arg,res>
+        val elemArg = elemTuple(0)
+        val elemRes = elemTuple(1)
+        if (argCell == elemArg) {
+          comparableArgCell = true
+          // If argCell is comparable at the Scala level, we generate SMT constraints based on it
+          val select = tla.apalacheSelectInFun(elemArg.toNameEx, funCell.toNameEx)
+          val eql = tla.eql(res.toNameEx, select)
+          rewriter.solverContext.assertGroundExpr(eql)
 
-      // Cache the equality between the picked argument and the supplied argument, O(1) constraints
-      nextState = rewriter.lazyEq.cacheEqConstraints(nextState, Seq((pickedArg, argCell)))
-      // If oracle < N, then pickedArg = argCell
-      val pickedElemInDom = tla.not(oracle.whenEqualTo(nextState, nElems))
-      val argEq = tla.eql(pickedArg.toNameEx, argCell.toNameEx)
-      val argEqWhenNonEmpty = tla.impl(pickedElemInDom, argEq)
-      rewriter.solverContext.assertGroundExpr(argEqWhenNonEmpty)
+          nextState = nextState.updateArena(_.appendHasNoSmt(res, nextState.arena.getHas(elemRes): _*))
 
-      // We require oracle = N iff there is no element equal to argCell, O(N) constraints
-      for ((elem, no) <- relationElems.zipWithIndex) {
-        val elemArg = nextState.arena.getHas(elem).head
-        nextState = rewriter.lazyEq.cacheEqConstraints(nextState, Seq((elemArg, argCell)))
-        val inDom = tla.apalacheSelectInSet(elemArg.toNameEx, domainCell.toNameEx)
-        val neqArg = tla.not(rewriter.lazyEq.safeEq(elemArg, argCell))
-        val found = tla.not(oracle.whenEqualTo(nextState, nElems))
-        // ~inDom \/ neqArg \/ found, or equivalently, (inDom /\ elemArg = argCell) => found
-        rewriter.solverContext.assertGroundExpr(tla.or(tla.not(inDom), neqArg, found))
-        // oracle = i => inDom. The equality pickedArg = argCell is enforced by argEqWhenNonEmpty
-        rewriter.solverContext.assertGroundExpr(tla.or(tla.not(oracle.whenEqualTo(nextState, no)), inDom))
+          if (elemRes.cellType.isInstanceOf[FunT] || elemRes.cellType.isInstanceOf[FinFunSetT]) {
+            nextState = nextState.updateArena(_.setDom(res, nextState.arena.getDom(elemRes)))
+            nextState = nextState.updateArena(_.setCdm(res, nextState.arena.getCdm(elemRes)))
+          } else if (elemRes.cellType.isInstanceOf[RecordT]) {
+            // Records do not contain cdm metadata
+            nextState = nextState.updateArena(_.setDom(res, nextState.arena.getDom(elemRes)))
+          }
+        }
       }
 
-      // We simply apply the picked arg to the SMT array encoding the function, O(1) constraints
-      val select = tla.apalacheSelectInFun(argCell.toNameEx, funCell.toNameEx)
-      val eql = tla.eql(res.toNameEx, select)
-      rewriter.solverContext.assertGroundExpr(eql)
+      // If argCell is not comparable at the Scala level, e.g., due to quantifier use, we need to use an oracle
+      if (!comparableArgCell) {
+        // We use an oracle to pick an arg for which the function is applied
+        val (oracleState, oracle) = picker.oracleFactory.newDefaultOracle(nextState, nElems + 1)
+        nextState = picker.pickByOracle(oracleState, oracle, relationElems, oracleState.arena.cellTrue().toNameEx)
+        val pickedElem = nextState.asCell
 
-      // The edges, dom, and cdm are forwarded below
-      nextState = nextState.updateArena(_.appendHasNoSmt(res, nextState.arena.getHas(pickedRes): _*))
-      if (pickedRes.cellType.isInstanceOf[FunT] || pickedRes.cellType.isInstanceOf[FinFunSetT]) {
-        nextState = nextState.updateArena(_.setDom(res, nextState.arena.getDom(pickedRes)))
-        nextState = nextState.updateArena(_.setCdm(res, nextState.arena.getCdm(pickedRes)))
-      } else if (pickedRes.cellType.isInstanceOf[RecordT]) {
-        // Records do not contain cdm metadata
-        nextState = nextState.updateArena(_.setDom(res, nextState.arena.getDom(pickedRes)))
+        assert(nextState.arena.getHas(pickedElem).size == 2)
+        val pickedArg = nextState.arena.getHas(pickedElem)(0)
+        val pickedRes = nextState.arena.getHas(pickedElem)(1)
+
+        // Cache the equality between the picked argument and the supplied argument, O(1) constraints
+        nextState = rewriter.lazyEq.cacheEqConstraints(nextState, Seq((pickedArg, argCell)))
+        // If oracle < N, then pickedArg = argCell
+        val pickedElemInDom = tla.not(oracle.whenEqualTo(nextState, nElems))
+        val argEq = tla.eql(pickedArg.toNameEx, argCell.toNameEx)
+        val argEqWhenNonEmpty = tla.impl(pickedElemInDom, argEq)
+        rewriter.solverContext.assertGroundExpr(argEqWhenNonEmpty)
+
+        // We require oracle = N iff there is no element equal to argCell, O(N) constraints
+        for ((elem, no) <- relationElems.zipWithIndex) {
+          val elemArg = nextState.arena.getHas(elem).head
+          nextState = rewriter.lazyEq.cacheEqConstraints(nextState, Seq((elemArg, argCell)))
+          val inDom = tla.apalacheSelectInSet(elemArg.toNameEx, domainCell.toNameEx)
+          val neqArg = tla.not(rewriter.lazyEq.safeEq(elemArg, argCell))
+          val found = tla.not(oracle.whenEqualTo(nextState, nElems))
+          // ~inDom \/ neqArg \/ found, or equivalently, (inDom /\ elemArg = argCell) => found
+          rewriter.solverContext.assertGroundExpr(tla.or(tla.not(inDom), neqArg, found))
+          // oracle = i => inDom. The equality pickedArg = argCell is enforced by argEqWhenNonEmpty
+          rewriter.solverContext.assertGroundExpr(tla.or(tla.not(oracle.whenEqualTo(nextState, no)), inDom))
+        }
+
+        // We simply apply the picked arg to the SMT array encoding the function, O(1) constraints
+        val select = tla.apalacheSelectInFun(argCell.toNameEx, funCell.toNameEx)
+        val eql = tla.eql(res.toNameEx, select)
+        rewriter.solverContext.assertGroundExpr(eql)
+
+        // The edges, dom, and cdm are forwarded below
+        nextState = nextState.updateArena(_.appendHasNoSmt(res, nextState.arena.getHas(pickedRes): _*))
+        if (pickedRes.cellType.isInstanceOf[FunT] || pickedRes.cellType.isInstanceOf[FinFunSetT]) {
+          nextState = nextState.updateArena(_.setDom(res, nextState.arena.getDom(pickedRes)))
+          nextState = nextState.updateArena(_.setCdm(res, nextState.arena.getCdm(pickedRes)))
+        } else if (pickedRes.cellType.isInstanceOf[RecordT]) {
+          // Records do not contain cdm metadata
+          nextState = nextState.updateArena(_.setDom(res, nextState.arena.getDom(pickedRes)))
+        }
       }
     }
 
