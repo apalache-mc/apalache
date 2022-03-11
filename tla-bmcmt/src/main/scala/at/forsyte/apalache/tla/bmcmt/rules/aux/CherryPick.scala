c85e6fd9ab4ceacd97435dd3cdb7a794021b0267
Thomas Pani <thomas@informal.systems>
2022-03-11 11:59:48 +0100

    Annotate unused parameter with @unused
    Kept for argument compatibility with calls at callsite

--- tla-bmcmt/src/main/scala/at/forsyte/apalache/tla/bmcmt/rules/aux/CherryPick.scala
+++ tla-bmcmt/src/main/scala/at/forsyte/apalache/tla/bmcmt/rules/aux/CherryPick.scala
@@ -12,6 +12,7 @@ import at.forsyte.apalache.tla.lir.oper.TlaOper
 
 import scala.collection.immutable.SortedMap
 import at.forsyte.apalache.tla.typecheck.ModelValueHandler
+import scalaz.unused
 
 /**
  * An element picket that allows us:
@@ -254,7 +255,7 @@ class CherryPick(rewriter: SymbStateRewriter) {
    *   a new symbolic state with the expression holding a fresh cell that stores the picked element.
    */
   def pickRecord(
-      cellTypeToIgnore: CellT,
+      @unused cellTypeToIgnore: CellT,
       state: SymbState,
       oracle: Oracle,
       records: Seq[ArenaCell],
