a5634ad2bb850bc2be9535b363aae1ed9f20cb12
Thomas Pani <thomas@informal.systems>
2022-03-11 09:45:23 +0100

    Call function with parentheses

--- tla-io/src/main/scala/at/forsyte/apalache/io/OutputManager.scala
+++ tla-io/src/main/scala/at/forsyte/apalache/io/OutputManager.scala
@@ -52,7 +52,7 @@ object OutputManager extends LazyLogging {
   }
 
   /* This should only ever be set if the IntermediateFlag is true */
-  private def setIntermediateDir: Unit = {
+  private def setIntermediateDir(): Unit = {
     intermediateDirOpt = Some(runDir.resolve(IntermediateFoldername))
   }
 
@@ -128,7 +128,7 @@ object OutputManager extends LazyLogging {
     setCustomRunDir(config.runDir)
 
     if (cfg.writeIntermediate) {
-      setIntermediateDir
+      setIntermediateDir()
       intermediateDirOpt.foreach(ensureDirExists)
       customIntermediateRunDir.foreach(ensureDirExists)
     }
