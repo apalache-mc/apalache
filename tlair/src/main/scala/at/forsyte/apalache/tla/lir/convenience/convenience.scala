9abed634a851df55a441627fe85e43bec42ded8e
Thomas Pani <thomas@informal.systems>
2022-03-14 11:17:38 +0100

    Remove unused implicit conversions

--- tlair/src/main/scala/at/forsyte/apalache/tla/lir/convenience/convenience.scala
+++ tlair/src/main/scala/at/forsyte/apalache/tla/lir/convenience/convenience.scala
@@ -1,7 +1,5 @@
 package at.forsyte.apalache.tla.lir
 
-import at.forsyte.apalache.tla.lir.oper.{FixedArity, OperArity}
-
 /**
  * Convenient shortcuts and definitions, mostly used in tests. Import them, when needed.
  */
@@ -11,8 +9,4 @@ package object convenience {
    * This is just a short-hand to Builder, so one can type more naturally, e.g., tla.plus(tla.int(2), tla.int(3))
    */
   val tla: Builder = new Builder()
-
-  implicit def makeUID(id: Int): UID = UID(id)
-
-  implicit def opArity(n: Int): OperArity = FixedArity(n)
 }
