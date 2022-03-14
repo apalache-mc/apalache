c1f68fc276df7f1eb127bbe5ccb8f1a856924aa2
Thomas Pani <thomas@informal.systems>
2022-03-14 10:05:13 +0100

    Use withFixture(NoArgTest) for encoding-dependent tests
    The previous solution with `OneArgTest` causes compiler warnings of the
    shape
    
        Adaptation of argument list by inserting () is deprecated: this is
        unlikely to be what you want.
    
    because of the implicit (unused) fixture parameter of type `Any`.

--- tla-bmcmt/src/test/scala/at/forsyte/apalache/tla/bmcmt/TestArena.scala
+++ tla-bmcmt/src/test/scala/at/forsyte/apalache/tla/bmcmt/TestArena.scala
@@ -2,14 +2,16 @@ package at.forsyte.apalache.tla.bmcmt
 
 import at.forsyte.apalache.tla.bmcmt.smt.Z3SolverContext
 import at.forsyte.apalache.tla.bmcmt.types.{BoolT, FinSetT, UnknownT}
-import org.scalatest.funsuite.FixtureAnyFunSuite
+import org.scalatest.funsuite.AnyFunSuite
 
-trait TestArena extends FixtureAnyFunSuite {
-  protected type FixtureParam = Any
-
+/**
+ * [[Arena]] tests. Override [[AnyFunSuite.withFixture()]] to set up specific solver contexts (e.g., for different
+ * encodings).
+ */
+trait TestArena extends AnyFunSuite {
   protected var solver: Z3SolverContext = _
 
-  test("create cells") { _ =>
+  test("create cells") {
     val emptyArena = Arena.create(solver)
     val arena = emptyArena.appendCell(UnknownT())
     assert(emptyArena.cellCount + 1 == arena.cellCount)
@@ -19,7 +21,7 @@ trait TestArena extends FixtureAnyFunSuite {
     assert(BoolT() == arena2.topCell.cellType)
   }
 
-  test("add 'has' edges") { _ =>
+  test("add 'has' edges") {
     val arena = Arena.create(solver).appendCell(FinSetT(UnknownT()))
     val set = arena.topCell
     val arena2 = arena.appendCell(BoolT())
@@ -28,7 +30,7 @@ trait TestArena extends FixtureAnyFunSuite {
     assert(List(elem) == arena3.getHas(set))
   }
 
-  test("BOOLEAN has FALSE and TRUE") { _ =>
+  test("BOOLEAN has FALSE and TRUE") {
     val arena = Arena.create(solver)
     assert(List(arena.cellFalse(), arena.cellTrue()) == arena.getHas(arena.cellBooleanSet()))
   }
