e42c54f1c4ad362cbd905f1db01bb3b566870e43
Thomas Pani <thomas@informal.systems>
2022-03-21 17:40:55 +0100

    Look up modules located next to the file supplied on CLI (#1491)
    Look up modules in the directory containing the main TLA+ file supplied
    on CLI. Drops support for lookup in `TLA-Library`, unless the main file
    has no parent directory. Drops support for `TLA_PATH`, since it becomes
    virtually useless.
    
    Mirrors the behavior of TLC since
    https://github.com/tlaplus/tlaplus/commit/b905a86e31714bf5002290141b51b41ffe280c8e
    
    This also adds a separate dummy module `DummyForIntegrationTests.tla` to the
    Apalache standard library, for use with integration tests.

---
+++ src/tla/DummyForIntegrationTests.tla
@@ -0,0 +1,3 @@
+----------------------- MODULE DummyForIntegrationTests -----------------------
+(* This is a dummy module for use with integration tests. *)
+===============================================================================
--- tla-io/src/main/scala/at/forsyte/apalache/tla/imp/SanyNameToStream.scala
+++ tla-io/src/main/scala/at/forsyte/apalache/tla/imp/SanyNameToStream.scala
@@ -8,11 +8,37 @@ import java.io.File
  * A decorator of SANY's SimpleFilenameToStream that rewires some names to Apalache names.
  *
  * @author
- *   Igor Konnov
+ *   Igor Konnov, Thomas Pani
  */
-class SanyNameToStream extends SimpleFilenameToStream {
+trait SanyNameToStream extends SimpleFilenameToStream {
   override def resolve(name: String, isModule: Boolean): File = {
     val wiredOrOriginalName = StandardLibrary.wiredModules.getOrElse(name, name)
     super.resolve(wiredOrOriginalName, isModule)
   }
+}
+
+/**
+ * A decorator of SANY's SimpleFilenameToStream that rewires some names to Apalache names.
+ */
+object SanyNameToStream {
+
+  /**
+   * Construct a filename resolver that looks for files (in this order) in
+   *   1. the library paths in the `TLA-Library` Java system variable,
+   *   1. SANY's base path (package `tla2sany.StandardModules` in `tla2tools.jar`)
+   *
+   * @see
+   *   [docs/apalache/running.md#module-lookup]
+   */
+  def apply() = new SimpleFilenameToStream() with SanyNameToStream
+
+  /**
+   * Construct a filename resolver that looks for files (in this order) in
+   *   1. the path given by parameter `libraryPath`, and
+   *   1. SANY's base path (package `tla2sany.StandardModules` in `tla2tools.jar`)
+   *
+   * @see
+   *   [docs/apalache/running.md#module-lookup]
+   */
+  def apply(libraryPath: String) = new SimpleFilenameToStream(libraryPath) with SanyNameToStream
 }
