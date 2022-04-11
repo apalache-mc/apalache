17b1861a97aa39f71495bf819c9614d09691d0f5
Shon Feder <shon@informal.systems>
2022-04-11 18:45:13 -0400

    Use view before mapValues

--- tla-types/src/main/scala/at/forsyte/apalache/tla/typecheck/etc/EtcTypeChecker.scala
+++ tla-types/src/main/scala/at/forsyte/apalache/tla/typecheck/etc/EtcTypeChecker.scala
@@ -261,7 +261,7 @@ class EtcTypeChecker(varPool: TypeVarPool, inferPolytypes: Boolean = true) exten
 
         // translate the binders in the lambda expression, so we can quickly propagate the types of the parameters
         val preCtx =
-          new TypeContext((ctx.types + (name -> operScheme))
+          new TypeContext((ctx.types + (name -> operScheme)).view
                 .mapValues(p => p.copy(approxSolution.subRec(p.principalType)))
                 .toMap)
         val extCtx = translateBinders(preCtx, letInSolver, binders)
