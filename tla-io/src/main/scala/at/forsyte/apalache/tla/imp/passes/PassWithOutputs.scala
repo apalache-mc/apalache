a42e0e9f2f5536a18a5933c9bbba0682c947a07f
Kukovec <jure.kukovec@gmail.com>
2022-04-11 16:40:43 +0200

    TODO notes

--- tla-io/src/main/scala/at/forsyte/apalache/tla/imp/passes/PassWithOutputs.scala
+++ tla-io/src/main/scala/at/forsyte/apalache/tla/imp/passes/PassWithOutputs.scala
@@ -11,6 +11,8 @@ import at.forsyte.apalache.tla.lir.TlaModule
  *   Jure Kukovec
  */
 trait PassWithOutputs extends Pass {
+  // TODO: This should get called automatically in PassChainExecutor after the refactoring associated with #858.
+  //        Currently blocked by cyclic dependencies
   def writeOut(writerFactory: TlaWriterFactory, module: TlaModule): Unit = {
     val passNumString = f"${passNumber}%02d" // Leading 0s
     writerFactory.writeModuleAllFormats(module.copy(name = s"${passNumString}_Out${name}"), TlaWriter.STANDARD_MODULES)
