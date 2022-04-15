dfe6072f3f957973c0acd90c9b4d4bae74972b84
Thomas Pani <thomas@informal.systems>
2022-04-15 18:52:06 +0200

    Remove unused parameter `ModelCheckerParams.saveDir`

--- tla-bmcmt/src/main/scala/at/forsyte/apalache/tla/bmcmt/passes/BoundedCheckerPassImpl.scala
+++ tla-bmcmt/src/main/scala/at/forsyte/apalache/tla/bmcmt/passes/BoundedCheckerPassImpl.scala
@@ -65,11 +65,10 @@ class BoundedCheckerPassImpl @Inject() (
     val stepsBound = options.getOrElse[Int]("checker", "length", 10)
     val tuning = options.getOrElse[Map[String, String]]("general", "tuning", Map[String, String]())
     val debug = options.getOrElse[Boolean]("general", "debug", false)
-    val saveDir = options.getOrError[Path]("io", "outdir").toFile
     // TODO: default smtEncoding option is needed here for executions with TestCmd, add encoding option to TestCmd instead
     val smtEncoding = options.getOrElse[SMTEncoding]("checker", "smt-encoding", oopsla19Encoding)
 
-    val params = new ModelCheckerParams(input, stepsBound, saveDir, tuning, debug)
+    val params = new ModelCheckerParams(input, stepsBound, tuning, debug)
     params.discardDisabled = options.getOrElse[Boolean]("checker", "discardDisabled", true)
     params.checkForDeadlocks = !options.getOrElse[Boolean]("checker", "noDeadlocks", false)
     params.nMaxErrors = options.getOrElse[Int]("checker", "maxError", 1)
