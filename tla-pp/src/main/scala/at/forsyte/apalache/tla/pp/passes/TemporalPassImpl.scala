86f558b67411c57f4218508ac89d5a81784607a4
Philip Offtermatt <p.offtermatt@gmail.com>
2022-05-05 13:05:37 +0200

    Add temporal encoding pass skeleton

---
+++ tla-pp/src/main/scala/at/forsyte/apalache/tla/pp/passes/TemporalPassImpl.scala
@@ -0,0 +1,47 @@
+package at.forsyte.apalache.tla.pp.passes
+
+import at.forsyte.apalache.infra.passes.PassOptions
+import at.forsyte.apalache.tla.lir.{ModuleProperty, TlaModule}
+import at.forsyte.apalache.io.lir.TlaWriterFactory
+import at.forsyte.apalache.tla.lir.transformations.TransformationTracker
+import at.forsyte.apalache.tla.lir.transformations.standard._
+import at.forsyte.apalache.tla.pp.{TemporalEncoder, UniqueNameGenerator}
+import com.google.inject.Inject
+import com.typesafe.scalalogging.LazyLogging
+
+/**
+ * Desugarer pass.
+ *
+ * @param options
+ *   pass options
+ * @param tracker
+ *   transformation tracker
+ * @param nextPass
+ *   next pass to call
+ */
+class TemporalPassImpl @Inject() (
+    val options: PassOptions,
+    tracker: TransformationTracker,
+    gen: UniqueNameGenerator,
+    writerFactory: TlaWriterFactory)
+    extends TemporalPass with LazyLogging {
+
+  override def name: String = "TemporalPass"
+
+  override def execute(tlaModule: TlaModule): Option[TlaModule] = {
+    logger.info("  > Rewriting temporal operators...")
+    val input = tlaModule
+    val varNames = input.varDeclarations.map {
+      _.name
+    }.toSet
+    val output = ModuleByExTransformer(TemporalEncoder(gen, varNames, tracker))(input)
+
+    // dump the result of preprocessing
+    writeOut(writerFactory, output)
+    Some(output)
+  }
+
+  override def dependencies = Set(ModuleProperty.TypeChecked)
+
+  override def transformations = Set(ModuleProperty.TemporalEncoded)
+}
