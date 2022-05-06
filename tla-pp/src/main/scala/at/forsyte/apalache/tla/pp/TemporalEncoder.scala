2ebfbb59297cdda66a2f2863d04932dc8226f21b
Philip Offtermatt <p.offtermatt@gmail.com>
2022-05-06 15:51:38 +0200

    Further implementation of temporal encoding

--- tla-pp/src/main/scala/at/forsyte/apalache/tla/pp/TemporalEncoder.scala
+++ tla-pp/src/main/scala/at/forsyte/apalache/tla/pp/TemporalEncoder.scala
@@ -8,6 +8,8 @@ import at.forsyte.apalache.tla.lir.transformations.{TlaExTransformation, Transfo
 import at.forsyte.apalache.tla.lir.values.{TlaInt, TlaStr}
 
 import javax.inject.Singleton
+import com.typesafe.scalalogging.LazyLogging
+import at.forsyte.apalache.tla.lir.io.UsableAsTLAIdentifierPrinter
 
 /**
  * <p>Encode temporal operators.</p>
@@ -16,10 +18,36 @@ import javax.inject.Singleton
  *   Philip Offtermatt
  */
 @Singleton
-class TemporalEncoder(gen: UniqueNameGenerator) {
+class TemporalEncoder(gen: UniqueNameGenerator) extends LazyLogging {
 
-  def encode(mod: TlaModule, invName: String, optViewName: Option[String] = None): TlaModule = {
-    mod
+  def encode(module: TlaModule, invName: String, optViewName: Option[String] = None): TlaModule = {
+    val levelFinder = new TlaDeclLevelFinder(module)
+
+    module.declarations.find(_.name == invName) match {
+      case Some(inv: TlaOperDecl) if inv.formalParams.isEmpty =>
+        // either a state invariant, or an action invariant
+        val level = levelFinder(inv)
+        level match {
+          case TlaLevelTemporal =>
+            logger.info("  > Encoding temporal property")
+            logger.info(inv.formalParams.toString())
+            encodeExpression(module, inv.body)
+          case _ =>
+            logger.info("  > Not a temporal property, nothing to encode")
+            module
+        }
+
+      case _ =>
+        throw new TlaInputError(s"  > Invariant is not in proper form, nothing to encode", None)
+    }
+  }
+
+  def encodeExpression(module: TlaModule, expr: TlaEx): TlaModule = {
+    val levelFinder = new TlaDeclLevelFinder(module)
+
+    // logger.info(expr)
+    module
+
   }
 }
 
