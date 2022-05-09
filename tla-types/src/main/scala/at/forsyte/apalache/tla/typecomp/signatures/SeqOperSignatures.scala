ac98176ccf735e6f968ef7a09ede68e8a536f2c2
Kukovec <jure.kukovec@gmail.com>
2022-05-09 12:03:37 +0200

    signatures

---
+++ tla-types/src/main/scala/at/forsyte/apalache/tla/typecomp/signatures/SeqOperSignatures.scala
@@ -0,0 +1,72 @@
+package at.forsyte.apalache.tla.typecomp.signatures
+
+import at.forsyte.apalache.tla.lir._
+import at.forsyte.apalache.tla.lir.oper.TlaSeqOper
+import at.forsyte.apalache.tla.typecheck.etc.TypeVarPool
+import at.forsyte.apalache.tla.typecomp.SignatureGenMap
+
+/**
+ * Produces a SignatureMap for all sequence operators
+ *
+ * @author
+ *   Jure Kukovec
+ */
+class SeqOperSignatures {
+  import TlaSeqOper._
+
+  /** Returns a map that assigns a signature generator to each TLaArithOper. */
+  def getMap(varPool: TypeVarPool): SignatureGenMap = {
+
+    // (Seq(t)) => t
+    val headSig = head -> { _: Int =>
+      val t = varPool.fresh
+      OperT1(Seq(SeqT1(t)), t)
+    }
+
+    // (Seq(t)) => Seq(t)
+    val tailSig = tail -> { _: Int =>
+      val t = varPool.fresh
+      OperT1(Seq(SeqT1(t)), SeqT1(t))
+    }
+
+    // (Seq(t), t) => Seq(t)
+    val appendSig = append -> { _: Int =>
+      val t = varPool.fresh
+      OperT1(Seq(SeqT1(t), t), SeqT1(t))
+    }
+
+    // (Seq(t), Seq(t)) => Seq(t)
+    val concatSig = concat -> { _: Int =>
+      val t = varPool.fresh
+      OperT1(Seq(SeqT1(t), SeqT1(t)), SeqT1(t))
+    }
+
+    // (Seq(t)) => Int
+    val lenSig = len -> { _: Int =>
+      val t = varPool.fresh
+      OperT1(Seq(SeqT1(t)), IntT1())
+    }
+
+    // (Seq(t), Int, Int) => Seq(t)
+    val subseqSig = subseq -> { _: Int =>
+      val t = varPool.fresh
+      OperT1(Seq(SeqT1(t), IntT1(), IntT1()), SeqT1(t))
+    }
+
+    // (Seq(t), (Seq(t)) => Bool) => Seq(t)
+    val selectseqSig = selectseq -> { _: Int =>
+      val t = varPool.fresh
+      OperT1(Seq(SeqT1(t), OperT1(Seq(SeqT1(t)), BoolT1())), SeqT1(t))
+    }
+
+    Map(
+        headSig,
+        tailSig,
+        appendSig,
+        concatSig,
+        lenSig,
+        subseqSig,
+        selectseqSig,
+    )
+  }
+}
