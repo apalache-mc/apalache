6f04b6e62532f8e191540b86cf6f2aa442dde9cc
Philip Offtermatt <p.offtermatt@gmail.com>
2022-05-10 10:26:07 +0200

    Fix format

--- tlair/src/test/scala/at/forsyte/apalache/tla/lir/TestTlaLevelFinder.scala
+++ tlair/src/test/scala/at/forsyte/apalache/tla/lir/TestTlaLevelFinder.scala
@@ -2,7 +2,7 @@ package at.forsyte.apalache.tla.lir
 
 import org.junit.runner.RunWith
 import org.scalacheck.Prop
-import org.scalacheck.Prop.{AnyOperators, all, forAll, passed}
+import org.scalacheck.Prop.{all, forAll, passed, AnyOperators}
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatestplus.junit.JUnitRunner
 import org.scalatestplus.scalacheck.Checkers
