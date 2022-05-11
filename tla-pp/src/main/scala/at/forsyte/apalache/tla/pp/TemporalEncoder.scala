7a94402fb54f8f29e834574f105a9c71e78a1be2
Philip Offtermatt <p.offtermatt@gmail.com>
2022-05-11 10:54:06 +0200

    Continue temporal encoder implementation draft

--- tla-pp/src/main/scala/at/forsyte/apalache/tla/pp/TemporalEncoder.scala
+++ tla-pp/src/main/scala/at/forsyte/apalache/tla/pp/TemporalEncoder.scala
@@ -1,58 +1,98 @@
 package at.forsyte.apalache.tla.pp
 
-import at.forsyte.apalache.tla.lir.TypedPredefs._
 import at.forsyte.apalache.tla.lir._
-import at.forsyte.apalache.tla.lir.convenience._
-import at.forsyte.apalache.tla.lir.oper._
-import at.forsyte.apalache.tla.lir.transformations.{TlaExTransformation, TransformationTracker}
-import at.forsyte.apalache.tla.lir.values.{TlaInt, TlaStr}
 
 import javax.inject.Singleton
 import com.typesafe.scalalogging.LazyLogging
-import at.forsyte.apalache.tla.lir.io.UsableAsTLAIdentifierPrinter
+import at.forsyte.apalache.tla.lir.values.TlaBool
 
 /**
- * <p>Encode temporal operators.</p>
+ * Encode temporal operators.
  *
  * @author
  *   Philip Offtermatt
  */
 @Singleton
-class TemporalEncoder(gen: UniqueNameGenerator) extends LazyLogging {
+class TemporalEncoder(module: TlaModule, gen: UniqueNameGenerator) extends LazyLogging {
+  import TemporalEncoder.NAME_PREFIX
+  val levelFinder = new TlaLevelFinder(module)
 
-  def encode(module: TlaModule, invName: String, optViewName: Option[String] = None): TlaModule = {
-    val levelFinder = new TlaDeclLevelFinder(module)
+  def encodeInvariants(invariants: List[String], init: String, next: String): TlaModule = {
+    val temporalInvariants = invariants.filter(invName => {
+      module.declarations.find(_.name == invName) match {
+        case Some(inv: TlaOperDecl) if inv.formalParams.isEmpty =>
+          // either a state invariant, or an action invariant
+          val level = levelFinder.getLevelOfDecl(inv)
+          level == TlaLevelTemporal
+        case _ => false
+      }
+    })
 
-    module.declarations.find(_.name == invName) match {
-      case Some(inv: TlaOperDecl) if inv.formalParams.isEmpty =>
-        // either a state invariant, or an action invariant
-        val level = levelFinder(inv)
-        level match {
-          case TlaLevelTemporal =>
-            logger.info("  > Encoding temporal property")
-            logger.info(inv.formalParams.toString())
-            encodeExpression(module, inv.body)
-          case _ =>
-            logger.info("  > Not a temporal property, nothing to encode")
-            module
-        }
+    if(temporalInvariants.isEmpty){
+      logger.info("  > No temporal properties found, nothing to encode")
+      module
+    } else {
+      logger.info(s"  > Found ${temporalInvariants.length} temporal invariants")
+      logger.info(s"  > Adding logic for loop finding")
 
-      case _ =>
-        throw new TlaInputError(s"  > Invariant is not in proper form, nothing to encode", None)
+      val initDecl = module.declarations.find(_.name == init)
+      if(initDecl.isEmpty){
+        logger.info(s"  > init predicate named `${init}` not found, cannot encode")
+        module
+      }
+
+      val nextDecl = module.declarations.find(_.name == next)
+      if(nextDecl.isEmpty){
+        logger.info(s"  > next predicate named `${next}` not found, cannot encode")
+        module
+      }
+      val moduleWithLoopLogic = addLoopLogic(module, initDecl.get, nextDecl.get)
+      moduleWithLoopLogic
     }
   }
 
-  def encodeExpression(module: TlaModule, expr: TlaEx): TlaModule = {
-    val levelFinder = new TlaDeclLevelFinder(module)
+  // def encode(invName: String, optViewName: Option[String] = None): TlaModule = {
+  //   module.declarations.find(_.name == invName) match {
+  //     case Some(inv: TlaOperDecl) if inv.formalParams.isEmpty =>
+  //       // either a state invariant, or an action invariant
+  //       val level = levelFinder.getLevelOfDecl(inv)
+  //       level match {
+  //         case TlaLevelTemporal =>
+  //           logger.info("  > Encoding temporal property")
+  //           logger.info(inv.formalParams.toString())
+  //           encodeExpression(module, inv.body)
+  //         case _ =>
+  //           logger.info("  > Not a temporal property, nothing to encode")
+  //           module
+  //       }
 
-    // logger.info(expr)
-    module
+  //     case _ =>
+  //       throw new TlaInputError(s"  > Invariant is not in proper form, nothing to encode", None)
+  //   }
+  // }
 
+  def createLoopVariables(originalVariables: Set[TlaVarDecl]): Set[TlaVarDecl] = {
+    originalVariables.map(varDecl =>
+      createLoopVariableForVariable(varDecl))
+  }
+
+  def createLoopVariableForVariable(varDecl: TlaVarDecl): TlaVarDecl = {
+      TlaVarDecl(s"${NAME_PREFIX}Loop_${varDecl.name}_${gen.newName()}")(varDecl.typeTag)
+  }
+
+  def addLoopLogic(module: TlaModule, init: TlaDecl, next: TlaDecl): TlaModule = {
+    val onLoop = TlaVarDecl(s"${NAME_PREFIX}InLoop_${gen.newName()}")(Typed(BoolT1))
+    val loopVariables = createLoopVariables(Set.from(module.varDeclarations)) + onLoop
+
+    print("init: \n" + init + "\n")
+
+    print("next: \n" + next + "\n")
+    
+
+    new TlaModule(module.name, module.declarations ++ loopVariables)
   }
 }
 
 object TemporalEncoder {
-  def apply(gen: UniqueNameGenerator): TemporalEncoder = {
-    new TemporalEncoder(gen)
-  }
+  val NAME_PREFIX = ""
 }
