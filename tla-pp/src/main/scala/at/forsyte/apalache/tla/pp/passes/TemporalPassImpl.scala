b76350d205492b6a13abad5e84a59b4539024ab1
Philip Offtermatt <p.offtermatt@gmail.com>
2022-05-11 10:54:06 +0200

    Continue temporal encoder implementation draft

--- tla-pp/src/main/scala/at/forsyte/apalache/tla/pp/passes/TemporalPassImpl.scala
+++ tla-pp/src/main/scala/at/forsyte/apalache/tla/pp/passes/TemporalPassImpl.scala
@@ -8,6 +8,7 @@ import at.forsyte.apalache.tla.lir.transformations.standard._
 import at.forsyte.apalache.tla.pp.{TemporalEncoder, UniqueNameGenerator}
 import com.google.inject.Inject
 import com.typesafe.scalalogging.LazyLogging
+import at.forsyte.apalache.tla.lir.TlaLevelFinder
 
 /**
  * Desugarer pass.
@@ -29,21 +30,23 @@ class TemporalPassImpl @Inject() (
 
   override def execute(tlaModule: TlaModule): Option[TlaModule] = {
     logger.info("  > Rewriting temporal operators...")
-    
-    val newModule =
-    options.get[List[String]]("checker", "inv") match {
-      case Some(invariants) =>
-        invariants.foldLeft(tlaModule) { (mod, invName) =>
-            logger.info(s"  > Found invariant $invName")
-            val optViewName = options.get[String]("checker", "view")
-            if (optViewName.isDefined) {
-              logger.info(s"  > Using state view ${optViewName.get}")
-            }
-            TemporalEncoder(gen).encode(mod, invName, optViewName)
-          }
+
+    val newModule = options.get[List[String]]("checker", "inv") match {
       case None =>
-        logger.info("  > No temporal property given, nothing to rewrite")
+        logger.info("  > No invariants specified, nothing to encode")
         tlaModule
+      case Some(invariants) =>
+        val init = options.get[String]("checker", "init")
+        if (init.isEmpty){
+          logger.info("  > `init` is not set, cannot encode invariants")
+          None
+        }
+        val next = options.get[String]("checker", "next")
+        if (init.isEmpty){
+          logger.info("  > `next` is not set, cannot encode invariants")
+          None
+        }
+        new TemporalEncoder(tlaModule, gen).encodeInvariants(invariants, init.get, next.get)
     }
 
     writeOut(writerFactory, newModule)
