88b0643f96c20d52ec07dc9a0db7430c7e41ebbf
Kukovec <jure.kukovec@gmail.com>
2022-05-12 15:04:15 +0200

    Typed Builder implementation Part 3.5: Polymorphism support (#1743)
    * polymorphism support
    
    * misc comments
    
    * resolved TODO
    
    * uncommented test
    
    * .
    
    * rm unused implicit
    
    * Apply suggestions from code review
    
    Co-authored-by: Thomas Pani <thomas@informal.systems>
    
    * PR comments
    
    Co-authored-by: Thomas Pani <thomas@informal.systems>

--- tla-types/src/main/scala/at/forsyte/apalache/tla/typecomp/signatures/BoolOperSignatures.scala
+++ tla-types/src/main/scala/at/forsyte/apalache/tla/typecomp/signatures/BoolOperSignatures.scala
@@ -1,9 +1,8 @@
 package at.forsyte.apalache.tla.typecomp.signatures
 
 import at.forsyte.apalache.tla.lir.oper.TlaBoolOper
-import at.forsyte.apalache.tla.lir.{BoolT1, OperT1, SetT1}
-import at.forsyte.apalache.tla.typecheck.etc.TypeVarPool
-import at.forsyte.apalache.tla.typecomp.{liftOper, SignatureGenMap}
+import at.forsyte.apalache.tla.lir.{BoolT1, SetT1}
+import at.forsyte.apalache.tla.typecomp.{BuilderUtil, SignatureMap}
 
 /**
  * Produces a SignatureMap for all Boolean operators
@@ -13,58 +12,44 @@ import at.forsyte.apalache.tla.typecomp.{liftOper, SignatureGenMap}
  */
 object BoolOperSignatures {
   import TlaBoolOper._
+  import BuilderUtil._
 
   /**
    * Returns a map that assigns a signature generator to each TlaBoolOper. Because some of the operators (quantifiers)
    * are polymorphic, their signatures will contain type variables produced on-demand by varPool.
    */
-  def getMap(varPool: TypeVarPool): SignatureGenMap = {
+  def getMap: SignatureMap = {
 
     // And/or are polyadic, but all the args are Bool
     // (Bool, ..., Bool) => Bool
-    val polyadic: SignatureGenMap = Seq(
+    val polyadic: SignatureMap = Seq(
         and,
         or,
-    ).map {
-      _ -> { n: Int =>
-        OperT1(Seq.fill(n)(BoolT1()), BoolT1())
-      }
-    }.toMap
+    ).map { signatureMapEntry(_, { case seq if seq.forall(_ == BoolT1()) => BoolT1() }) }.toMap
 
     // =>, <=> are binary, mono
     // (Bool, Bool) => Bool
-    val binary: SignatureGenMap = Seq(
+    val binary: SignatureMap = Seq(
         implies,
         equiv,
-    ).map { _ -> liftOper(OperT1(Seq.fill(2)(BoolT1()), BoolT1())) }.toMap
+    ).map { signatureMapEntry(_, { case Seq(BoolT1(), BoolT1()) => BoolT1() }) }.toMap
 
     // Quantifiers are polymorphic in the elemet/set types
     // (t, Set(t), Bool) => Bool
-    val boundedQuant: SignatureGenMap = Seq(
+    val boundedQuant: SignatureMap = Seq(
         forall,
         exists,
-    ).map {
-      _ -> { _: Int =>
-        val t = varPool.fresh
-        val setT = SetT1(t)
-        OperT1(Seq(t, setT, BoolT1()), BoolT1())
-      }
-    }.toMap
+    ).map { signatureMapEntry(_, { case Seq(t, SetT1(tt), BoolT1()) if t == tt => BoolT1() }) }.toMap
 
     // (t, Bool) => Bool
-    val unboundedQuant: SignatureGenMap = Seq(
+    val unboundedQuant: SignatureMap = Seq(
         forallUnbounded,
         existsUnbounded,
-    ).map {
-      _ -> { _: Int =>
-        val t = varPool.fresh
-        OperT1(Seq(t, BoolT1()), BoolT1())
-      }
-    }.toMap
+    ).map { signatureMapEntry(_, { case Seq(_, BoolT1()) => BoolT1() }) }.toMap
 
     // ~ is unary
     // (Bool) => Bool
-    val notSig: (TlaBoolOper, Int => OperT1) = not -> OperT1(Seq(BoolT1()), BoolT1())
+    val notSig = signatureMapEntry(not, { case Seq(BoolT1()) => BoolT1() })
 
     polyadic ++ binary ++ boundedQuant ++ unboundedQuant + notSig
   }
