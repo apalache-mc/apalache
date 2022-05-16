597fcfbd281c9ab4518808d4aff6bf8b18df78cc
rodrigo7491 <rodrigo7491@gmail.com>
2022-05-16 17:06:34 +0200

    optimize basic set membership

--- tla-bmcmt/src/main/scala/at/forsyte/apalache/tla/bmcmt/rules/SetInRuleWithArrays.scala
+++ tla-bmcmt/src/main/scala/at/forsyte/apalache/tla/bmcmt/rules/SetInRuleWithArrays.scala
@@ -6,7 +6,7 @@ import at.forsyte.apalache.tla.bmcmt.types._
 import at.forsyte.apalache.tla.bmcmt.{ArenaCell, RewriterException, SymbState, SymbStateRewriter}
 import at.forsyte.apalache.tla.lir.UntypedPredefs._
 import at.forsyte.apalache.tla.lir.convenience.tla
-import at.forsyte.apalache.tla.lir.{BoolT1, FunT1, SetT1, TlaEx}
+import at.forsyte.apalache.tla.lir.{BoolT1, ConstT1, FunT1, IntT1, RealT1, SetT1, StrT1, TlaEx, VarT1}
 
 /**
  * Rewrites set membership tests: x \in S, x \in SUBSET S, and x \in [S -> T].
@@ -119,25 +119,33 @@ class SetInRuleWithArrays(rewriter: SymbStateRewriter) extends SetInRule(rewrite
       var nextState = state.updateArena(_.appendCell(BoolT1()))
       val pred = nextState.arena.topCell.toNameEx
 
-      // EqConstraints need to be generated, since missing in-relations, e.g. in sets of tuples, will lead to errors.
-      // TODO: Inlining this method is pointless. We should consider handling tuples and other structures natively in SMT.
-      // If elemCell is a function, cacheEqConstraints will generate constraints over its set of relations. This is
-      // problematic because the relations are only being carried in the arena, without SMT constrains being generated.
       elemCell.cellType match {
-        case CellTFrom(FunT1(_, _)) => ()
+        // For types that don't rely on lazy equality, we can simply query the SMT array for membership
+        case CellTFrom(_ @IntT1() | RealT1() | BoolT1() | StrT1() | ConstT1(_) | VarT1(_) | FunT1(_, _) | SetT1(_)) =>
+          val eql = tla.eql(pred, tla.apalacheSelectInSet(elemCell.toNameEx, setCell.toNameEx))
+          rewriter.solverContext.assertGroundExpr(eql)
+          nextState.setRex(pred)
+
+        // If the element's type does not support SMT equality, we rely on lazy equality
         case _ =>
-          nextState = rewriter.lazyEq.cacheEqConstraints(nextState, potentialElems.map((_, elemCell)))
-      }
+          // EqConstraints need to be generated, since missing in-relations, e.g. in sets of tuples, will lead to errors.
+          // If elemCell is a function, cacheEqConstraints will generate constraints over its set of relations. This is
+          // problematic because the relations are only being carried in the arena, without SMT constrains being generated.
+          elemCell.cellType match {
+            case CellTFrom(FunT1(_, _)) => ()
+            case _ => nextState = rewriter.lazyEq.cacheEqConstraints(nextState, potentialElems.map((_, elemCell)))
+          }
 
-      def inAndEq(elem: ArenaCell) = {
-        simplifier
-          .simplifyShallow(tla.and(tla.apalacheSelectInSet(elem.toNameEx, setCell.toNameEx),
-                  tla.eql(elem.toNameEx, elemCell.toNameEx)))
-      }
+          def inAndEq(elem: ArenaCell) = {
+            val select = tla.apalacheSelectInSet(elem.toNameEx, setCell.toNameEx)
+            val eql = tla.eql(elem.toNameEx, elemCell.toNameEx)
+            simplifier.simplifyShallow(tla.and(select, eql))
+          }
 
-      val elemsInAndEq = potentialElems.map(inAndEq)
-      rewriter.solverContext.assertGroundExpr(simplifier.simplifyShallow(tla.eql(pred, tla.or(elemsInAndEq: _*))))
-      nextState.setRex(pred)
+          val elemsInAndEq = potentialElems.map(inAndEq)
+          rewriter.solverContext.assertGroundExpr(simplifier.simplifyShallow(tla.eql(pred, tla.or(elemsInAndEq: _*))))
+          nextState.setRex(pred)
+      }
     }
   }
 }
