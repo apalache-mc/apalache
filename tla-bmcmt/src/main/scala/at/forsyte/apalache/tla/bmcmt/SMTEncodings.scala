35e567b37460f5a0a64555ba6009176d82efc905
Thomas Pani <thomas@informal.systems>
2022-05-17 16:37:12 +0200

    Add .toString() to `SMTEncoding`s

--- tla-bmcmt/src/main/scala/at/forsyte/apalache/tla/bmcmt/SMTEncodings.scala
+++ tla-bmcmt/src/main/scala/at/forsyte/apalache/tla/bmcmt/SMTEncodings.scala
@@ -2,5 +2,9 @@ package at.forsyte.apalache.tla.bmcmt
 
 sealed trait SMTEncoding
 
-case object oopsla19Encoding extends SMTEncoding
-case object arraysEncoding extends SMTEncoding
+case object oopsla19Encoding extends SMTEncoding {
+  override def toString: String = "oopsla19"
+}
+case object arraysEncoding extends SMTEncoding {
+  override def toString: String = "arrays"
+}
