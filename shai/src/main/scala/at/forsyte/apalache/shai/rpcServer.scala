ac81a27c70911281f58d05cf455f19bf8c59f61f
Shon Feder <shon@informal.systems>
2022-06-01 19:55:33 -0400

    Add welcome message

--- shai/src/main/scala/at/forsyte/apalache/shai/rpcServer.scala
+++ shai/src/main/scala/at/forsyte/apalache/shai/rpcServer.scala
@@ -1,12 +1,15 @@
 package at.forsyte.apalache.shai.v1
 
-import zio.{Ref, ZEnv}
+import zio.{console, Ref, ZEnv, ZIO}
 import java.util.UUID
 import scalapb.zio_grpc.ServerMain
 import scalapb.zio_grpc.ServiceList
 
 object RpcServer extends ServerMain {
   override def port: Int = 8822
+
+  override def welcome: ZIO[ZEnv, Throwable, Unit] =
+    console.putStrLn("The Apalache server is running. Press Ctrl-C to stop.")
 
   val createService = for {
     // A thread safe mutable reference to the active connections
