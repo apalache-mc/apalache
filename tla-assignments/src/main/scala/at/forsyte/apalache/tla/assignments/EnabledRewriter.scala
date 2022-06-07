1d938471d1aceadcada7c16ac3a999c92e9d49df
Philip Offtermatt <p.offtermatt@gmail.com>
2022-06-07 17:44:58 +0200

    Implement substituting primed variables for unprimed expressions

--- tla-assignments/src/main/scala/at/forsyte/apalache/tla/assignments/EnabledRewriter.scala
+++ tla-assignments/src/main/scala/at/forsyte/apalache/tla/assignments/EnabledRewriter.scala
@@ -10,6 +10,7 @@ import at.forsyte.apalache.tla.assignments.SmtFreeSymbolicTransitionExtractor
 import at.forsyte.apalache.tla.lir.oper.TlaActionOper
 import at.forsyte.apalache.tla.lir.oper.ApalacheOper
 import at.forsyte.apalache.tla.lir.values.TlaBool
+import at.forsyte.apalache.tla.lir.oper.TlaBoolOper
 
 /**
  * Attempts to rewrite `ENABLED foo` operators into formulas that are true when action `foo` is enabled.
@@ -28,7 +29,7 @@ class EnabledRewriter(
    * Removes the assignments x' := foo from an expression by replacing them with TRUE
    */
   private def removeAssignmentsFromExpression(ex: TlaEx): TlaEx = {
-    val res = ex match {
+    ex match {
       case OperEx(oper, args @ _*) =>
         oper match {
           case ApalacheOper.assign =>
@@ -39,19 +40,115 @@ class EnabledRewriter(
       case _ =>
         ex
     }
-    res
   }
 
   /**
-   * TODO: Extracts a map of assignments primeVar := foo from an expression
+   * Extracts a map of assignments primeVar := foo from an expression
    */
   private def extractAssignmentsFromExpression(ex: TlaEx): Map[String, TlaEx] = {
-    Map.empty
+    ex match {
+      case OperEx(oper, args @ _*) =>
+        oper match {
+          case ApalacheOper.assign =>
+            // by the meaning of the assign operator, will be the name of a (primed) variables
+            // there should not be none, so we'll throw an exception if it is the case
+            val varName = findPrimedVariableInExpression(args(0))
+            if (varName.isEmpty) {
+              throw new LirError("Unexpected: did not find a primed variable on the left-hand side" +
+                "of the assign statement: " + ex.toString())
+            }
+            return Map[String, TlaEx](
+                (
+                    varName.get,
+                    args(1),
+                )
+            )
+          case _ =>
+            return args
+              .map(extractAssignmentsFromExpression)
+              .foldLeft(
+                  Map.empty[String, TlaEx]
+              ) { case (acc, newMap) =>
+                acc.++(newMap)
+              }
+        }
+      case _ =>
+        Map.empty[String, TlaEx]
+    }
+  }
+
+  /*
+   * Finds some occurence of a primed variable in the expression and returns its name.
+   * Throws an exception if there are multiple primed variables in the expression.
+   */
+  private def findPrimedVariableInExpression(ex: TlaEx): Option[String] = {
+    ex match {
+      case NameEx(_) => None
+      case LetInEx(_, _) =>
+        throw new NotInKeraError("There should be no let-in expressions left after inlining", ex)
+      case OperEx(TlaActionOper.prime, NameEx(name)) => Some(name)
+      case OperEx(_, args @ _*) =>
+        args.map(findPrimedVariableInExpression).foldLeft(None: Option[String]) { case (curOption, newOption) =>
+          (curOption, newOption) match {
+            case (None, None)      => None
+            case (Some(str), None) => Some(str)
+            case (None, Some(str)) => Some(str)
+            case (Some(str), Some(otherstr)) =>
+              throw new LirError("Expect to find only one primed variable" +
+                s"in the expression, but found these two variables primed: ${str}, ${otherstr}")
+          }
+        }
+      case _ => None
+    }
   }
 
   /**
-   * TODO: Rewrite occurrences of primed variable as unprimed expression; needs to be done iteratively to replace more and more variables
+   * Flattens the map of assignments, e.g.
+   *
+   * {{{Map(x' -> y' + 1, y' -> x + 5)}}} should become {{{Map(x' -> (x + 5) + 1, y' -> x + 5)}}}
+   *
+   * There should be no circular dependencies, e.g. {{{Map(x' -> y' + 1, y' -> x + 5)}}}. Then, the output is guaranteed
+   * to have no primed variables on right-hand-sides.
    */
+  private def flattenAssignments(assignmentMap: Map[String, TlaEx]): Map[String, TlaEx] = {
+    def flattenOnce(assignmentMap: Map[String, TlaEx]): Map[String, TlaEx] = {
+      assignmentMap.map { case (name, assignment) =>
+        (name, flattenEx(assignment, assignmentMap))
+      }
+    }
+
+    var currentMap = assignmentMap
+
+    // at most, need to flatten once for every variable (unless there are circular dependencies)
+    for (_ <- 1 to assignmentMap.size) {
+      val newMap = flattenOnce(assignmentMap)
+      // no variables are replaced, so can abort early
+      if (currentMap == newMap) {
+        return currentMap
+      }
+      currentMap = newMap
+    }
+    currentMap
+  }
+
+  /*
+   * In the provided expression, replace all occurrences of names
+   * that are keys in the assignment map by their respective assigned expression,
+   * i.e. the result of {{{assignmentMap.getOrElse(name, ex)}}}
+   */
+  private def flattenEx(ex: TlaEx, assignmentMap: Map[String, TlaEx]): TlaEx = {
+    ex match {
+      case OperEx(TlaActionOper.prime, NameEx(name)) =>
+        // replace a name expression by its assignment
+        // or leave it if no assignment exists for the name expression
+        assignmentMap.getOrElse(name, ex)
+      case OperEx(oper, args @ _*) =>
+        OperEx(oper, args.map(arg => flattenEx(arg, assignmentMap)): _*)(ex.typeTag)
+      case LetInEx(_, _) =>
+        throw new NotInKeraError("There should be no let-in expressions left after inlining", ex)
+      case _ => ex
+    }
+  }
 
   private def transformEnabled(ex: TlaEx): TlaEx = {
     val vars = module.varDeclarations.map(_.name)
@@ -64,17 +161,12 @@ class EnabledRewriter(
 
     val transitionsWithoutAssignments = transitionPairs.map(symbTrans => {
       val assignmentEx = symbTrans._2
-      val modifiedEx = removeAssignmentsFromExpression(assignmentEx)
-      print("\n::::::::::::::::\n")
-      print(modifiedEx.toString())
-      print("\n::::::::::::::::\n")
+      val assignments = flattenAssignments(extractAssignmentsFromExpression(assignmentEx))
+      val modifiedEx = flattenEx(removeAssignmentsFromExpression(assignmentEx), assignments)
+      modifiedEx
     })
-    print("\n--------------------------------\n")
-    print(ex.toString())
-    print("\n================================\n")
-    print(transitionsWithoutAssignments.toString())
-    print("\n--------------------------------\n")
-    NullEx
+
+    OperEx(TlaBoolOper.or, transitionsWithoutAssignments: _*)(Typed(BoolT1))
   }
 
   override def apply(ex: TlaEx): TlaEx = {
