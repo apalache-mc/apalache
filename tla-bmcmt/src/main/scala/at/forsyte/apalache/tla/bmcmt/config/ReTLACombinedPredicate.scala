2b5fd7b8f66a0b33487c2374aa7c35a466195520
Igor Konnov <igor@informal.systems>
2022-06-22 13:24:40 +0200

    fix another integration test

--- tla-bmcmt/src/main/scala/at/forsyte/apalache/tla/bmcmt/config/ReTLACombinedPredicate.scala
+++ tla-bmcmt/src/main/scala/at/forsyte/apalache/tla/bmcmt/config/ReTLACombinedPredicate.scala
@@ -1,6 +1,6 @@
 package at.forsyte.apalache.tla.bmcmt.config
 
-import at.forsyte.apalache.tla.lir.transformations.standard.MonotypeLanguagePred
+import at.forsyte.apalache.tla.lir.transformations.standard.{MonotypeLanguagePred, ReTLALanguagePred}
 import at.forsyte.apalache.tla.lir.transformations.{LanguagePred, PredResult}
 import at.forsyte.apalache.tla.lir.{TlaEx, TlaModule}
 
@@ -9,7 +9,7 @@ import at.forsyte.apalache.tla.lir.{TlaEx, TlaModule}
  */
 class ReTLACombinedPredicate extends LanguagePred {
   private val mono = new MonotypeLanguagePred()
-  private val retla = new ReTLACombinedPredicate()
+  private val retla = new ReTLALanguagePred()
 
   override def isExprOk(ex: TlaEx): PredResult = {
     mono.isExprOk(ex).and(retla.isExprOk(ex))
