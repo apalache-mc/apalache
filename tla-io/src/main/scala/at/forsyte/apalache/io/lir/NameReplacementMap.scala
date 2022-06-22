1bcbec5d24c34d36b4c7dcbc776bce921aec15b8
Philip Offtermatt <p.offtermatt@gmail.com>
2022-06-22 09:42:40 +0200

    Remove @param writer when no param exists

--- tla-io/src/main/scala/at/forsyte/apalache/io/lir/NameReplacementMap.scala
+++ tla-io/src/main/scala/at/forsyte/apalache/io/lir/NameReplacementMap.scala
@@ -5,8 +5,6 @@ import scala.collection.mutable.HashMap
 /**
  * Maps names of variables to more readable strings. Variable names need to be valid TLA variable names, while the
  * readable strings do not have such restrictions.
- * @param writer
- *   a print writer to use
  * @author
  *   Philip Offtermatt
  */
