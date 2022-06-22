7a203c66f8c6de7ba788c982b4004906ed68327d
Philip Offtermatt <p.offtermatt@gmail.com>
2022-06-22 08:18:02 +0200

    Fix comment for NameReplacementMap

--- tla-io/src/main/scala/at/forsyte/apalache/io/lir/NameReplacementMap.scala
+++ tla-io/src/main/scala/at/forsyte/apalache/io/lir/NameReplacementMap.scala
@@ -3,8 +3,8 @@ package at.forsyte.apalache.io.lir
 import scala.collection.mutable.HashMap
 
 /**
- * This class produces counterexamples in the Informal Trace Format.
- *
+ * Maps names of variables to more readable strings. Variable names need to be valid TLA variable names, while the
+ * readable strings do not have such restrictions.
  * @param writer
  *   a print writer to use
  * @author
