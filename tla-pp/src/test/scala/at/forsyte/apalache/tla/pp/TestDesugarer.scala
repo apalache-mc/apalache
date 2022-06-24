4c04e32039de2ba1b4008ad229ff2cde4cedae05
Philip Offtermatt <p.offtermatt@gmail.com>
2022-06-24 13:19:43 +0200

    Desugar again after transforming stuttering to avoid keeping unchanged

--- tla-pp/src/test/scala/at/forsyte/apalache/tla/pp/TestDesugarer.scala
+++ tla-pp/src/test/scala/at/forsyte/apalache/tla/pp/TestDesugarer.scala
@@ -803,7 +803,7 @@ class TestDesugarer extends AnyFunSuite with BeforeAndAfterEach {
     val output = desugarer.transform(input)
 
     val expected =
-      tla.or(tla.name("A").typed(BoolT1), tla.unchanged(tla.name("B").typed(IntT1)).typed(BoolT1)).typed(BoolT1)
+      desugarer.transform(tla.or(tla.name("A").typed(BoolT1), tla.unchanged(tla.name("B").typed(IntT1)).typed(BoolT1)).typed(BoolT1))
     assert(expected.eqTyped(output))
   }
 
@@ -814,7 +814,7 @@ class TestDesugarer extends AnyFunSuite with BeforeAndAfterEach {
     val output = desugarer.transform(input)
 
     val expected =
-      tla
+      desugarer.transform(tla
         .and(
             tla.name("A").typed(BoolT1),
             tla
@@ -824,6 +824,7 @@ class TestDesugarer extends AnyFunSuite with BeforeAndAfterEach {
               .typed(BoolT1),
         )
         .typed(BoolT1)
+      )
     assert(expected.eqTyped(output))
   }
 }
