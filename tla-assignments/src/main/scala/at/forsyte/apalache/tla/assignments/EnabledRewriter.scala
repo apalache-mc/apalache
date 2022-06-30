288f0f78b97e63c063d77ad92fa95ff68e98ca5a
Philip Offtermatt <p.offtermatt@gmail.com>
2022-06-30 11:24:03 +0200

    Use replaceFixed

--- tla-assignments/src/main/scala/at/forsyte/apalache/tla/assignments/EnabledRewriter.scala
+++ tla-assignments/src/main/scala/at/forsyte/apalache/tla/assignments/EnabledRewriter.scala
@@ -17,6 +17,7 @@ import at.forsyte.apalache.tla.pp.NotInKeraError
 import at.forsyte.apalache.tla.pp.ConstSimplifier
 import at.forsyte.apalache.tla.pp.Normalizer
 import at.forsyte.apalache.tla.lir.transformations.standard.ReplaceFixed
+import at.forsyte.apalache.tla.lir.TypedPredefs.TypeTagAsTlaType1
 
 /**
  * Attempts to rewrite `ENABLED foo` operators into formulas that are true when action `foo` is enabled.
@@ -195,7 +196,16 @@ class EnabledRewriter(
    * i.e. the result of {{{assignmentMap.getOrElse(name, ex)}}}
    */
   private def flattenEx(ex: TlaEx, assignmentMap: Map[String, TlaEx]): TlaEx = {
-    assignmentMap.foldLeft(ex) { case (k, v) => ReplaceFixed(utils.builder.prime(utils.builder.name(k, kType)), v) }
+    assignmentMap.foldLeft(ex) { case (ex, (varName, replacementEx)) =>
+      ReplaceFixed(tracker)
+        .whenEqualsTo(
+            utils.builder.prime(utils.builder.name(
+                    varName,
+                    replacementEx.typeTag.asTlaType1(),
+                )),
+            replacementEx,
+        )(ex)
+    }
   }
 
   /**
