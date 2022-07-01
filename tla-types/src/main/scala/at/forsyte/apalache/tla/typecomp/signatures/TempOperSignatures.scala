70e00c5933518b03b943c69a1a3dc0abeedf392f
Kukovec <jure.kukovec@gmail.com>
2022-07-01 16:16:05 +0200

    init

---
+++ tla-types/src/main/scala/at/forsyte/apalache/tla/typecomp/signatures/TempOperSignatures.scala
@@ -0,0 +1,20 @@
+package at.forsyte.apalache.tla.typecomp.signatures
+
+//import at.forsyte.apalache.tla.lir.oper.TlaTempOper
+//import at.forsyte.apalache.tla.lir.BoolT1
+import at.forsyte.apalache.tla.typecomp.SignatureMap
+
+/**
+ * Produces a SignatureMap for all temporal operators
+ *
+ * @author
+ *   Jure Kukovec
+ */
+object TempOperSignatures {
+//  import BuilderUtil._
+//  import TlaTempOper._
+
+  def getMap: SignatureMap =
+    Map.empty
+
+}
