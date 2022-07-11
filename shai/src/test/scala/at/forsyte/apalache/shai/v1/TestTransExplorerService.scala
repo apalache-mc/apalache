8ad3944e284ad54ef630dbf33baca9e60bdc2600
Shon Feder <shon@informal.systems>
2022-07-11 00:21:26 -0400

    Add tests for the `loadModel` rpc

--- shai/src/test/scala/at/forsyte/apalache/shai/v1/TestTransExplorerService.scala
+++ shai/src/test/scala/at/forsyte/apalache/shai/v1/TestTransExplorerService.scala
@@ -4,7 +4,7 @@ import zio._
 import zio.test._
 import zio.test.Assertion._
 import java.util.UUID
-import at.forsyte.apalache.shai.v1.transExplorer.ConnectRequest
+import at.forsyte.apalache.shai.v1.transExplorer.{ConnectRequest, LoadModelRequest}
 
 object TransExplorerServiceSpec extends DefaultRunnableSpec {
 
@@ -20,6 +20,50 @@ object TransExplorerServiceSpec extends DefaultRunnableSpec {
           c1 <- s.openConnection(ConnectRequest())
           c2 <- s.openConnection(ConnectRequest())
         } yield assert(c1.id)(not(equalTo(c2.id)))
-      }
+      },
+      testM("invalid spec returns error") {
+        val spec = """|---- missing module declaration ----
+                      |Foo == TRUE
+                      |====
+                      |""".stripMargin
+        for {
+          s <- service
+          conn <- s.openConnection(ConnectRequest())
+          resp <- s.loadModel(LoadModelRequest(conn.id, spec))
+          msg = resp.result.err.get
+        } yield assert(msg)(containsString("Parsing failed: Error by TLA+ parser"))
+      },
+      testM("valid spec returns parsed model") {
+        val spec =
+          """|---- MODULE A ----
+             |Foo == TRUE
+             |====
+             |""".stripMargin
+
+        for {
+          s <- service
+          conn <- s.openConnection(ConnectRequest())
+          resp <- s.loadModel(LoadModelRequest(conn.id, spec))
+        } yield assert(resp.result.isSpec)(isTrue)
+      },
+      testM("valid multi-module spec loads parsed model") {
+        val auxSpec =
+          """|---- MODULE B ----
+             |BOp == TRUE
+             |==================
+             |""".stripMargin
+
+        val spec =
+          """|---- MODULE A ----
+             |EXTENDS B
+             |Foo == BOp
+             |==================
+             |""".stripMargin
+        for {
+          s <- service
+          conn <- s.openConnection(ConnectRequest())
+          resp <- s.loadModel(LoadModelRequest(conn.id, spec, Seq(auxSpec)))
+        } yield assert(resp.result.isSpec)(isTrue)
+      },
   )
 }
