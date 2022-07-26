a97137f9b1d6736ca12b8ff49b9a98237f087d7c
Shon Feder <shon@informal.systems>
2022-07-26 17:02:13 -0400

    Create `infra.passes.options` object
    This object is mimicking a package currently (see
    https://docs.scala-lang.org/style/naming-conventions.html#objects). We
    may move this stuff into a package if it gets too large.
    
    This package will collect all the statically declared options parameters
    we use to configure Apalache.

---
+++ mod-infra/src/main/scala/at/forsyte/apalache/infra/passes/options.scala
@@ -0,0 +1,28 @@
+package at.forsyte.apalache.infra.passes
+
+/** Collects the passoptions supported for configuring Apalache's various passes */
+object options {
+
+  /**
+   * Defines the SMT encoding options supported
+   *
+   * @author
+   *   Shon Feder
+   */
+  sealed abstract class SMTEncoding
+
+  object SMTEncoding {
+    final case object Oopsla19 extends SMTEncoding {
+      override def toString: String = "oopsla19"
+    }
+    final case object Arrays extends SMTEncoding {
+      override def toString: String = "arrays"
+    }
+
+    val ofString: String => SMTEncoding = {
+      case "arrays"        => Arrays
+      case "oopsla19"      => Oopsla19
+      case oddEncodingType => throw new IllegalArgumentException(s"Unexpected SMT encoding type $oddEncodingType")
+    }
+  }
+}
