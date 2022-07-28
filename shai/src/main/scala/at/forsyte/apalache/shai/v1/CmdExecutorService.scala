14567336cb60b05b17dfccd050d73c897c220a4d
Shon Feder <shon@informal.systems>
2022-07-28 15:07:45 -0400

    Add CmdExecutor service
    Closes #2020

---
+++ shai/src/main/scala/at/forsyte/apalache/shai/v1/CmdExecutorService.scala
@@ -0,0 +1,23 @@
+package at.forsyte.apalache.shai.v1
+
+import at.forsyte.apalache.shai.v1.cmdExecutor.ZioCmdExecutor
+import zio.{Semaphore, ZEnv}
+import com.typesafe.scalalogging.Logger
+
+/**
+ * Provides the [[CmdExecutorService]]
+ *
+ * ==Overview==
+ *
+ * The [[CmdExecutorService]] exposes an RPC interface to execute Apalache's CLI subcommand, thus enabling clients to
+ * utilize much of Apalache's CLI functionality with the benefit of structured configuration, input, and output and
+ * avoiding the repeated startup costs of the JVM.
+ *
+ * [[CmdExecutorSerive]] is meant to be registered with [[RpcServer]], and should not need to be used directly.
+ */
+
+class CmdExecutorService(parserSemaphore: Semaphore, logger: Logger) extends ZioCmdExecutor.ZCmdExecutor[ZEnv, Any] {
+
+  // TODO These will be used when we start adding the RPC calls
+  val _todo = (parserSemaphore, logger)
+}
