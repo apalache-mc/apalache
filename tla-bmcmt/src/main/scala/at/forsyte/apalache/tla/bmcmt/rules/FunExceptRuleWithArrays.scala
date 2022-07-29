0b17fec8db457bdbb4922a9d4b561ea250da30ce
rodrigo7491 <rodrigo7491@gmail.com>
2022-07-29 14:04:58 +0200

    cache equality constraints from FunExcept

--- tla-bmcmt/src/main/scala/at/forsyte/apalache/tla/bmcmt/rules/FunExceptRuleWithArrays.scala
+++ tla-bmcmt/src/main/scala/at/forsyte/apalache/tla/bmcmt/rules/FunExceptRuleWithArrays.scala
@@ -69,6 +69,21 @@ class FunExceptRuleWithArrays(rewriter: SymbStateRewriter) extends FunExceptRule
     val eql = tla.eql(resultFunCell.toNameEx, funCell.toNameEx)
     rewriter.solverContext.assertGroundExpr(eql)
 
+    val resultRelationElems = nextState.arena.getHas(resultRelation)
+    nextState = rewriter.lazyEq.cacheEqConstraints(nextState, resultRelationElems.zip(relationCells)) // fixes mod1
+    // the chain of constraints was being lost
+
+    nextState = rewriter.lazyEq.cacheEqConstraints(nextState, resultRelationElems.map((_, newPairCell))) // fixes mod3
+
+    // probably redundant
+    // val relationCells2 = relationCells.map(e => nextState.arena.getHas(e)(1))
+    // val resultRelationElems2 = nextState.arena.getHas(resultRelation).map(e => nextState.arena.getHas(e)(1))
+    // nextState = rewriter.lazyEq.cacheEqConstraints(nextState, resultRelationElems2.zip(relationCells2))
+
+    // val resultRelationElems1 = nextState.arena.getHas(resultRelation).map(e => nextState.arena.getHas(e)(0))
+    // nextState = rewriter.lazyEq.cacheEqConstraints(nextState, nextState.arena.getHas(domainCell).zip(resultRelationElems1))
+
+    /*
     // inDomain with lazy equality
     val domElems = nextState.arena.getHas(domainCell)
     nextState = rewriter.lazyEq.cacheEqConstraints(nextState, domElems.map((_, indexCell)))
@@ -77,19 +92,21 @@ class FunExceptRuleWithArrays(rewriter: SymbStateRewriter) extends FunExceptRule
       tla.and(tla.apalacheSelectInSet(elem.toNameEx, domainCell.toNameEx),
           rewriter.lazyEq.safeEq(elem, indexCell)) // use lazy equality
     }
+     */
 
-    nextState = nextState.updateArena(_.appendCell(BoolT1))
-    val inDomain = nextState.arena.topCell.toNameEx
+    // nextState = nextState.updateArena(_.appendCell(BoolT1))
+    // val inDomain = nextState.arena.topCell.toNameEx
 
-    val elemsInAndEq = nextState.arena.getHas(domainCell).map(inAndEq)
-    rewriter.solverContext.assertGroundExpr(tla.eql(inDomain, tla.or(elemsInAndEq: _*)))
+    // val elemsInAndEq = nextState.arena.getHas(domainCell).map(inAndEq)
+    // rewriter.solverContext.assertGroundExpr(tla.eql(inDomain, tla.or(elemsInAndEq: _*)))
 
+    // There is no need to constrain updates, only accesses
     // Add a constraint updating resultFunCell if needed
     // val inDomain = tla.apalacheSelectInSet(indexCell.toNameEx, domainCell.toNameEx)
     val updateFun = tla.apalacheStoreInFun(valueCell.toNameEx, resultFunCell.toNameEx, indexCell.toNameEx)
-    val unchanged = tla.apalacheStoreNotInFun(valueCell.toNameEx, resultFunCell.toNameEx)
-    val ite = tla.ite(inDomain, updateFun, unchanged)
-    rewriter.solverContext.assertGroundExpr(ite)
+    // val unchanged = tla.apalacheStoreNotInFun(valueCell.toNameEx, resultFunCell.toNameEx)
+    // val ite = tla.ite(inDomain, updateFun, unchanged)
+    rewriter.solverContext.assertGroundExpr(updateFun)
 
     nextState.setRex(resultFunCell.toNameEx)
   }
