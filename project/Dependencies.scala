41d59e584aed4f9bb2928c8f74142d1d74a4b12c
Scala Steward <scala_steward@virtuslab.com>
2022-08-04 14:49:47 +0000

    Update scalatest to 3.2.13

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -54,7 +54,7 @@ object Dependencies {
     val scalacheck = "org.scalacheck" %% "scalacheck" % "1.16.0" % Test
     val easymock = "org.easymock" % "easymock" % "4.3" % Test
 
-    val scalaTestVersion = "3.2.11"
+    val scalaTestVersion = "3.2.13"
     val scalatest = "org.scalatest" %% "scalatest" % scalaTestVersion % Test
     val scalatestplusEasymock = "org.scalatestplus" %% "easymock-4-3" % s"${scalaTestVersion}.0" % Test
     val scalatestplusJunit = "org.scalatestplus" %% "junit-4-13" % s"${scalaTestVersion}.0" % Test
