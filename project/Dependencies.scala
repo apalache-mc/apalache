2ac0f88d15ed7da84555cca6dce381c206366876
Shon Feder <shon@informal.systems>
2022-08-12 23:55:37 -0400

    Add dependency on shapeless
    This enables generic manipulation of case classes, greatly reducing the
    boilerplate needed to manage the config values.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -32,6 +32,7 @@ object Dependencies {
     val scalaCollectionContrib = "org.scala-lang.modules" %% "scala-collection-contrib" % "0.2.2"
     val scalaz = "org.scalaz" %% "scalaz-core" % "7.3.5"
     val slf4j = "org.slf4j" % "slf4j-api" % "1.7.36"
+    val shapeless = "com.chuusai" %% "shapeless" % "2.3.9"
     val tla2tools = "org.lamport" % "tla2tools" % "1.7.0-SNAPSHOT"
     val ujson = "com.lihaoyi" %% "ujson" % "2.0.0"
     val z3 = "tools.aqua" % "z3-turnkey" % "4.8.17"
