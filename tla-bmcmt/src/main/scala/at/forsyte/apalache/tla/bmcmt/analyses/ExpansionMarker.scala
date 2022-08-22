81c4f1c11d2eac8553fe5bd4661d8200339a5bd5
Igor Konnov <igor@informal.systems>
2022-08-22 13:49:56 +0200

    update the warning about set expansion

--- tla-bmcmt/src/main/scala/at/forsyte/apalache/tla/bmcmt/analyses/ExpansionMarker.scala
+++ tla-bmcmt/src/main/scala/at/forsyte/apalache/tla/bmcmt/analyses/ExpansionMarker.scala
@@ -35,8 +35,6 @@ class ExpansionMarker @Inject() (tracker: TransformationTracker) extends TlaExTr
       val tag = ex.typeTag
       if (shallExpand) {
         // Expand the set as well as the underlying set!
-        logger.warn(s"The set S = $ex will be expanded, producing 2^Cardinality(S) constraints.")
-        logger.warn("Expansion may significantly slow down the solver, e.g., when Cardinality(S) >= 10.")
         OperEx(ApalacheOper.expand, OperEx(op, transform(true)(underlyingSet))(tag))(tag)
       } else {
         // Do not expand the set itself, but expand the underlying set!
@@ -47,10 +45,8 @@ class ExpansionMarker @Inject() (tracker: TransformationTracker) extends TlaExTr
       val tag = ex.typeTag
       if (shallExpand) {
         // Expand everything, including the function set.
-        logger.warn(
-            s"The function set [ S -> T ] = $ex will be expanded, producing Cardinality(T)^Cardinality(S) constraints.")
-        logger.warn(
-            "Expansion may significantly slowdown the solver, e.g., when Cardinality(T)^Cardinality(S) >= 1000.")
+        // Expansion of function sets is not implemented yet, see:
+        // https://github.com/informalsystems/apalache/issues/1452
         OperEx(ApalacheOper.expand, OperEx(op, transform(true)(dom), transform(true)(cdm))(tag))(tag)
       } else {
         // Only expand the domain, but keep the co-domain unexpanded,
