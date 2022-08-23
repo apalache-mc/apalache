29a9ded85ffdff66799857f5569cddd2a62699a3
Shon Feder <shon@informal.systems>
2022-08-23 17:06:17 -0400

    Fix binding more

--- mod-infra/src/main/scala/at/forsyte/apalache/infra/passes/DerivedPredicates.scala
+++ mod-infra/src/main/scala/at/forsyte/apalache/infra/passes/DerivedPredicates.scala
@@ -3,7 +3,7 @@ package at.forsyte.apalache.infra.passes
 // TODO doc
 //
 // Allows communicating mutable spec behavior between passes
-// TODO What's the right name for this class?
+//  What's the right name for this class?
 trait DerivedPredicates {
   var invariants: List[String]
   var temporal: List[String]
@@ -21,11 +21,13 @@ object DerivedPredicates {
       var cinit: Option[String])
       extends DerivedPredicates
 
-  def apply(
-      invariants: List[String] = List.empty,
-      temporal: List[String] = List.empty,
-      next: String = "",
-      init: String = "",
-      cinit: Option[String] = None): DerivedPredicates =
-    new Impl(invariants = invariants, temporal = temporal, next = next, init = init, cinit = cinit)
+  object Impl {
+    def apply(
+        invariants: List[String] = List.empty,
+        temporal: List[String] = List.empty,
+        next: String = "",
+        init: String = "",
+        cinit: Option[String] = None): DerivedPredicates =
+      new Impl(invariants = invariants, temporal = temporal, next = next, init = init, cinit = cinit)
+  }
 }
