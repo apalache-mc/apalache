b2ccd7c99943ae9fde3566cc70d5536f6cd37e1f
Shon Feder <shon@informal.systems>
2022-08-25 14:51:22 -0400

    Update ToolModule docs

--- mod-infra/src/main/scala/at/forsyte/apalache/infra/passes/ToolModule.scala
+++ mod-infra/src/main/scala/at/forsyte/apalache/infra/passes/ToolModule.scala
@@ -6,18 +6,14 @@ import at.forsyte.apalache.infra.passes.options.OptionGroup
 /**
  * An extension of Google Guice AbstractModule to be used by apalache modules to configure pass order and options.
  *
- * TODO: Remove bad data
- *
- * `ToolModule`'s are parameterized on a [[at.forsyte.apalache.infra.passes.options.OptionGroup OptionGroup]], which
- * statically enforces the connection between a particular sequence of passes and the set of options which are required
- * to run those passes. In the degenerate case were no options are needed for a sequnece of passes, classes can extend
+ * `ToolModule`'s are parameterized on a [[infra.passes.options.OptionGroup OptionGroup]], which statically enforces the
+ * connection between a particular sequence of passes and the set of options which are required to run those passes. In
+ * the degenerate case were no options are needed for a sequnece of passes, classes can extend
  * `ToolModule(OptionGroup.Empty())`.
  *
- * Every subclass of `ToolModule` should end it's `configure` method by calling `super.configure()`, which ensures the
- * needed options are available for passes.
- *
  * Subclasses are generally expected to reduce the upper bound on `O`, giving a more precise specification of the
- * options required to run the passesses.
+ * options required to run the passesses with an `OptionGroup.Has*` trait. See, e.g.,
+ * [[infra.passes.options.OptionGroup.HasChecker OptionGroup.HasChecker]].
  *
  * @author
  *   Gabriela Moreira
