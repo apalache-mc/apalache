9af9c071da4f01c8b14ca071d9c2dc88c243caa2
Shon Feder <shon@informal.systems>
2022-09-07 07:38:53 -0400

    Update tla-io/src/main/scala/at/forsyte/apalache/io/json/JsonRepresentation.scala

--- tla-io/src/main/scala/at/forsyte/apalache/io/json/JsonRepresentation.scala
+++ tla-io/src/main/scala/at/forsyte/apalache/io/json/JsonRepresentation.scala
@@ -3,9 +3,6 @@ package at.forsyte.apalache.io.json
 /**
  * A representation of json. The concrete implementation may depend on external json libraries. Defines a toString
  * method, to be used when performing IO.
- *
- * @tparam T
- *   The class of the value which the JsonRepresentation uses to reprsent JSON
  */
 trait JsonRepresentation {
 
