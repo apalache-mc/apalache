76fef1f5f81f1cf5aa40378e32573e44b9c87dfd
Shon Feder <shon@informal.systems>
2022-09-30 12:25:58 -0400

    Fix comment

--- tla-types/src/main/scala/at/forsyte/apalache/tla/typecheck/SourceAwareTypeCheckerListener.scala
+++ tla-types/src/main/scala/at/forsyte/apalache/tla/typecheck/SourceAwareTypeCheckerListener.scala
@@ -4,14 +4,7 @@ import at.forsyte.apalache.tla.lir.storage.{ChangeListener, SourceLocator}
 import at.forsyte.apalache.tla.imp.src.SourceStore
 import at.forsyte.apalache.tla.lir.UID
 
-/**
- * <p>Type checker calls the listener on important events:</p>
- *
- * <ol> <li>the type of a new expression has been computed, or </li> <li>a type error has been found.</li> </ol>
- *
- * @author
- *   Igor Konnov
- */
+/** A [[TypeCheckerListener]] that has a source store and and a change listener */
 abstract class SourceAwareTypeCheckerListener(sourceStore: SourceStore, changeListener: ChangeListener)
     extends TypeCheckerListener {
 
