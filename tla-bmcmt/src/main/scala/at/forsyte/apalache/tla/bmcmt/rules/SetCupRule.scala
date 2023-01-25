1a90aae3eb8a741ce7d3460d78321093d49916fa
Kukovec <jure.kukovec@gmail.com>
2023-01-25 12:02:53 +0100

    setCupRule

--- tla-bmcmt/src/main/scala/at/forsyte/apalache/tla/bmcmt/rules/SetCupRule.scala
+++ tla-bmcmt/src/main/scala/at/forsyte/apalache/tla/bmcmt/rules/SetCupRule.scala
@@ -2,7 +2,7 @@ package at.forsyte.apalache.tla.bmcmt.rules
 
 import at.forsyte.apalache.infra.passes.options.SMTEncoding
 import at.forsyte.apalache.tla.bmcmt._
-import at.forsyte.apalache.tla.bmcmt.arena.ElemPtr
+import at.forsyte.apalache.tla.bmcmt.arena.{ElemPtr, FixedElemPtr, SmtExprElemPtr}
 import at.forsyte.apalache.tla.lir.{OperEx, TlaType1}
 import at.forsyte.apalache.tla.types.tla
 import at.forsyte.apalache.tla.lir.oper.{TlaBoolOper, TlaSetOper}
@@ -31,13 +31,27 @@ class SetCupRule(rewriter: SymbStateRewriter) extends RewritingRule {
         val leftSetCell = nextState.asCell
         nextState = rewriter.rewriteUntilDone(nextState.setRex(rightSet))
         val rightSetCell = nextState.asCell
-        val leftElems = nextState.arena.getHasPtr(leftSetCell)
-        val rightElems = nextState.arena.getHasPtr(rightSetCell)
+        val leftPtrs = nextState.arena.getHasPtr(leftSetCell)
+        val rightPtrs = nextState.arena.getHasPtr(rightSetCell)
 
-        val common = Set(leftElems: _*).intersect(Set(rightElems: _*))
-        val onlyLeft = Set(leftElems: _*).diff(common)
-        val onlyRight = Set(rightElems: _*).diff(common)
-        val allDistinct = common.toSeq ++ onlyLeft.toSeq ++ onlyRight.toSeq
+        val leftElems = leftPtrs.map(_.elem).toSet
+        val rightElems = rightPtrs.map(_.elem).toSet
+        val common = leftElems.intersect(rightElems)
+
+        val cellMap = (leftPtrs ++ rightPtrs).foldLeft(Map.empty[ArenaCell, Seq[ElemPtr]]) { case (m, ptr) =>
+          val elem = ptr.elem
+          m + (elem -> (m.getOrElse(elem, Seq.empty) :+ ptr))
+        }
+
+        // Fixed pointers dominate, if no pointer is fixed we take the disjunction of the smt constraints
+        val unionElemPtrs: Seq[ElemPtr] = cellMap.toSeq.map { case (cell, ptrs) =>
+          ptrs match {
+            case Seq(single) => single
+            case _ =>
+              if (ptrs.exists { _.isInstanceOf[FixedElemPtr] }) FixedElemPtr(cell)
+              else SmtExprElemPtr(cell, tla.or(ptrs.map(_.toSmt): _*))
+          }
+        }
 
         rewriter.solverContext.config.smtEncoding match {
           case SMTEncoding.Arrays =>
@@ -45,7 +59,7 @@ class SetCupRule(rewriter: SymbStateRewriter) extends RewritingRule {
             val newType = TlaType1.fromTypeTag(state.ex.typeTag)
             nextState = nextState.updateArena(_.appendCell(newType, isUnconstrained = true))
             val newSetCell = nextState.arena.topCell
-            nextState = nextState.updateArena(_.appendHas(newSetCell, allDistinct: _*))
+            nextState = nextState.updateArena(_.appendHas(newSetCell, unionElemPtrs: _*))
 
             // since newSet is initially unconstrained, we equate it to leftSet to add leftSet's elements to newSet
             rewriter.solverContext.assertGroundExpr(tla.eql(newSetCell.toBuilder, leftSetCell.toBuilder))
@@ -62,37 +76,24 @@ class SetCupRule(rewriter: SymbStateRewriter) extends RewritingRule {
             val newType = TlaType1.fromTypeTag(state.ex.typeTag)
             nextState = nextState.updateArena(_.appendCell(newType))
             val newSetCell = nextState.arena.topCell
-            nextState = nextState.updateArena(_.appendHas(newSetCell, allDistinct: _*))
+            nextState = nextState.updateArena(_.appendHas(newSetCell, unionElemPtrs: _*))
 
-            // require each cell to be in in the union iff it is exactly in its origin set
-            def addOnlyCellCons(thisSet: ArenaCell, thisElemPtr: ElemPtr): Unit = {
-              val thisElem = thisElemPtr.elem
-              val inThis = tla.selectInSet(thisElem.toBuilder, thisSet.toBuilder)
-              val inCup = tla.storeInSet(thisElem.toBuilder, newSetCell.toBuilder)
-              val notInCup = tla.storeNotInSet(thisElem.toBuilder, newSetCell.toBuilder)
-              rewriter.solverContext.assertGroundExpr(tla.ite(inThis, inCup, notInCup))
+            unionElemPtrs.foreach { ptr =>
+              val elem = ptr.elem
+              val inL = tla.in(elem.toBuilder, leftSetCell.toBuilder)
+              val inR = tla.in(elem.toBuilder, rightSetCell.toBuilder)
+
+              // TODO: replace ITE condition with ptr.toSmt ?
+              val cond = {
+                if (common.contains(elem)) tla.or(inL, inR)
+                else if (leftElems.contains(elem)) inL
+                else inR
+              }
+
+              val inCup = tla.in(elem.toBuilder, newSetCell.toBuilder)
+              val notInCup = tla.notin(elem.toBuilder, newSetCell.toBuilder)
+              rewriter.solverContext.assertGroundExpr(tla.ite(cond, inCup, notInCup))
             }
-
-            def addEitherCellCons(thisElemPtr: ElemPtr): Unit = {
-              val thisElem = thisElemPtr.elem
-              val inThis = tla.selectInSet(thisElem.toBuilder, leftSetCell.toBuilder)
-              val inOther = tla.selectInSet(thisElem.toBuilder, rightSetCell.toBuilder)
-              val inCup = tla.storeInSet(thisElem.toBuilder, newSetCell.toBuilder)
-              val notInCup = tla.storeNotInSet(thisElem.toBuilder, newSetCell.toBuilder)
-              rewriter.solverContext.assertGroundExpr(tla.ite(tla.or(inThis, inOther), inCup, notInCup))
-            }
-
-            // new implementation: as we are not using uninterpreted functions anymore, we do not have to care about
-            // the problem described below.
-            // Add equality constraints, e.g., for ({1} \ {1}) \cup {1}. Otherwise, we might require equal cells to be
-            // inside and outside the resulting set
-            //        val prodIter = new Prod2SeqIterator(leftElems, rightElems)
-            //        val eqState = rewriter.lazyEq.cacheEqConstraints(rightState.setArena(arena), prodIter.toSeq)
-            // bugfix: we have to compare the elements in both sets and thus to introduce a quadratic number of constraints
-            // add SMT constraints
-            onlyLeft.foreach(addOnlyCellCons(leftSetCell, _))
-            onlyRight.foreach(addOnlyCellCons(rightSetCell, _))
-            common.foreach(addEitherCellCons)
 
             // that's it
             nextState.setRex(newSetCell.toBuilder)
