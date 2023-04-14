c80e5ad4428b3b6164e41f5c869cbe2ab7fda066
Kukovec <jure.kukovec@gmail.com>
2023-04-14 16:34:33 +0200

    DeLambda transformation

---
+++ tla-pp/src/main/scala/at/forsyte/apalache/tla/pp/DeLambdifier.scala
@@ -0,0 +1,29 @@
+package at.forsyte.apalache.tla.pp
+
+import at.forsyte.apalache.tla.lir._
+import at.forsyte.apalache.tla.lir.oper.TlaOper
+import at.forsyte.apalache.tla.lir.transformations._
+
+/**
+ * DeLambdifier turns applications of operator-typed LET-expressions, into LET-expressions with applications pushed into
+ * the body.
+ *
+ * @author
+ *   Jure Kukovec
+ */
+class DeLambdifier(tracker: TransformationTracker) extends TlaExTransformation {
+  override def apply(input: TlaEx): TlaEx = {
+    transform(input)
+  }
+
+  private def transform: TlaExTransformation = tracker.trackEx {
+    case ex @ OperEx(TlaOper.apply, LetInEx(nameEx @ NameEx(operName), decl @ TlaOperDecl(declName, _, _)), args @ _*)
+        if operName == declName =>
+      LetInEx(
+          OperEx(TlaOper.apply, nameEx +: args.map(transform): _*)(ex.typeTag),
+          decl.copy(body = transform(decl.body)),
+      )(ex.typeTag)
+
+    case ex @ _ => ex
+  }
+}
