a8684236e278f46005bf8946758ec56838074de1
Kukovec <jure.kukovec@gmail.com>
2023-05-22 13:50:59 +0200

    PR comment

--- tla-bmcmt/src/main/scala/at/forsyte/apalache/tla/bmcmt/rules2/RewriterScope.scala
+++ tla-bmcmt/src/main/scala/at/forsyte/apalache/tla/bmcmt/rules2/RewriterScope.scala
@@ -3,7 +3,11 @@ package at.forsyte.apalache.tla.bmcmt.rules2
 import at.forsyte.apalache.tla.bmcmt.{Binding, PureArena}
 
 /**
- * A RewriterScope holds data, which is either a byproduct of rewriting, or required supplementary information.
+ * A RewriterScope holds data considered mutable during a rewriting execution:
+ *   - a PureArena, which holds the subexpression cells and their relations (has, cdm, etc.)
+ *   - a Binding, which keeps track of variable names considered bound within the current rewriting scope, and the arena
+ *     cells they are represented by. The same expression might be evaluated under different cell bindings to the same
+ *     variable name (e.g. in `P` of `\E x \in S: P`).
  *
  * @author
  *   Jure Kukovec
