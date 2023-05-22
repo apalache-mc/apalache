48536128c8666bc61eafc17d3f4e4f517caad2ca
Kukovec <jure.kukovec@gmail.com>
2023-05-22 14:29:30 +0200

    Prototype implementation of ADR24 (#2554)
    * Prototype
    
    * fmt-fix
    
    * PR comments
    
    * Apply suggestions from code review
    
    Co-authored-by: Shon Feder <shon@informal.systems>
    
    * PR comment
    
    ---------
    
    Co-authored-by: Shon Feder <shon@informal.systems>

---
+++ tla-bmcmt/src/main/scala/at/forsyte/apalache/tla/bmcmt/rules2/StratifiedRule.scala
@@ -0,0 +1,41 @@
+package at.forsyte.apalache.tla.bmcmt.rules2
+
+import at.forsyte.apalache.tla.bmcmt.ArenaCell
+import at.forsyte.apalache.tla.lir.TlaEx
+
+/**
+ * * A single rewriting rule that implements operational semantics.
+ *
+ * A rule may be parameterized by `T`, which defines the sort of supplementary information passed between the
+ * `buildArena` and `addConstraints` methods, for the purpose of generating SMT constraints. If no such information is
+ * necessary, `T = Unit`.
+ *
+ * @author
+ *   Jure Kukovec
+ */
+trait StratifiedRule[T] {
+  def isApplicable(ex: TlaEx, scope: RewriterScope): Boolean
+
+  /**
+   * Returns a triple `(scope,cell,aux)`, where
+   *   - `scope` contains the new Arena and Binding generated from `ex`.
+   *   - `cell` is an ArenaCell representation of `ex`
+   *   - `aux` contains miscellaneous data, which may be used in `addConstraints` in the process of constraint
+   *     generation.
+   *
+   * This method promises not to generate constraints as a side-effect.
+   */
+  def buildArena(ex: TlaEx)(startingScope: RewriterScope): (RewriterScope, ArenaCell, T)
+
+  /**
+   * Given the output of `buildArena`, generates SMT constraints as a side-effect.
+   */
+  def addConstraints(scope: RewriterScope, cell: ArenaCell, auxData: T): Unit
+
+  def apply(ex: TlaEx)(startingScope: RewriterScope): (RewriterScope, ArenaCell) = {
+    val (scope, exprCell, auxCells) = buildArena(ex)(startingScope)
+    addConstraints(scope, exprCell, auxCells)
+    (scope, exprCell)
+  }
+
+}
---
+++ tla-bmcmt/src/main/scala/at/forsyte/apalache/tla/bmcmt/rules2/SubstStratifiedRule.scala
@@ -0,0 +1,40 @@
+package at.forsyte.apalache.tla.bmcmt.rules2
+
+import at.forsyte.apalache.tla.bmcmt._
+import at.forsyte.apalache.tla.lir.{NameEx, TlaEx}
+import at.forsyte.apalache.tla.pp.TlaInputError
+import com.typesafe.scalalogging.LazyLogging
+
+/**
+ * Substitutes a bound name with a cell. For instance, it substitutes a name that is declared with VARIABLE or CONSTANT,
+ * as well as bound variables declared with \A, \E, set operations, etc.
+ *
+ * @author
+ *   Jure Kukovec
+ */
+class SubstStratifiedRule extends StratifiedRule[Unit] with LazyLogging {
+  override def isApplicable(ex: TlaEx, scop: RewriterScope): Boolean = ex match {
+    case NameEx(x) =>
+      // make sure that x is not an SMT constant, but a variable name
+      !ArenaCell.isValidName(x)
+
+    case _ => false
+  }
+
+  def buildArena(ex: TlaEx)(startingScope: RewriterScope): (RewriterScope, ArenaCell, Unit) = ex match {
+    case NameEx(x) =>
+      val binding = startingScope.binding
+      if (binding.contains(x)) {
+        (startingScope, binding(x), ())
+      } else {
+        logger.error("This error may show up when CONSTANTS are not initialized.")
+        logger.error("Check the manual: https://apalache.informal.systems/docs/apalache/parameters.html")
+        throw new TlaInputError(s"${getClass.getSimpleName}: Variable $x is not assigned a value")
+      }
+
+    case _ =>
+      throw new RewriterException("%s is not applicable".format(getClass.getSimpleName), ex)
+  }
+
+  def addConstraints(scope: RewriterScope, cell: ArenaCell, auxData: Unit): Unit = ()
+}
