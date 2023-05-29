6525e3ef41b4280dc74829c8b531ce342e1fe39e
Shon Feder <shon@informal.systems>
2023-05-29 16:19:03 -0400

    Use TypeUnifier for FlixibleEquality
    Closes #2581
    
    The idea to replace the hand-coded and limit type subsumption checking
    with unification emerged during a conversation with @Kukovec, who
    suggested a more limited use of the unifier.
    
    This simplifies the code thru reuse, and should give us fully general
    and correct type guards on the builder.

--- tlair/src/main/scala/at/forsyte/apalache/tla/typecomp/signatures/FlexibleEquality.scala
+++ tlair/src/main/scala/at/forsyte/apalache/tla/typecomp/signatures/FlexibleEquality.scala
@@ -1,53 +1,26 @@
 package at.forsyte.apalache.tla.typecomp.signatures
 
-import at.forsyte.apalache.tla.lir.{FunT1, RecRowT1, RecT1, RowT1, SeqT1, SetT1, TlaType1, TupT1}
+import at.forsyte.apalache.tla.lir.TlaType1
 
-import scala.collection.immutable.SortedMap
-import at.forsyte.apalache.tla.lir.VarT1
+import at.forsyte.apalache.tla.types.TypeUnifier
+import at.forsyte.apalache.tla.types.TypeVarPool
+import at.forsyte.apalache.tla.types.Substitution
 
 /**
  * Determines flexible equality ([[compatible]]), i.e. an equivalence relation over TT1, which is equality for
  * non-record types, but allows the comparison of records types with compatible fields
  *
  * @author
- *   Jure Kukovec
+ *   Jure Kukovec, ShonFeder
  */
 object FlexibleEquality {
 
-  // None if no common supertype, Some(e) if compatible, and common supertype is e.
-  def commonSupertype(lhs: TlaType1, rhs: TlaType1): Option[TlaType1] = (lhs, rhs) match {
-    case (a, b) if a == b     => Some(a)
-    case (VarT1(_), b)        => Some(b)
-    case (a, VarT1(_))        => Some(a)
-    case (SeqT1(l), SeqT1(r)) => commonSupertype(l, r).map(SeqT1)
-    case (SetT1(l), SetT1(r)) => commonSupertype(l, r).map(SetT1)
-    case (FunT1(lD, lC), FunT1(rD, rC)) =>
-      for {
-        domT <- commonSupertype(lD, rD)
-        cdmT <- commonSupertype(lC, rC)
-      } yield FunT1(domT, cdmT)
-    case (TupT1(lElems @ _*), TupT1(rElems @ _*)) if lElems.size == rElems.size =>
-      val commonTup = lElems.zip(rElems).flatMap { case (l, r) => commonSupertype(l, r) }
-      if (commonTup.size == lElems.size) Some(TupT1(commonTup: _*))
-      else None
-    case (RecT1(lFields), RecT1(rFields)) =>
-      val intersect = lFields.keySet.intersect(rFields.keySet)
-      val intersectMapOpt = intersect.foldLeft(Option(SortedMap.empty[String, TlaType1])) { case (mapOpt, e) =>
-        for {
-          map <- mapOpt
-          supertype <- commonSupertype(lFields(e), rFields(e))
-        } yield map + (e -> supertype)
-      }
-      intersectMapOpt.map { m =>
-        RecT1(
-            (lFields ++ rFields) ++ m // ++ m last overrides the entries for all e \in intersect with the computed unions
-        )
-      }
-    case (l: RecRowT1, r: RecT1)                       => commonSupertype(r, l)
-    case (l: RecT1, RecRowT1(RowT1(fieldTypes, None))) => commonSupertype(l, RecT1(fieldTypes))
-    case (RecRowT1(RowT1(lFieldTypes, None)), RecRowT1(RowT1(rFieldTypes, None))) =>
-      commonSupertype(RecT1(lFieldTypes), RecT1(rFieldTypes))
-    case _ => None
+  // None if no common supertype, Some(e) if compatible, and the common supertype is e.
+  def commonSupertype(lhs: TlaType1, rhs: TlaType1): Option[TlaType1] = {
+    // TypeUnifier is not threadsafe, and creating a new instance
+    // for each invocation prevents race conditions in the tests
+    val typeUnifier = new TypeUnifier(new TypeVarPool())
+    typeUnifier.unify(Substitution.empty, lhs, rhs).map(_._2)
   }
 
   def commonSeqSupertype(seq: Seq[TlaType1]): Option[TlaType1] =
