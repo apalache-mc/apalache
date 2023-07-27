50b027999ae54f01a938b1fa10f43e0fc14d4c06
Kukovec <jure.kukovec@gmail.com>
2023-07-27 15:10:17 +0200

    PR comments 2

--- tla-bmcmt/src/main/scala/at/forsyte/apalache/tla/bmcmt/stratifiedRules/aux/caches/IntRangeCache.scala
+++ tla-bmcmt/src/main/scala/at/forsyte/apalache/tla/bmcmt/stratifiedRules/aux/caches/IntRangeCache.scala
@@ -14,6 +14,19 @@ import at.forsyte.apalache.tla.types.tla
  */
 class IntRangeCache(intValueCache: IntValueCache) extends Cache[PureArena, (Int, Int), (ArenaCell, Seq[ArenaCell])] {
 
+  /**
+   * Given a range as a boundary-pair `(a,b)`, returns a tuple `(rArena, (rCell, rCells))`, where:
+   *   - `rCell` is the cell representing the set `a..b`
+   *   - `rCells` is s sequence of cells `c_a, c_{a+1},..., c_b`, representing the range contents, i.e. `a, a+1, ...,
+   *     b`.
+   *   - `rArena` is an extension of `arena`, containing all of the above cells, and a relation
+   *     {{{rCell --(has)--> c_a, c_{a+1},..., c_b}}}
+   *
+   * Note that this method internally calls `intValueCache.getOrCreate`, which caches all integers in the range.
+   *
+   * If `a > b`, we treat the input as if `range` were `(1,0)`, regardless of the concrete values of `a` and `b`, which
+   * caches all empty ranges to the same value.
+   */
   override protected def create(arena: PureArena, range: (Int, Int)): (PureArena, (ArenaCell, Seq[ArenaCell])) = {
 
     // We normalize a..b, such that, if the initial a is greater than b (which would give an empty range),
