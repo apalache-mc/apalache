d20a4cab8af4905c73726ded0f0faec36b2520a8
Kukovec <jure.kukovec@gmail.com>
2023-07-27 15:08:24 +0200

    Update tla-bmcmt/src/test/scala/at/forsyte/apalache/tla/bmcmt/stratifiedRules/aux/FakeCtx.scala
    Co-authored-by: Thomas Pani <thomas@informal.systems>

--- tla-bmcmt/src/test/scala/at/forsyte/apalache/tla/bmcmt/stratifiedRules/aux/FakeCtx.scala
+++ tla-bmcmt/src/test/scala/at/forsyte/apalache/tla/bmcmt/stratifiedRules/aux/FakeCtx.scala
@@ -4,11 +4,11 @@ import at.forsyte.apalache.tla.bmcmt.smt.{SolverConfig, Z3SolverContext}
 import at.forsyte.apalache.tla.lir.TlaEx
 
 /**
- * A fake context, which just intercepts any expression added via assertGroundExpr and stores it in a Seq.
+ * A mock context, which just intercepts any expression added via assertGroundExpr and stores it in a Seq.
  *
  * We can use it in tests, to observe constraint generation without needing to instrument SMT.
  */
-class FakeCtx extends Z3SolverContext(SolverConfig.default) {
+class MockZ3SolverContext extends Z3SolverContext(SolverConfig.default) {
   var constraints: Seq[TlaEx] = Seq.empty
   override def assertGroundExpr(ex: TlaEx): Unit = constraints = constraints :+ ex
 }
