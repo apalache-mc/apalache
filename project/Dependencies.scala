ccda163e9b3ec5365a7ca3dce728933e1c1fa63d
Scala Steward <scala_steward@virtuslab.com>
2023-08-05 15:38:13 +0000

    Update logback-classic, logback-core to 1.4.9

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,7 +12,7 @@ object Dependencies {
 
   object Deps {
     // Versions
-    lazy val logbackVersion = "1.4.8"
+    lazy val logbackVersion = "1.4.9"
     lazy val clistVersion = "3.5.1"
 
     // Libraries
