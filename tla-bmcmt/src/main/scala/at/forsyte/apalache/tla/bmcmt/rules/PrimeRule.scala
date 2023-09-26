466953eff20a28b0884f9a032174af21bf588289
Kukovec <jure.kukovec@gmail.com>
2023-09-26 15:10:37 +0200

    Intermediate cell type fix

--- tla-bmcmt/src/main/scala/at/forsyte/apalache/tla/bmcmt/rules/PrimeRule.scala
+++ tla-bmcmt/src/main/scala/at/forsyte/apalache/tla/bmcmt/rules/PrimeRule.scala
@@ -2,8 +2,8 @@ package at.forsyte.apalache.tla.bmcmt.rules
 
 import at.forsyte.apalache.tla.bmcmt._
 import at.forsyte.apalache.tla.lir.oper.TlaActionOper
-import at.forsyte.apalache.tla.lir.{NameEx, OperEx}
-import at.forsyte.apalache.tla.lir.UntypedPredefs._
+import at.forsyte.apalache.tla.lir.{NameEx, OperEx, TlaType1, Typed, Untyped}
+import at.forsyte.apalache.tla.types.tla
 
 /**
  * Rename x' to NameEx("x'"). We only consider the case when prime is applied to a variable.
@@ -21,8 +21,11 @@ class PrimeRule extends RewritingRule {
 
   override def apply(state: SymbState): SymbState = {
     state.ex match {
-      case OperEx(TlaActionOper.prime, NameEx(name)) =>
-        state.setRex(NameEx(name + "'"))
+      case OperEx(TlaActionOper.prime, nEx @ NameEx(name)) =>
+        nEx.typeTag match {
+          case Typed(tt1: TlaType1) => state.setRex(tla.name(name + "'", tt1))
+          case _                    => throw new InternalCheckerError("Internal expressions should be Typed", nEx)
+        }
 
       case _ =>
         throw new RewriterException("Prime operator is only implemented for variables", state.ex)
