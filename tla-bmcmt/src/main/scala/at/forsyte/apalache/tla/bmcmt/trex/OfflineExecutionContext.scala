09049563424affbbac7fcab5735369c9940132d3
Thomas Pani <thomas@informal.systems>
2023-11-29 11:50:35 +0100

    Dispose of obsolete rewriter after snapshot recovery

--- tla-bmcmt/src/main/scala/at/forsyte/apalache/tla/bmcmt/trex/OfflineExecutionContext.scala
+++ tla-bmcmt/src/main/scala/at/forsyte/apalache/tla/bmcmt/trex/OfflineExecutionContext.scala
@@ -44,6 +44,7 @@ class OfflineExecutionContext(var rewriter: SymbStateRewriter, renaming: Increme
   override def recover(snapshot: OfflineExecutionContextSnapshot): Unit = {
     val solver = RecordingSolverContext.createZ3(Some(snapshot.smtLog), snapshot.solverConfig)
     // TODO: issue #105, remove references to SolverContext, so recovery becomes less of a hack
+
     val newRewriter = rewriter match {
       case _: SymbStateRewriterImplWithArrays =>
         new SymbStateRewriterImplWithArrays(solver, renaming, rewriter.exprGradeStore)
@@ -54,6 +55,8 @@ class OfflineExecutionContext(var rewriter: SymbStateRewriter, renaming: Increme
     newRewriter.config = rewriter.config
     newRewriter.recover(snapshot.rewriterSnapshot)
     newRewriter.solverContext = solver
+
+    rewriter.dispose()
     rewriter = newRewriter
   }
 
