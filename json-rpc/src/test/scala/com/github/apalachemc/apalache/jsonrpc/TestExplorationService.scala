f3bdef7c85f8d84011918690c144421041e6ad60
Igor Konnov <igor@konnov.phd>
2025-07-31 18:47:29 +0200

    add TestExplorationService

---
+++ json-rpc/src/test/scala/com/github/apalachemc/apalache/jsonrpc/TestExplorationService.scala
@@ -0,0 +1,49 @@
+package com.github.apalachemc.apalache.jsonrpc
+
+import at.forsyte.apalache.io.ConfigManager
+import org.junit.runner.RunWith
+import org.scalatest.BeforeAndAfter
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatestplus.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class TestExplorationService extends AnyFunSuite with BeforeAndAfter  {
+  private val text =
+    """---- MODULE Inc ----
+      |EXTENDS Integers
+      |VARIABLE x
+      |Init == x = 0
+      |Next == x' = x + 1
+      |=====================
+      """.stripMargin
+
+  private var service: ExplorationService = _
+
+  before {
+    val config = ConfigManager("{common.command: 'server'}")
+    service = new ExplorationService(config)
+  }
+
+  test("load spec") {
+    service.loadSpec(LoadSpecParams(sources = Seq(text))) match {
+      case Right(LoadSpecResult(sessionId)) =>
+        assert(sessionId.nonEmpty, "Session ID should not be empty")
+      case Left(error) =>
+        fail(s"Failed to load specification: $error")
+    }
+  }
+
+  test("dispose spec") {
+    service.loadSpec(LoadSpecParams(sources = Seq(text))) match {
+        case Right(LoadSpecResult(sessionId)) =>
+            service.disposeSpec(DisposeSpecParams(sessionId)) match {
+            case Right(DisposeSpecResult(newSessionId)) =>
+                assert(newSessionId == sessionId, "Session ID should remain the same after disposal")
+            case Left(error) =>
+                fail(s"Failed to dispose specification: $error")
+            }
+        case Left(error) =>
+            fail(s"Failed to load specification for disposal: $error")
+    }
+  }
+}
