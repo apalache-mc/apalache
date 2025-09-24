a77d186603c0ceb4058f9dfc4599a422346e59da
Igor Konnov <igor@konnov.phd>
2025-09-24 08:49:45 +0200

    fix the types in tracee

--- tla-bmcmt/src/main/scala/at/forsyte/apalache/tla/tracee/tracee.scala
+++ tla-bmcmt/src/main/scala/at/forsyte/apalache/tla/tracee/tracee.scala
@@ -1,10 +1,12 @@
 package at.forsyte.apalache.tla
 
+import at.forsyte.apalache.tla.lir.TlaEx
+
 /**
  * @author
  *   Jure Kukovec
  */
 package object tracee {
-  type State = at.forsyte.apalache.io.lir.State
-  type Trace = at.forsyte.apalache.io.lir.Trace
+  type State = Map[String, TlaEx]
+  type StateSeq = IndexedSeq[State]
 }
