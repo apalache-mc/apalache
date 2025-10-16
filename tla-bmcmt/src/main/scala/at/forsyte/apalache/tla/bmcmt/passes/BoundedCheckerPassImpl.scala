01e9cfd075c1918af50df3681d8ec29dd161c690
Igor Konnov <igor@konnov.phd>
2025-10-16 17:50:53 +0200

    remove recording context

--- tla-bmcmt/src/main/scala/at/forsyte/apalache/tla/bmcmt/passes/BoundedCheckerPassImpl.scala
+++ tla-bmcmt/src/main/scala/at/forsyte/apalache/tla/bmcmt/passes/BoundedCheckerPassImpl.scala
@@ -10,14 +10,12 @@ import at.forsyte.apalache.tla.bmcmt._
 import at.forsyte.apalache.tla.bmcmt.analyses.ExprGradeStore
 import at.forsyte.apalache.tla.bmcmt.rewriter.{MetricProfilerListener, RewriterConfig}
 import at.forsyte.apalache.tla.bmcmt.search._
-import at.forsyte.apalache.tla.bmcmt.smt.{RecordingSolverContext, SolverConfig}
+import at.forsyte.apalache.tla.bmcmt.smt.{RecordingSolverContext, SolverConfig, SolverContext, Z3SolverContext}
 import at.forsyte.apalache.tla.bmcmt.trex._
 import at.forsyte.apalache.tla.imp.src.SourceStore
 import at.forsyte.apalache.tla.lir.storage.ChangeListener
 import at.forsyte.apalache.tla.lir.transformations.LanguageWatchdog
-import at.forsyte.apalache.tla.lir.transformations.standard.{
-  IncrementalRenaming, KeraLanguagePred, MonotypeLanguagePred,
-}
+import at.forsyte.apalache.tla.lir.transformations.standard.{IncrementalRenaming, KeraLanguagePred, MonotypeLanguagePred}
 import at.forsyte.apalache.tla.lir.{ModuleProperty, TlaModule, TlaOperDecl}
 import at.forsyte.apalache.tla.pp.NormalizedNames
 import com.google.inject.{Inject, Singleton}
@@ -207,7 +205,8 @@ class BoundedCheckerPassImpl @Inject() (
       input: CheckerInput,
       tuning: Map[String, String],
       solverConfig: SolverConfig): Checker.CheckerResult = {
-    val solverContext: RecordingSolverContext = RecordingSolverContext.createZ3(None, solverConfig)
+    // In contrast to the local instances, we are not recording the SMT constraints.
+    val solverContext: SolverContext = new Z3SolverContext(solverConfig)
 
     val rewriter: SymbStateRewriterImpl = params.smtEncoding match {
       case SMTEncoding.OOPSLA19 =>
