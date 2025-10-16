e725aecdc9837d8f587d8919a8473bcc6dc566d0
Igor Konnov <igor@konnov.phd>
2025-10-16 19:26:10 +0200

    fix formatting

--- tla-bmcmt/src/main/scala/at/forsyte/apalache/tla/bmcmt/passes/BoundedCheckerPassImpl.scala
+++ tla-bmcmt/src/main/scala/at/forsyte/apalache/tla/bmcmt/passes/BoundedCheckerPassImpl.scala
@@ -15,7 +15,9 @@ import at.forsyte.apalache.tla.bmcmt.trex._
 import at.forsyte.apalache.tla.imp.src.SourceStore
 import at.forsyte.apalache.tla.lir.storage.ChangeListener
 import at.forsyte.apalache.tla.lir.transformations.LanguageWatchdog
-import at.forsyte.apalache.tla.lir.transformations.standard.{IncrementalRenaming, KeraLanguagePred, MonotypeLanguagePred}
+import at.forsyte.apalache.tla.lir.transformations.standard.{
+  IncrementalRenaming, KeraLanguagePred, MonotypeLanguagePred,
+}
 import at.forsyte.apalache.tla.lir.{ModuleProperty, TlaModule, TlaOperDecl}
 import at.forsyte.apalache.tla.pp.NormalizedNames
 import com.google.inject.{Inject, Singleton}
