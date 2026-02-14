7af1e4ce75ef3d1507761e64555dd3d37af6b750
Scala Steward <scala_steward@virtuslab.com>
2026-02-14 15:27:37 +0000

    Update ujson, upickle to 4.4.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -35,8 +35,8 @@ object Dependencies {
     val shapeless = "com.chuusai" %% "shapeless" % "2.3.12"
     val tla2tools = ("org.lamport" % "tla2tools" % "1.7.4").from(
         "https://github.com/tlaplus/tlaplus/releases/download/v1.7.4/tla2tools.jar")
-    val ujson = "com.lihaoyi" %% "ujson" % "4.3.2"
-    val upickle = "com.lihaoyi" %% "upickle" % "4.3.2"
+    val ujson = "com.lihaoyi" %% "ujson" % "4.4.3"
+    val upickle = "com.lihaoyi" %% "upickle" % "4.4.3"
     val z3 = "tools.aqua" % "z3-turnkey" % "4.14.1"
     val zio = "dev.zio" %% "zio" % zioVersion
     // Keep up to sync with version in plugins.sbt
