e42450fe0b16da464014198db5f82099e328c5e5
Thomas Pani <thomas.pani@gmail.com>
2026-05-12 09:42:22 +0200

    Document CVC5 array solver limitations

--- tla-bmcmt/src/test/scala/at/forsyte/apalache/tla/bmcmt/smt/TestCvc5SolverContext.scala
+++ tla-bmcmt/src/test/scala/at/forsyte/apalache/tla/bmcmt/smt/TestCvc5SolverContext.scala
@@ -5,6 +5,7 @@ import at.forsyte.apalache.tla.bmcmt.arena.PureArenaAdapter
 import at.forsyte.apalache.tla.lir.IntT1
 import at.forsyte.apalache.tla.types.{tlaU => tla}
 import at.forsyte.apalache.tla.typecomp._
+import io.github.cvc5.{CVC5ApiException, Kind, Solver, TermManager}
 import org.scalatest.funsuite.AnyFunSuite
 
 class TestCvc5SolverContext extends AnyFunSuite {
@@ -44,4 +45,68 @@ class TestCvc5SolverContext extends AnyFunSuite {
       solver.dispose()
     }
   }
+
+  test("cvc5 requires arrays-exp for constant arrays") {
+    val termManager = new TermManager()
+    val solver = new Solver(termManager)
+    try {
+      val intSort = termManager.getIntegerSort
+      val arraySort = termManager.mkArraySort(intSort, intSort)
+      val f = termManager.mkConst(arraySort, "f")
+      val default = termManager.mkConstArray(arraySort, termManager.mkInteger(0))
+
+      // default = const 0
+      // (= f default)
+      solver.assertFormula(termManager.mkTerm(Kind.EQUAL, f, default))
+
+      val error = intercept[CVC5ApiException] {
+        solver.checkSat()
+      }
+      assert(
+          error.getMessage == "Cannot handle assertion with term of kind STORE_ALL in this configuration. Try --arrays-exp.")
+    } finally {
+      solver.deletePointer()
+      termManager.deletePointer()
+    }
+  }
+
+  test("cvc5 rejects equality between store chains over different constant arrays") {
+    val termManager = new TermManager()
+    val solver = new Solver(termManager)
+    try {
+      solver.setOption("arrays-exp", "true")
+
+      val intSort = termManager.getIntegerSort
+      val arraySort = termManager.mkArraySort(intSort, intSort)
+      val k1 = termManager.mkConst(intSort, "k1")
+      val k2 = termManager.mkConst(intSort, "k2")
+      val v1 = termManager.mkConst(intSort, "v1")
+      val v2 = termManager.mkConst(intSort, "v2")
+
+      val f = termManager.mkConstArray(arraySort, termManager.mkInteger(0))
+      val g = termManager.mkConstArray(arraySort, termManager.mkInteger(1))
+      val f1 = termManager.mkTerm(Kind.STORE, f, k1, v1)
+      val g1 = termManager.mkTerm(Kind.STORE, g, k2, v2)
+
+      // f = const 0
+      // g = const 1
+      // (= f1 (store f k1 v1))
+      // (= g1 (store g k2 v2))
+      // (= f1 g1)
+
+      solver.assertFormula(termManager.mkTerm(Kind.EQUAL, f, f))
+      solver.assertFormula(termManager.mkTerm(Kind.EQUAL, g, g))
+      solver.assertFormula(termManager.mkTerm(Kind.EQUAL, f1, g1))
+
+      val error = intercept[CVC5ApiException] {
+        solver.checkSat()
+      }
+      assert(
+          error.getMessage == "Array theory solver does not yet support write-chains connecting two different constant arrays")
+    } finally {
+      solver.deletePointer()
+      termManager.deletePointer()
+    }
+  }
+
 }
