4e8f359e4d0674471551e11ad1ec440d6d3c58f9
Igor Konnov <igor@konnov.phd>
2026-08-01 17:49:52 +0200

    DCO remediation commit for Igor Konnov <igor@konnov.phd>
    I, Igor Konnov <igor@konnov.phd>, hereby add my Signed-off-by to this commit: 497563d22f52f9b59066070f949b5a92353b44e4
    
    Signed-off-by: Igor Konnov <igor@konnov.phd>

--- tla-io/src/main/scala/at/forsyte/apalache/io/annotations/StandardAnnotations.scala
+++ tla-io/src/main/scala/at/forsyte/apalache/io/annotations/StandardAnnotations.scala
@@ -4,6 +4,7 @@ package at.forsyte.apalache.io.annotations
  * This trait collects the name of the standard annotations. In case we want to change them later.
  */
 object StandardAnnotations {
+
   /**
    * Type annotation. It should have a single string argument.
    */
