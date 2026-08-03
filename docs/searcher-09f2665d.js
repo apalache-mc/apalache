cbd41311ff88906aed9f2085d034871e8fb1463a
konnov <konnov@users.noreply.github.com>
2026-08-03 09:33:18 +0000

    deploy: 9aaff09459766519c9bf4d7a9cf1f22c127ff7e8

--- docs/searcher-09f2665d.js
+++ docs/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-b5572066.js',
+                path_to_root + 'searchindex-aa14a244.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
