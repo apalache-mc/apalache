5ed01cfa89092ca213f3d2c49373821db5efef1f
Scala Steward <scala_steward@virtuslab.com>
2026-08-05 14:33:11 +0000

    Update scalaz-core from 7.3.5 to 7.3.9
    Signed-off-by: Scala Steward <scala_steward@virtuslab.com>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -39,7 +39,7 @@ object Dependencies {
     val logging = "com.typesafe.scala-logging" %% "scala-logging" % "3.9.6"
     val scalaParserCombinators = "org.scala-lang.modules" %% "scala-parser-combinators" % "2.4.0"
     val scalaCollectionContrib = "org.scala-lang.modules" %% "scala-collection-contrib" % "0.3.0"
-    val scalaz = "org.scalaz" %% "scalaz-core" % "7.3.5"
+    val scalaz = "org.scalaz" %% "scalaz-core" % "7.3.9"
     val slf4j = "org.slf4j" % "slf4j-api" % "2.0.18"
     val shapeless = "com.chuusai" %% "shapeless" % "2.3.12"
     val tla2tools = ("org.lamport" % "tla2tools" % "1.8.0").from(
