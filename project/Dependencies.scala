2d593e3e372f1cb313e2798bd8f7083d89fb0db4
Scala Steward <scala_steward@virtuslab.com>
2026-08-15 13:51:09 +0000

    Update upickle from 4.3.2 to 4.4.3
    Signed-off-by: Scala Steward <scala_steward@virtuslab.com>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -45,7 +45,7 @@ object Dependencies {
     val tla2tools = ("org.lamport" % "tla2tools" % "1.8.0").from(
         "https://github.com/tlaplus/tlaplus/releases/download/v1.8.0/tla2tools.jar")
     val ujson = "com.lihaoyi" %% "ujson" % "4.4.3"
-    val upickle = "com.lihaoyi" %% "upickle" % "4.3.2"
+    val upickle = "com.lihaoyi" %% "upickle" % "4.4.3"
     val z3 = "tools.aqua" % "z3-turnkey" % "4.14.1"
     val zio = "dev.zio" %% "zio" % zioVersion
     // Keep up to sync with version in plugins.sbt
